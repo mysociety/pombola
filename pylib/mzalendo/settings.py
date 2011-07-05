8eefa4f85e691e993eff0d3eefb49e2979bd5d37
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-07-05 11:28:31 +0100

    fix for admin static files

--- pylib/mzalendo/settings.py
+++ pylib/mzalendo/settings.py
@@ -111,14 +111,14 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join( base_dir, "/web/static/" )
+    os.path.join( base_dir, "web/static/" ),
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
-    # 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
