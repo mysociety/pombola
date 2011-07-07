5ca3b9fecca4dc2aacc0c8a5f9e6528f0e371242
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-07-07 13:42:11 +0100

    typo

--- pylib/mzalendo/urls.py
+++ pylib/mzalendo/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 
+import settings
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
