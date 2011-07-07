a1d0c6540b40e2fa20793f6a390231dc90ae5a63
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-07-07 13:58:33 +0100

    Added collected_files dir

--- pylib/mzalendo/settings.py
+++ pylib/mzalendo/settings.py
@@ -96,7 +96,7 @@ MEDIA_ROOT = os.path.join( base_dir, "web/static/")
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.normpath( os.path.join( base_dir, "../collected_static/") )
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
