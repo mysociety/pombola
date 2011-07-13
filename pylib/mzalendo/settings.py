1eb03b7765886831f237ab396fc574e3a2ee2545
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-07-13 13:46:30 +0100

    Send all logging output to STDERR

--- pylib/mzalendo/settings.py
+++ pylib/mzalendo/settings.py
@@ -160,29 +160,29 @@ INSTALLED_APPS = (
     'core',
 )
 
-# Disable the django logging so that the default python method is
-# used (stream to stdout)
-LOGGING_CONFIG = None
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'class': 'django.utils.log.AdminEmailHandler'
-#         }
-#     },
-#     'loggers': {
-#         'django.request': {
-#             'handlers': ['mail_admins'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#     }
-# }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'stream_to_stderr': {
+            'level': 'WARN',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['stream_to_stderr'],
+            'level': 'WARN',
+            'propagate': True,
+        },
+    }
+}
