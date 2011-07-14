96aec9b02a4e2be7ed2b2c36139f39c77c6b910a
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-07-14 15:24:27 +0100

    script to create parties from json dumps

---
+++ pylib/mzalendo/core/utils/import_parties_from_json.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+# Horrible boilerplate - there must be a better way :)
+sys.path.append(
+    os.path.abspath(
+        os.path.dirname(__file__) + '../../..'
+    )
+)
+
+import setup_env
+
+import simplejson
+from pprint import pprint
+from django.template.defaultfilters import slugify
+from core import models
+from django_date_extensions.fields import ApproximateDateField, ApproximateDate
+
+party_kind = models.OrganisationKind.objects.get(slug="party")
+parties    = simplejson.loads( sys.stdin.read() )
+
+for party in parties:
+    pprint( party )
+    
+    try:
+        org = models.Organisation.objects.get(
+            slug = slugify(party['Acronym'])
+        )
+    except models.Organisation.DoesNotExist:
+        org = models.Organisation(
+            slug = slugify(party['Acronym'])
+        )
+        
+    org.kind        = party_kind  
+    org.original_id = party['PartyID']
+    org.name        = party['Name']
+
+    if party.get('FoundationYear'):
+        org.started = ApproximateDate(year=int(party['FoundationYear']))
+
+    org.save()
