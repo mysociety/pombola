e55f0ac639b8b208e9dd7b68e94bec2f50d029e4
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-07-15 11:10:27 +0100

    Added source to the contacts so we can track where they come from

--- pylib/mzalendo/core/models.py
+++ pylib/mzalendo/core/models.py
@@ -29,6 +29,7 @@ class Contact(models.Model):
     kind    = models.ForeignKey('ContactKind')
     value   = models.TextField()
     note    = models.TextField(blank=True, help_text="publicaly visible, use to clarify contact detail")
+    source  = models.CharField(max_length=500, blank=True, default='', help_text="where did this contact detail come from")
 
     # link to other objects using the ContentType system
     content_type   = models.ForeignKey(ContentType)
