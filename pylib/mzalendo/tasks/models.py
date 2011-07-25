df19223462da071a58eb0ecccdbaca6f2666b299
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-07-25 12:19:52 +0100

    Added creation/deletion of tasks based on object and list

--- pylib/mzalendo/tasks/models.py
+++ pylib/mzalendo/tasks/models.py
@@ -22,5 +22,42 @@ class Task(models.Model):
     def __unicode__(self):
         return "%s for %s" % ( self.task_code, self.content_object )
 
+
+    @classmethod
+    def objects_for(cls, obj):
+        """Return qs for all tasks for the given object"""
+        return cls.objects.filter(
+            content_type = ContentType.objects.get_for_model(obj),
+            object_id    = obj.pk
+        )
+
+    @classmethod
+    def update_for_object(cls, obj, task_code_list):
+        """Create specified tasks for this objects, delete ones that are missing"""
+
+        # get the details needed to create a generic
+        content_type = ContentType.objects.get_for_model(obj)
+        object_id    = obj.pk
+
+        # note all tasks seen so we can delete redundant existing ones
+        seen_tasks = []
+
+        # check that we have tasks for all codes requested
+        for task_code in task_code_list:
+            task, created = Task.objects.get_or_create(
+                content_type = content_type,
+                object_id    = object_id,
+                task_code    = task_code,
+            )
+            seen_tasks.append( task_code )
+
+        # go through all tasks in db and delete redundant ones
+        for task in cls.objects_for(obj):
+            if task.task_code in seen_tasks: continue
+            task.delete()
+
+        pass
+
+
     class Meta:
        ordering = ["content_type", "object_id", "task_code", ]      
--- pylib/mzalendo/tasks/tests.py
+++ pylib/mzalendo/tasks/tests.py
@@ -1,16 +1,68 @@
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
+Test the tasks
 """
 
 from django.test import TestCase
+from django.contrib.sites.models import Site
+from models import Task
+
+class TaskTest(TestCase):
+
+    def setUp(self):
+        # use the site object to run tests against
+        self.test_object = Site.objects.all()[0]
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_list_creation_and_deletion(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test that tasks are created and deleted correctly by being given an object and a list
         """
-        self.assertEqual(1 + 1, 2)
+
+        initial_list  = [ 'task-code-1', 'task-code-2' ]
+        modified_list = [ 'task-code-1', 'task-code-3' ]
+
+        # check that there are none at the start
+        self.assertEqual( Task.objects.count(), 0 )
+
+        # create a couple of tasks
+        Task.update_for_object(
+            self.test_object,
+            initial_list
+        )
+        self.assertItemsEqual(
+            [ i.task_code for i in Task.objects_for( self.test_object) ],
+            initial_list,
+        )
+        
+        # modify one of the tasks
+        for task in Task.objects_for( self.test_object ).filter(task_code='task-code-1'):
+            task.note = "test notes"
+            task.save()
+    
+        # run again - this time with a new task
+        Task.update_for_object(
+            self.test_object,
+            modified_list
+        )
+        self.assertItemsEqual(
+            [ i.task_code for i in Task.objects_for( self.test_object) ],
+            modified_list,
+        )
+
+        # check previously modified task unchanged
+        for task in Task.objects_for( self.test_object ).filter(task_code='task-code-1'):
+            self.assertEqual( task.note, "test notes" )
+    
+        # run again - with empty list
+        Task.update_for_object(
+            self.test_object,
+            []
+        )
+        self.assertItemsEqual(
+            [ i.task_code for i in Task.objects_for( self.test_object) ],
+            [],
+        )
+        
+        # check that all tasks are now deleted
+        self.assertEqual( Task.objects.count(), 0 )
+
