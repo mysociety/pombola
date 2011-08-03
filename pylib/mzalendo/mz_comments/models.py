d9b2b024eca5e6a971fbe9b97a28a1592ed19a1f
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-08-03 08:51:23 +0100

    Most recent comments first

--- pylib/mzalendo/mz_comments/models.py
+++ pylib/mzalendo/mz_comments/models.py
@@ -3,3 +3,6 @@ from django.contrib.comments.models import Comment
 
 class CommentWithTitle(Comment):
     title = models.CharField(max_length=300)
+    
+    class Meta():
+        ordering = ['-submit_date']
