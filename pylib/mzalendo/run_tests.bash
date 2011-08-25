cde6fd42cb2cc6e49becba7f41003f11e904813a
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-08-25 10:49:02 +0100

    Test runner

---
+++ pylib/mzalendo/run_tests.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# abort on any error
+set -e
+
+# run our tests
+./manage.py test core images mz_comments info tasks
+
+# This is a very ugly solution to running all the tests just for our own code.
+# running './manage.py test' will cause all the django etc tests to run as well
+# which is not wanted. Many of these tests fail - most likely due to the
+# mzalendo settings being used.
+#
+# It would be much nicer to have a better way to run the tests.
