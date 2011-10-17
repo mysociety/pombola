c041d37757b3a4d3df1708412e0e7c4f6bf80c36
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-17 15:52:55 +0100

    Better formatting of CDF results

--- pylib/mzalendo/settings.py
+++ pylib/mzalendo/settings.py
@@ -165,6 +165,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'django_bcrypt',
     
     'registration_defaults',
