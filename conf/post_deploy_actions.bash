bf46d90e425a41b5c7f3b36a45ed545ef1387dc4
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-24 16:19:20 +0100

    Add in some virtualenv related bits

---
+++ conf/post_deploy_actions.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# abort on any errors
+set -e
+
+# check that we are in the expected directory
+cd `dirname $0`/..
+
+# create/update the virtual environment
+pip install \
+    -E ../mzalendo-virtualenv \
+    -r requirements.txt
+
+# use the virtualenv just created/updated
+source ../mzalendo-virtualenv/bin/activate
+
+# make sure that there is no old code (the .py files may have been git deleted) 
+find . -name '*.pyc' -delete
+
+# go to the project directory for local config
+cd pylib/mzalendo
+
+# get the database up to speed
+./manage.py syncdb
+./manage.py migrate
+
+# gather all the static files in one place
+./manage.py collectstatic --noinput
+
+cd --
