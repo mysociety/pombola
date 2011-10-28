142696b71c1c7bb9359a9635d8182b95572662a4
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-28 18:24:58 +0100

    Delete old comments system

--- mzalendo/mz_comments/__init__.py
+++
@@ -1,8 +0,0 @@
-from mz_comments.models import CommentWithTitle
-from mz_comments.forms import CommentFormWithTitle
-
-def get_model():
-    return CommentWithTitle
-
-def get_form():
-    return CommentFormWithTitle
