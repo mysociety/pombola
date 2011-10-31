b816d24331ba8acc909a9ec3344a8a7071539f16
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-10-31 12:28:54 +0000

    Approve/reject comments from admin list view

--- mzalendo/comments2/admin.py
+++ mzalendo/comments2/admin.py
@@ -19,49 +19,30 @@ class CommentAdmin(admin.ModelAdmin):
     ordering = ('-submit_date',)
     raw_id_fields = ('user',)
     search_fields = ('comment', 'user__username', 'ip_address')
-    # actions = ["flag_comments", "approve_comments", "remove_comments"]
+    actions = ["approve_comments", "reject_comments"]
 
-    # def get_actions(self, request):
-    #     actions = super(CommentsAdmin, self).get_actions(request)
-    #     # Only superusers should be able to delete the comments from the DB.
-    #     if not request.user.is_superuser and 'delete_selected' in actions:
-    #         actions.pop('delete_selected')
-    #     if not request.user.has_perm('comments.can_moderate'):
-    #         if 'approve_comments' in actions:
-    #             actions.pop('approve_comments')
-    #         if 'remove_comments' in actions:
-    #             actions.pop('remove_comments')
-    #     return actions
-    # 
-    # def flag_comments(self, request, queryset):
-    #     self._bulk_flag(request, queryset, perform_flag,
-    #                     lambda n: ungettext('flagged', 'flagged', n))
-    # flag_comments.short_description = _("Flag selected comments")
-    # 
-    # def approve_comments(self, request, queryset):
-    #     self._bulk_flag(request, queryset, perform_approve,
-    #                     lambda n: ungettext('approved', 'approved', n))
-    # approve_comments.short_description = _("Approve selected comments")
-    # 
-    # def remove_comments(self, request, queryset):
-    #     self._bulk_flag(request, queryset, perform_delete,
-    #                     lambda n: ungettext('removed', 'removed', n))
-    # remove_comments.short_description = _("Remove selected comments")
-    # 
-    # def _bulk_flag(self, request, queryset, action, done_message):
-    #     """
-    #     Flag, approve, or remove some comments from an admin action. Actually
-    #     calls the `action` argument to perform the heavy lifting.
-    #     """
-    #     n_comments = 0
-    #     for comment in queryset:
-    #         action(request, comment)
-    #         n_comments += 1
-    # 
-    #     msg = ungettext(u'1 comment was successfully %(action)s.',
-    #                     u'%(count)s comments were successfully %(action)s.',
-    #                     n_comments)
-    #     self.message_user(request, msg % {'count': n_comments, 'action': done_message(n_comments)})
+    def get_actions(self, request):
+        actions = super(CommentAdmin, self).get_actions(request)
+        # Only superusers should be able to delete the comments from the DB.
+        if not request.user.is_superuser and 'delete_selected' in actions:
+            actions.pop('delete_selected')
+        if not request.user.has_perm('comments.can_moderate'):
+            if 'approve_comments' in actions:
+                actions.pop('approve_comments')
+            if 'remove_comments' in actions:
+                actions.pop('reject_comments')
+        return actions
+    
+    def approve_comments(self, request, queryset):
+        queryset.update(status="approved")
+        self.message_user(request, "Approved the comments")
+    approve_comments.short_description = _("Approve selected comments")
+    
+    def reject_comments(self, request, queryset):
+        queryset.update(status="rejected")
+        self.message_user(request, "Rejected the comments")
+    reject_comments.short_description = _("Reject selected comments")
+
 
 class CommentFlagAdmin(admin.ModelAdmin):
     list_display  = [ 'id', 'user', 'comment', 'flag_date', ]
