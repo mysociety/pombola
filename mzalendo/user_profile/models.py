df848f520b4cf77504756809df7f81b0431668de
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-11-02 16:05:11 +0000

    Basics fields

--- mzalendo/user_profile/models.py
+++ mzalendo/user_profile/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    # This field is required.
+    user = models.OneToOneField(User)
+
+    # should we create a public profile page for this user?
+    is_profile_public = models.NullBooleanField()
+
+    # private details
+    mobile = models.CharField(max_length=30, blank=True)
+
+    # other web presence
+    facebook = models.CharField(max_length=30, blank=True)
+    twitter = models.CharField(max_length=30, blank=True)
+
+    # location
+    constituency = models.ForeignKey(
+        'core.Place',
+        limit_choices_to = {
+            'kind__slug': 'constituency',
+        },
+        blank=True, null=True,
+    )
