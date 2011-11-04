3e1b9373f87ae70223a1c0daf616019abb8480ac
Edmund von der Burg <evdb@ecclestoad.co.uk>
2011-11-04 11:04:21 +0000

    Move tests into subdirectory

--- mzalendo/user_profile/tests/test_user_creation.py
+++ mzalendo/user_profile/tests/test_user_creation.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from models import UserProfile
+from user_profile.models import UserProfile
 
 class ProfileTest(TestCase):
 
