1462304d7240e9e7c281ce9d8f3bd3c892ed869d
Edmund von der Burg <evdb@mysociety.org>
2011-11-11 10:22:04 +0000

    Only show social login links if configured

--- mzalendo/core/context_processors.py
+++ mzalendo/core/context_processors.py
@@ -9,5 +9,6 @@ def add_settings( request ):
             'STATIC_GENERATION_NUMBER': settings.STATIC_GENERATION_NUMBER,
             'MAPIT_URL':                settings.MAPIT_URL,
             'GOOGLE_ANALYTICS_ACCOUNT': settings.GOOGLE_ANALYTICS_ACCOUNT,
+            'SOCIAL_AUTH_ENABLED_BACKENDS': settings.SOCIAL_AUTH_ENABLED_BACKENDS,
         }        
     }
