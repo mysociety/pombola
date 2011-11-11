f7f712ffa1ce8eb2edb12cbb32a36ab4ffeb23e5
Edmund von der Burg <evdb@mysociety.org>
2011-11-11 13:28:25 +0000

    Create the source model for hansard (#78)

--- mzalendo/hansard/models/__init__.py
+++ mzalendo/hansard/models/__init__.py
@@ -1,1 +1,2 @@
+from source import Source
 from chunk import Chunk
---
+++ mzalendo/hansard/models/source.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Source(models.Model):
+    """
+    Sources of the hansard transcripts
+    
+    For example a PDF transcript.
+    """
+
+    name           = models.CharField(max_length=200, unique=True)
+    date           = models.DateField()
+    url            = models.URLField()
+    last_processed = models.DateField(blank=True, null=True)
+
+    class Meta:
+        app_label = 'hansard'
+        ordering = [ 'date', 'name' ]
+
+    def __unicode__(self):
+        return self.name
+            
