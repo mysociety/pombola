cf7c5c5fca7d26e9369d4e44de22e1a585c51056
Edmund von der Burg <evdb@mysociety.org>
2011-11-22 19:14:16 +0000

    Use coffeescript instead

--- mzalendo/core/static/main.js
+++ mzalendo/core/static/main.js
@@ -1,14 +1,10 @@
-// enable the autocomplete on main search box
-$(
-    function() {
-        $('#main_search_box').autocomplete({
-            source: "/autocomplete",
-            minLength: 2,
-            select: function( event, ui ) {
-            	if ( ui.item ) {
-                    window.location = ui.item.url;
-            	}
-            }
-        })
-    }
-)
+
+  $(function() {
+    return $('#main_search_box').autocomplete({
+      source: "/autocomplete",
+      minLength: 2,
+      select: function(event, ui) {
+        if (ui.item) return window.location = ui.item.url;
+      }
+    });
+  });
