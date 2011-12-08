f548cbb0dec568df78673e07120a528310fa0532
Edmund von der Burg <evdb@mysociety.org>
2011-12-08 12:15:49 +0000

    send user to correct page following social login (#84)

--- mzalendo/user_profile/seltests/test_twitter.py
+++ mzalendo/user_profile/seltests/test_twitter.py
@@ -9,7 +9,9 @@ class TwitterTestCase(TwitterSeleniumTestCase):
         self.twitter_login()
         self.twitter_revoke_access_to_test_app()
         
-        self.open_url('/')
+        start_url = '/person/all'
+        
+        self.open_url(start_url)
 
         # Go to twitter and cancel the login
         self.twitter_click_login_link_on_mzalendo_site()
@@ -21,12 +23,24 @@ class TwitterTestCase(TwitterSeleniumTestCase):
             'You tried to login using a website like Twitter' in self.page_source
         )
 
+        # Note - ideally even the failed login would lead to us retaining the
+        # 'next' parameter so that an alternative login would work. Have created an
+        # issue for this: https://github.com/omab/django-social-auth/issues/192
+        
+        # for now go back to the page we want to return to
+        self.open_url(start_url)
+
         # go to Twitter and confirm the log in
         self.twitter_click_login_link_on_mzalendo_site()
         driver.find_element_by_id("allow").click()
 
         # check that we are now logged in
         self.assert_user_logged_in()
+        
+        # check that we are still on the start_url page
+        # print start_url
+        # print driver.current_url
+        self.assertTrue( start_url in driver.current_url )
 
         # check that the user created has the correct name
         user = self.get_current_user()
