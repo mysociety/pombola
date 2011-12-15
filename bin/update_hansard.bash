83a6fd386eb143668d3d81e4b8d3d1211e45d410
Edmund von der Burg <evdb@mysociety.org>
2011-12-15 15:47:50 +0000

    Wrapper script to be run from cron to update all of hansard

---
+++ bin/update_hansard.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# abort on any errors
+set -e
+
+# check that we are in the expected directory
+cd `dirname $0`/../mzalendo
+
+source ../../mzalendo-virtualenv/bin/activate
+
+./manage.py hansard_check_for_new_sources
+./manage.py hansard_process_sources
+./manage.py hansard_assign_speakers
