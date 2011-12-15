6a201518dc8f99c4ec4d652c0bc51c3b5e09c8f6
Edmund von der Burg <evdb@mysociety.org>
2011-12-15 15:20:59 +0000

    Hook up most of the toolbar buttons to pages

--- mzalendo/hansard/urls.py
+++ mzalendo/hansard/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from hansard.views import IndexView, SittingView
 
 urlpatterns = patterns('hansard.views',
-    url( r'^$', IndexView.as_view() ),
+    url( r'^$', IndexView.as_view(), name="index" ),
 
     # not the final URL structure - but something to start work with
     url( r'^sitting/(?P<pk>\d+)/', SittingView.as_view(), name="sitting_view" ),
