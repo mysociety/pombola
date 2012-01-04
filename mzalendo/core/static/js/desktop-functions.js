e2ae9f87817c03cbb45f2b6fb36be4050e2df03a
Josh Angell <josh@supercooldesign.co.uk>
2012-01-04 14:47:33 +0000

    add height fix for profile-info box when it > #page in height
    Signed-off-by: Josh Angell <josh@supercooldesign.co.uk>

--- mzalendo/core/static/js/desktop-functions.js
+++ mzalendo/core/static/js/desktop-functions.js
@@ -164,4 +164,16 @@ $(function(){
   $('ul.scorecard article').live('click', function(){
     hideShow($('div.details', $(this)), $(this));
   });
+
+
+  /*
+   * Height fix for pages with .profile-info box
+   */
+  var pro_h = $('.profile-info').height()+210; //add 210 for the profile pic
+  var main_h = $('#page').height();
+
+  if(pro_h > main_h){
+    $('#page').css({'min-height':pro_h});
+  }
+
 });
