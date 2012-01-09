5b9b9ce70123f9f061bb5a6786b4ffcc4256d923
Edmund von der Burg <evdb@mysociety.org>
2012-01-09 12:50:50 +0000

    Added docs and manager/queryset to aliases

--- mzalendo/hansard/models/alias.py
+++ mzalendo/hansard/models/alias.py
@@ -4,8 +4,32 @@ from django.db import models
 
 from core.models import Person
 
+class AliasQuerySet(models.query.QuerySet):
+    pass
+
+class AliasManager(models.Manager):
+    def get_query_set(self):
+        return AliasQuerySet(self.model)        
+
 class Alias(models.Model):
-    """Model for linking a parliamentary alias to a person"""
+    """
+    Model for linking a parliamentary alias to a person
+    
+    The person and ignored fields can both be blank. Their combinations have
+    the following meanings:
+    
+      person   | ignored  | meaning
+      ---------+----------+--------------------------------------------
+      has val  | None     | use this alias                             
+      None     | has val  | ignore this alias                          
+      None     | None     | Human should inspect this entry            
+      has val  | has val  | Does not make sense - entry will be ignored
+
+    When the check is run for missing aliases entries are made with both person
+    and ignored as None for aliases that are found. These can then be used in
+    the interface to find the next alias that needs to be filled in.
+
+    """
 
     alias   = models.CharField( max_length=40, unique=True )
     person  = models.ForeignKey( Person, blank=True, null=True )
