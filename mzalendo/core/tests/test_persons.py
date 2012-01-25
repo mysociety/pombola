cb6c47f4796439f77277854e8eb662a09a7decaf
Duncan Parkes <duncan@mysociety.org>
2012-01-25 20:36:13 +0000

    Add some tests for the scorecard_overall method as we'd like it to be.

--- mzalendo/core/tests/test_persons.py
+++ mzalendo/core/tests/test_persons.py
@@ -42,43 +42,90 @@ class PersonTest(WebTest):
         
 class PersonScorecardTest(TestCase):
     def setUp(self):    
-        # create a test person
+        # Alf is a person, but with no particular position.
         self.alf = models.Person.objects.create(
-            legal_name="Alfred Smith"
-        )
+            legal_name="Alfred Smith",
+            slug='alfred_smith',
+            )
         
-        self.score_category_1 = mzalendo.scorecards.models.Category.objects.create(
-            name='Test1',
-            slug='test1',
-            synopsis='Test1 Synopsis',
-            description='Test1 Description',
+        self.cat_contactability = mzalendo.scorecards.models.Category.objects.create(
+            name='Contactability',
+            slug='contactability',
+            synopsis='How easy is it to get in contact with this person',
+            description='Contactability description',
             )
 
-        self.score_category_2 = mzalendo.scorecards.models.Category.objects.create(
-            name='Test2',
-            slug='test2',
-            synopsis='Test2 Synopsis',
-            description='Test2 Description',
+        self.cat_ntacdf = mzalendo.scorecards.models.Category.objects.create(
+            name='NTACDF',
+            slug='ntacdf',
+            synopsis='NTA constituency data',
+            description='NTA constituency data description',
             )
 
         self.alf.scorecard_entries.create(
-            category=self.score_category_1,
-            date=datetime.date(2525, 1, 1),
+            category=self.cat_contactability,
+            date=datetime.date(2010, 1, 1),
             remark="Very good!",
             score=1,
             )
 
-    def testScorecardOverall(self):
+        # Bob, however, is the MP for Bobsplace.
+        self.bob = models.Person.objects.create(
+            legal_name="Bob Jones",
+            slug='bob_jones',
+            )
+
+        self.mp_title = models.PositionTitle.objects.create(
+            name='mp',
+            )
+
+        self.place_kind_constituency = models.PlaceKind.objects.create(
+            name='Constituency',
+            )
+
+        self.bobs_place = models.Place.objects.create(
+            name="Bob's Place",
+            kind=self.place_kind_constituency,
+            )
+
+        self.bobs_position = models.Position.objects.create(
+            person=self.bob,
+            place=self.bobs_place,
+            category='political',
+            title=self.mp_title,
+            )
+        
+        # Bob is average at one thing
+        self.bob.scorecard_entries.create(
+            category=self.cat_contactability,
+            date=datetime.date(2010, 1, 1),
+            remark="Vaguely contactable if you know what you're doing.",
+            score=0,
+            )
+
+        # Bob's constituency data is pretty bad though.
+        self.bobs_place.scorecard_entries.create(
+            category=self.cat_ntacdf,
+            date=datetime.date(2010, 1, 1),
+            remark="Nothing much left here, it's all been stolen.",
+            score=-1,
+            )
+
+    def testScorecardOverallNonMP(self):
         # import pdb;pdb.set_trace()
         assert self.alf.scorecard_overall() == 1
         assert self.alf.scorecard_overall_as_word() == 'good'
 
         self.alf.scorecard_entries.create(
-            category=self.score_category_2,
-            date=datetime.date(2525, 1, 1),
+            category=self.cat_ntacdf,
+            date=datetime.date(2010, 1, 1),
             remark="Awful.",
             score=-1,
             )
 
         assert self.alf.scorecard_overall() == 0
         assert self.alf.scorecard_overall_as_word() == 'average'
+
+    def testScorecardOverallMP(self):
+        assert self.bob.scorecard_overall() == -0.5, "Bob's score: %s" %self.bob.scorecard_overall()
+        assert self.bob.scorecard_overall_as_word() == 'bad', "Bob's word: %s" %self.bob.scorecard_overall_as_word()
