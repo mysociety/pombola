fb1d4ffc483bc923caf6d4b5f35b4f6e364c39cb
Duncan Parkes <duncan@mysociety.org>
2012-01-25 15:57:14 +0000

    Add a couple of simple tests for the Person scorecard.

--- mzalendo/core/tests/test_persons.py
+++ mzalendo/core/tests/test_persons.py
@@ -1,20 +1,19 @@
 import re
+import datetime
 
 from django.conf import settings
-
 from django.core import mail
 from django_webtest import WebTest
-from core         import models
 from django.test.client import Client
+from django.test import TestCase
+
 from django.contrib.auth.models import User
 
+from core import models
+import mzalendo.scorecards.models
 
 class PersonTest(WebTest):
-    def setUp(self):
-        pass
-        
-    def test_naming(self):
-        
+    def test_naming(self):        
         # create a test person
         person = models.Person(
             legal_name="Alfred Smith"
@@ -41,3 +40,45 @@ class PersonTest(WebTest):
         self.assertEqual( person.name, "Fred Smith" )
         self.assertEqual( person.additional_names(), ['Freddy Smith'] )
         
+class PersonScorecardTest(TestCase):
+    def setUp(self):    
+        # create a test person
+        self.alf = models.Person.objects.create(
+            legal_name="Alfred Smith"
+        )
+        
+        self.score_category_1 = mzalendo.scorecards.models.Category.objects.create(
+            name='Test1',
+            slug='test1',
+            synopsis='Test1 Synopsis',
+            description='Test1 Description',
+            )
+
+        self.score_category_2 = mzalendo.scorecards.models.Category.objects.create(
+            name='Test2',
+            slug='test2',
+            synopsis='Test2 Synopsis',
+            description='Test2 Description',
+            )
+
+        self.alf.scorecard_entries.create(
+            category=self.score_category_1,
+            date=datetime.date(2525, 1, 1),
+            remark="Very good!",
+            score=1,
+            )
+
+    def testScorecardOverall(self):
+        # import pdb;pdb.set_trace()
+        assert self.alf.scorecard_overall() == 1
+        assert self.alf.scorecard_overall_as_word() == 'good'
+
+        self.alf.scorecard_entries.create(
+            category=self.score_category_2,
+            date=datetime.date(2525, 1, 1),
+            remark="Awful.",
+            score=-1,
+            )
+
+        assert self.alf.scorecard_overall() == 0
+        assert self.alf.scorecard_overall_as_word() == 'average'
