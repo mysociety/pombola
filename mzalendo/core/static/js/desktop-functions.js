65a729e084be1c2deddfbef3a47fe8dbb30ee3e5
Dave Whiteland <dave@mysociety.org>
2012-01-27 09:44:47 +0000

    remove interactive behaviour for scorcard items with no additional info to reveal

--- mzalendo/core/static/js/desktop-functions.js
+++ mzalendo/core/static/js/desktop-functions.js
@@ -157,7 +157,7 @@ $(function(){
   $('div.details').hide();
 
   // hide/show details
-  $('ul.scorecard article').live('click', function(){
+  $('ul.scorecard article.has-data').live('click', function(){
     hideShow($('div.details', $(this)), $(this));
   });
 
