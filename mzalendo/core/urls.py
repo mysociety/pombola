8ebdc2cd59436494e68d2a819429b930fc8664a5
Duncan Parkes <duncan@mysociety.org>
2012-02-02 11:34:53 +0000

    Refactor core urls with some includes.

--- mzalendo/core/urls.py
+++ mzalendo/core/urls.py
@@ -4,53 +4,36 @@ from django.views.generic import DetailView, ListView
 
 from core import models
 
-urlpatterns = patterns('core.views',
-    # Homepage
-    url(r'^$', 'home', name='home'),
+person_patterns = patterns('core.views',
+    url(r'^all/',       'person_list',       name='person_list'),
 
-    # Lists
-    url(r'^person/all/',       'person_list',       name='person_list'),
-
-    url(r'^place/all/',
-        ListView.as_view(model=models.Place),
-        name='place_list'),
-    url(r'^place/constituencies/',
-        ListView.as_view(queryset=models.Place.objects.all().constituencies()),
-        name='constituency_list'),
-    url(r'^place/counties/',
-        ListView.as_view(queryset=models.Place.objects.all().counties()),
-        name='county_list'),
-
-    url(r'^organisation/all/', 'organisation_list', name='organisation_list'),
-    
     # featured person ajax load
     url(
-        r'^person/featured/(?P<direction>(before|after))/(?P<current_slug>[-\w]+)',
+        r'^featured/(?P<direction>(before|after))/(?P<current_slug>[-\w]+)',
         'featured_person', 
         name='featured_person'
     ),
     
-    # Objects
-    url(r'^person/(?P<slug>[-\w]+)/',       'person',       name='person'),
-    url(r'^position/(?P<slug>[-\w]+)/',     'position',     name='position'),
+    url(r'^(?P<slug>[-\w]+)/',       'person',       name='person'),
+                         )
 
-    url(r'^place/is/(?P<slug>[-\w]+)/', 'place_kind', name='place_kind'),
-    url(r'^place/(?P<slug>[-\w]+)/$',   'place',      name='place'),
+place_patterns = patterns('core.views',
+    url(r'^all/',
+        ListView.as_view(model=models.Place),
+        name='place_list'),
+    url(r'^constituencies/',
+        ListView.as_view(queryset=models.Place.objects.all().constituencies()),
+        name='constituency_list'),
+    url(r'^counties/',
+        ListView.as_view(queryset=models.Place.objects.all().counties()),
+        name='county_list'),
 
-    url(r'^organisation/is/(?P<slug>[-\w]+)/', 'organisation_kind', name='organisation_kind'),
-    url(r'^organisation/(?P<slug>[-\w]+)/$',   'organisation',      name='organisation'),
+    url(r'^is/(?P<slug>[-\w]+)/', 'place_kind', name='place_kind'),
+    url(r'^(?P<slug>[-\w]+)/$',   'place',      name='place'),
 
     # Tab content
     url(
-        r'^organisation/(?P<slug>[-\w]+)/related_person_tab', 
-        DetailView.as_view(
-            model=models.Organisation,
-            template_name_suffix='_related_person_tab',
-        ),
-        name="organisation_related_person_tab",        
-    ),
-    url(
-        r'^place/(?P<slug>[-\w]+)/related_person_tab', 
+        r'^(?P<slug>[-\w]+)/related_person_tab', 
         DetailView.as_view(
             model=models.Place,
             template_name_suffix='_related_person_tab',
@@ -58,14 +41,43 @@ urlpatterns = patterns('core.views',
         name="place_related_person_tab",        
     ),
     url(
-        r'^place/(?P<slug>[-\w]+)/related_organisation_tab', 
+        r'^(?P<slug>[-\w]+)/related_organisation_tab', 
         DetailView.as_view(
             model=models.Place,
             template_name_suffix='_related_organisation_tab',
         ),
         name="place_related_organisation_tab",        
     ),
+                          )
+
+organisation_patterns = patterns('core.views',
+    url(r'^all/', 'organisation_list', name='organisation_list'),
+
+    # Tab content
+    url(
+        r'^(?P<slug>[-\w]+)/related_person_tab', 
+        DetailView.as_view(
+            model=models.Organisation,
+            template_name_suffix='_related_person_tab',
+        ),
+        name="organisation_related_person_tab",        
+    ),
+
+    url(r'^is/(?P<slug>[-\w]+)/', 'organisation_kind', name='organisation_kind'),
+    url(r'^(?P<slug>[-\w]+)/$',   'organisation',      name='organisation'),
     
+                                 )
+
+urlpatterns = patterns('core.views',
+    # Homepage
+    url(r'^$', 'home', name='home'),
+
+    (r'^person/', include(person_patterns)),
+    (r'^place/', include(place_patterns)),
+    (r'^organisation/', include(organisation_patterns)),
+
+    url(r'^position/(?P<slug>[-\w]+)/',     'position',     name='position'),
+
     # specials
     url(r'^parties', 'parties', name='parties'),
 
