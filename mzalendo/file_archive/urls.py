a139ed1903ec90ad7e1d3ba550a7f601c1369b97
Edmund von der Burg <evdb@mysociety.org>
2012-02-06 16:05:28 +0000

    Add link to the url for the file - closes #273

--- mzalendo/file_archive/urls.py
+++ mzalendo/file_archive/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import patterns, include, url, handler404
 import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<slug>[\w\-]+)$', views.redirect_to_file, name='file_archive' ),
+    url( r'^(?P<slug>[\w\-]+)$', views.redirect_to_file, name='file_archive' ),
 )
