6989152714fff599d347306e3e941e9e0d58caac
Dave Whiteland <dave@mysociety.org>
2012-02-06 16:40:47 +0000

    baby js trick to preserve search terms switching between searches

--- mzalendo/core/static/js/desktop-functions.js
+++ mzalendo/core/static/js/desktop-functions.js
@@ -241,4 +241,18 @@ $(function(){
   }).on('mouseout', function(){
     $(this).removeClass('active');
   });
+  
+  $("#search-hansard-home").click(function(e){
+    e.preventDefault();
+    location.href="/search/hansard?q=" + escape($('#core-search').val());
+  });
+  $("#search-hansard-instead").click(function(e){
+    e.preventDefault();
+    location.href="/search/hansard?q=" + escape($('#id_q').val());
+  });
+  $("#search-core-instead").click(function(e){
+    e.preventDefault();
+    location.href="/search?q=" + escape($('#id_q').val());
+  })
+  
 });
