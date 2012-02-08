223b27a35287550bf59063c3d20fb4491a157354
Edmund von der Burg <evdb@mysociety.org>
2012-02-08 16:54:22 +0000

    Capture email for feedback  closes #313

--- mzalendo/feedback/admin.py
+++ mzalendo/feedback/admin.py
@@ -3,11 +3,11 @@ from feedback.models import Feedback
 
 class FeedbackAdmin(admin.ModelAdmin):
 
-    list_display = ('comment', 'user', 'url', 'created',)
+    list_display = ('comment', 'user', 'email', 'url', 'created',)
     list_filter = ('created',)
     date_hierarchy = 'created'
     ordering = ('-created',)
     raw_id_fields = ('user',)
-    search_fields = ('comment', 'user__username', 'url')
+    search_fields = ('comment', 'user__username', 'url', 'email')
 
 admin.site.register( Feedback, FeedbackAdmin )
--- mzalendo/feedback/seltests/feedback_dialog.py
+++ mzalendo/feedback/seltests/feedback_dialog.py
@@ -22,54 +22,68 @@ class FeedbackTestCase(MzalendoSeleniumTestCase):
         # use a large window so we get the dialog
         self.create_feedback_using_wide_screen(message='wide window')
 
+        # Test again with emails
+        self.create_feedback_using_narrow_screen(message='narrow window + email', email="foo@example.com")
+        self.create_feedback_using_wide_screen(message='wide window + email', email="foo@example.com")
+
         # log in and check that the user is stored.
         self.login('superuser')
         self.create_feedback_using_wide_screen(message='logged_in_user')
 
-        # check that the two bits of feedback are stored in the the database
-        self.assertEqual( Feedback.objects.all().count(), 3 )
-
         actual = {}
         for fb in Feedback.objects.all():
-            if fb.user: actual[fb.comment] = fb.user.username
-            else:       actual[fb.comment] = None
+            data = {}
+
+            data['email'] = fb.email
+
+            if fb.user: data['username'] = fb.user.username
+            else:       data['username'] = None
+
+            actual[fb.comment] = data
+
+        # import pprint
+        # pprint.pprint(actual)
 
         self.assertEqual(
             actual,
             {
-                'narrow window':  None,
-                'wide window':    None,
-                'logged_in_user': 'superuser',
+                'narrow window':         {'username': None,        'email': '', },
+                'wide window':           {'username': None,        'email': '', },
+                'narrow window + email': {'username': None,        'email': 'foo@example.com', },
+                'wide window + email':   {'username': None,        'email': 'foo@example.com', },
+                'logged_in_user':        {'username': 'superuser', 'email': '', },
             }
         )
 
 
-    def create_feedback_using_wide_screen(self, message):
+    def create_feedback_using_wide_screen(self, message, email=''):
         self.resize_to_desktop()
         self.open_url(reverse('home'))        
 
         driver = self.driver
 
         driver.find_element_by_link_text("Give us feedback").click()
-        self.fill_in_feedback_form(message)
+        self.fill_in_feedback_form(message, email)
         driver.find_element_by_css_selector("span.ui-icon.ui-icon-closethick").click()
 
 
-    def create_feedback_using_narrow_screen(self, message):
+    def create_feedback_using_narrow_screen(self, message, email=''):
         self.resize_to_mobile()
         self.open_url(reverse('home'))
 
         driver = self.driver
 
         driver.find_element_by_link_text("Give us feedback").click()
-        self.fill_in_feedback_form(message)
+        self.fill_in_feedback_form(message, email)
         driver.find_element_by_link_text("page you were on").click()
     
     
-    def fill_in_feedback_form(self, message):
+    def fill_in_feedback_form(self, message, email):
         driver = self.driver
         driver.find_element_by_id("id_comment").clear()
         driver.find_element_by_id("id_comment").send_keys(message)
+        driver.find_element_by_id("id_email").clear()
+        driver.find_element_by_id("id_email").send_keys(email)
         driver.find_element_by_xpath("//input[@value='Send feedback']").click()
         time.sleep(1) # just to let things catch up
         self.failUnless(self.thanks_text in driver.page_source)
