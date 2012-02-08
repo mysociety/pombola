ecfde473b413e75f2589a431b44eb1fb2b87dfd2
Edmund von der Burg <evdb@mysociety.org>
2012-02-08 20:54:13 +0000

    Clarify the time period that Hansard scoring is done over

--- mzalendo/scorecards/management/commands/scorecard_update_person_hansard_appearances.py
+++ mzalendo/scorecards/management/commands/scorecard_update_person_hansard_appearances.py
@@ -22,6 +22,8 @@ class Command(NoArgsCommand):
         # Find all the people we should score for
         people = Person.objects.all().is_mp()
         
+        # How far back should we look for hansard appearances?
+        duration_string = " in the last six months"
         lower_limit = datetime.date.today() - datetime.timedelta(183)
 
         for person in people:
@@ -35,13 +37,13 @@ class Command(NoArgsCommand):
 
             if hansard_count < 6:
                 entry.score = -1
-                entry.remark = "Hardly ever speaks in parliament"
+                entry.remark = "Hardly ever spoke in parliament" + duration_string
             elif hansard_count < 60:
                 entry.score = 0
-                entry.remark = "Sometimes speaks in parliament"
+                entry.remark = "Sometimes spoke in parliament" + duration_string
             else:
                 entry.score = 1
-                entry.remark = "Frequently speaks in parliament"
+                entry.remark = "Frequently spoke in parliament" + duration_string
             
             entry.date = datetime.date.today()
 
