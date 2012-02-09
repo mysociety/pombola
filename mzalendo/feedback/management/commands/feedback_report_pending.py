fb94265c4fd3e5ad2fc500edcfd874db8bffda18
Edmund von der Burg <evdb@mysociety.org>
2012-02-09 13:01:20 +0000

    Create an alert script for pending feedback (closes #329)

---
+++ mzalendo/feedback/management/commands/feedback_report_pending.py
@@ -0,0 +1,31 @@
+import datetime
+
+from django.core.management.base import NoArgsCommand
+from django.contrib.sites.models import Site
+
+from feedback.models import Feedback
+
+class Command(NoArgsCommand):
+    help = 'Report all the feedbac that needs attention'
+    args = ''
+
+    def handle_noargs(self, **options):
+
+        pending = Feedback.objects.filter( status='pending' )
+        
+        
+        # If there are no reports to deal with then exit
+        if not pending.exists():
+            return
+        
+        subject = "Feedback requires attention - %u pending reports" % pending.count()
+        message = "Please visit the admin and process the feedback as needed."
+        url = 'http://%s/admin/feedback/feedback/?status=pending' % Site.objects.get_current().domain
+        
+
+        print subject
+        print
+        print message
+        print
+        print url
+
