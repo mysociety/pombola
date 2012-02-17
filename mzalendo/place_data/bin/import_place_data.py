d6f2abb1cdf8d98094a18593265299e28d3e2f91
Duncan Parkes <duncan@mysociety.org>
2012-02-17 17:06:02 +0000

    Generalize the import script to do both constituency and county data.

--- mzalendo/place_data/bin/import_place_data.py
+++ mzalendo/place_data/bin/import_place_data.py
@@ -9,10 +9,13 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'mzalendo.settings'
 sys.path.append('../../../')
 sys.path.append('../../')
 
+from django.template.defaultfilters import slugify
+
 from mzalendo.core.models import Place
 from mzalendo.place_data.models import Entry
 
-filename = sys.argv[1]
+place_kind_slug = sys.argv[1]
+filename = sys.argv[2]
 csv_file = open(filename, 'rU')
 
 csv_reader = csv.DictReader(csv_file, dialect='excel')
@@ -21,16 +24,27 @@ csv_reader = csv.DictReader(csv_file, dialect='excel')
 csv_reader.fieldnames = [x.strip() for x in csv_reader.fieldnames]
 
 for row in csv_reader:
-    place_slug = row['slug'].strip()
+    try:
+        place_slug = row['slug'].strip()
+    except KeyError:
+        # If there's no slug column, try slugifying the name column
+        # This will currently only happen on the Counties - the constituency
+        # spreadsheet has slugs.
+        # If we needed this to work for constituencies, we'd have to not add
+        # -constituency on the end as they don't have that.
+        place_slug = slugify(row['name'].strip()) + '-' + place_kind_slug
+    
+    # Check place with this slug exists and is of the right kind.
+    try:
+        place = Place.objects.get(slug=place_slug, kind__slug=place_kind_slug)
+    except Place.DoesNotExist:
+        print "Cannot find %s with slug %s, continuing with next place." % (place_kind_slug, place_slug)
+        continue
 
     try:
-        data_row = Entry.objects.get(place__slug=place_slug)
+        data_row = Entry.objects.get(place=place)
     except Entry.DoesNotExist:
         data_row = Entry()
-        try:
-            place = Place.objects.get(slug=place_slug)
-        except:
-            import pdb;pdb.set_trace()
         data_row.place = place
 
     data_row.population_male = int(row['Male Population'])
@@ -46,10 +60,15 @@ for row in csv_reader:
     data_row.area_rank = int(row['Area Size Rank 1=Highest'])
     data_row.population_density = decimal.Decimal(row['Density people per Sq. Km'])
     data_row.population_density_rank = int(row['Population Density Rank 1=Highest'])
-    data_row.registered_voters_total = int(row['Total Registered Voters'])
-    data_row.registered_voters_proportion = decimal.Decimal(row['Registered Voters as % of Population'])
-    data_row.registered_voters_proportion_rank = int(row['Registered Voters % Rank 1=Highest'])
-    data_row.youth_voters_proportion = decimal.Decimal(row['Youth Voters as a % of Total'])
-    data_row.youth_voters_proportion_rank = int(row['Youth Voters % Rank 1=Highest'])
+
+    try:
+        data_row.registered_voters_total = int(row['Total Registered Voters'])
+        data_row.registered_voters_proportion = decimal.Decimal(row['Registered Voters as % of Population'])
+        data_row.registered_voters_proportion_rank = int(row['Registered Voters % Rank 1=Highest'])
+        data_row.youth_voters_proportion = decimal.Decimal(row['Youth Voters as a % of Total'])
+        data_row.youth_voters_proportion_rank = int(row['Youth Voters % Rank 1=Highest'])
+    except KeyError:
+        # One some kinds of place, such as Counties, these columns don't exist.
+        pass
 
     data_row.save()
