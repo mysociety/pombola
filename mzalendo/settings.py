83a2a5537559a69b591d94ddf622ead210f54fc8
Edmund von der Burg <evdb@mysociety.org>
2012-02-24 10:28:50 +0000

    For the GIS admin templates

--- mzalendo/settings.py
+++ mzalendo/settings.py
@@ -225,6 +225,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.gis',
     'django_bcrypt',
     
     'registration_defaults',
