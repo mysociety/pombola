ca6b1425aafbbc43388bf40c8f32d5576a528d09
Edmund von der Burg <evdb@mysociety.org>
2012-03-15 14:14:38 +0000

    Fix menus etc

--- wordpress/mzalendo-wordpress/footer.php
+++ wordpress/mzalendo-wordpress/footer.php
@@ -1,24 +1,30 @@
-            </div> <!-- close .page-wrapper -->
-        </div> <!-- close #page -->
+            </div>
+        </div>
 
         <footer id="site-footer">
             <div class="wrapper">
-                <ul class="nav">
-                    <li><a href="http://info.mzalendo.com/person/all/">People</a></li>
-                    <li><a href="http://info.mzalendo.com/organisation/all/">Organisations</a></li>
-                    <li><a href="http://info.mzalendo.com/place/all/">Places</a></li>
-                </ul>
 
-                <ul class="utilities">
-                    <li><a href="http://info.mzalendo.com/info/policies/">Privacy</a></li>
-                    <li><a href="http://info.mzalendo.com/feedback/" class="feedback_link">Give us feedback</a></li>
-                </ul>
+                <div id="footer-menu" class="footer-section">
+                    <h4>Mzalendo</h4>
+                    <ul class="nav">
+                      <li><a href="http://info.mzalendo.com/">Home</a></li>
+                      <li><a href="http://info.mzalendo.com/info/democracy-resources">Democracy Resources</a></li>
+                      <li><a href="http://info.mzalendo.com/person/politicians/">Politicians</a></li>
+                      <li><a href="http://info.mzalendo.com/info/parliament-overview">Parliament</a></li>
+                      <li><a href="http://info.mzalendo.com/info/places-overview">Places</a></li>
+                      <li><a href="http://info.mzalendo.com/info/2012-overview">2012</a></li>
+                      <li><a href="http://www.mzalendo.com/blog/">Blog</a></li>
+                      <li><a href="http://info.mzalendo.com/info/newsletter">Newsletter</a></li>
+                      <li><a href="http://info.mzalendo.com/info/mzalendo-overview">About</a></li>
+                      <li><a href="http://info.mzalendo.com/info/policies">Privacy</a></li>                        
+                    </ul>
+                </div>
 
-                <div class="subscribe-box">
+                <div class="footer-section subscribe-box">
                     <h2>Email Newsletter</h2>
 
                     <form action="http://mzalendo.us2.list-manage.com/subscribe/post?u=c0b4940a102336a932a5c30c4&amp;id=9963e6377d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
-                        <label for="mce-EMAIL">Subscribe to recieve updates</label>
+                        <label for="mce-EMAIL">Subscribe to receive updates</label>
                         <div class="subscribe-fields">
                             <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Your email address here" required>
                             <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
@@ -27,59 +33,9 @@
                 </div>
             </div>
         </footer>
-        <script type="text/javascript">
-            /*
-             * Test for mobile / desktop and load appropriate libs/scripts
-             */
-            // this is not yet ideal... it reacts a bit slow if the cdn fails
 
-            //define global vars
-            window.mzalendo_settings = {
-                analytics_id : 'UA-660910-5',
-                facebook_app_id: '315517795130516'
-            }
-
-            var post_jquery_desktop_load = [
-                '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js',
-                'http://info.mzalendo.com/static/js/libs/jquery.form-v2.94.js', // TODO - only load when needed for feedback form
-                'http://info.mzalendo.com/static/js/desktop-functions.js'
-            ];
-
-            Modernizr.load([
-                {
-                    test : Modernizr.mq('only screen and (min-width: 320px) and (max-width: 640px)'),
-                    yep : ['http://info.mzalendo.com/static/js/libs/zepto-0.8.min.js', 'http://info.mzalendo.com/static/js/mobile-functions.js', '<?php bloginfo('template_url'); ?>/assets/js/wp-mobile-functions.js'],
-                    nope : '//ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js',
-                    complete: function () {
-                        // only run this if mq failed - e.g. desktop or no mq support (IE)
-                        if(Modernizr.mq('only screen and (min-width: 320px) and (max-width: 640px)') === false) {
-                            // test for jquery
-                            if ( !window.jQuery ) {
-                                // not downloaded from CDN, so get local
-                                Modernizr.load([
-                                    {
-                                        load : 'http://info.mzalendo.com/static/js/libs/jquery-1.7.0.min.js',
-                                        complete : function () {
-                                            // once local has been fetched, check for jQuery object again and load static libs and functions if there
-                                            if ( window.jQuery ) {
-                                                Modernizr.load( post_jquery_desktop_load );
-                                            }
-                                        }
-                                    }
-                                ]);
-                            }else{
-                                // if jQuery object is there, load in the rest of the static libs and functions
-                                Modernizr.load( post_jquery_desktop_load );
-                            }
-                        }
-                    }
-                },
-                {
-                    load: ['//platform.twitter.com/widgets.js', 'http://info.mzalendo.com/static/js/analytics.js', 'http://info.mzalendo.com/static/js/fb-like.js']
-                }
-            ]);
-        </script>
         <div id="fb-root"></div>
         <?php // wp_footer(); ?>
     </body>
 </html>
+
--- wordpress/mzalendo-wordpress/header.php
+++ wordpress/mzalendo-wordpress/header.php
@@ -9,29 +9,31 @@
         <meta http-equiv="cleartype" content="on">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-            
+        <link rel="stylesheet" type="text/css" href="http://info.mzalendo.com/static/css/all.css" media="screen, handheld" />
 
-        <link rel="stylesheet" type="text/css" href="http://info.mzalendo.com/static/css/basic.css" media="screen, handheld" />
-        <link rel="stylesheet" type="text/css" href="http://info.mzalendo.com/static/css/mobile.css" media="only screen and (min-width: 320px) and (max-width: 640px)" />
-        <link rel="stylesheet" type="text/css" href="http://evdb.mzalendo.mysociety.org/static/css/desktop.css" media="only screen and (min-width: 640px)" />
-
-        <!--[if lt IE 9]>
-            <link rel="stylesheet" type="text/css" href="http://info.mzalendo.com/static/css/desktop.css" />
+        <!--[if (lt IE 9) & (!IEMobile)]>
+            <script src="http://info.mzalendo.com/static/js/respond.v1.1.0.min.js"></script>
         <![endif]-->
 
+        <script type="text/javascript">
+            window.mzalendo_settings = {
+                analytics_id : 'UA-660910-5',
+                facebook_app_id: '315517795130516',
+                static_url:                'http://info.mzalendo.com/static/',
+                static_generation_number:  '21',
+                extra_js: []
+            }
+        </script>
         <script src="http://info.mzalendo.com/static/js/libs/modernizr-2.0.6.custom.js"></script>
-        <link rel="icon" type="image/png" href="http://info.mzalendo.com/static/images/favicon.png" />
-        
+        <script src="http://info.mzalendo.com/static/js/loader.js?21"></script>
+
+        <link rel="icon" type="image/png" href="http://info.mzalendo.com/static/images/favicon.png">
         
         <!-- wp specific stuff -->
         <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
-        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/basic.css" media="screen, handheld" />
-        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/desktop.css" media="only screen and (min-width: 640px)" />
 
-        <link href="http://info.mzalendo.com/static/css/jquery-ui-1.8.17.custom.css" media="screen" rel="stylesheet" type="text/css" />
-        
         <?php 
             //no js allowed, I want only mine here....*evil laugh*
             //wp_enqueue_script('jquery');
@@ -41,118 +43,131 @@
     <body>
         <header id="site-header">
             <div class="wrapper">
-                <a href="http://info.mzalendo.com" id="logo">Mzalendo</a>
-                <form action="http://mzalendo.com/blog" method="get" id="search">
-                    <input id="main_search_box" type="text" name="s" placeholder="search&hellip;" />
-                    <button type="submit">Submit</button>
-                </form>
-                <menu id="site-user-tools">
-                    <ul>                        
-                        <li class="login"><a href="http://info.mzalendo.com/accounts/login/?next=/">login</a></li>
+              <a href="http://info.mzalendo.com/" id="logo">Mzalendo</a>
+
+                <menu id="mobile-top-tools">
+                    <ul>
+                        <li><a href="#footer-menu" class="m-icon-nav">menu</a></li>
                     </ul>
                 </menu>
+        
+                <form action="<?php bloginfo('url'); ?>/" method="get" id="header-search">
+                    <input  type="text" name="s" placeholder="search&hellip;" />
+                    <button type="submit">Submit</button>
+                </form>
+                
             </div>
         </header>
+
 
         <div id="main-menu">
             <nav class="wrapper">
                 <ul>
-                    <li><a href="http://info.mzalendo.com">Home</a></li>
-                    <li class=""><a href="http://info.mzalendo.com/info/democracy-resources">Democracy Resources</a>
-                        <ul id="democracy-resources">
-                            <li>
-                                <a href="http://info.mzalendo.com/info/citizens-rights">Citizens Rights &amp; Responsibilities</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/how-democracy-works">How Our Democracy Works</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/democracy-faq">Democracy FAQs</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/constitution-2010">Constitution 2010</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/election-legislation">Election Legislation</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/democracy-documents">Democracy Documents</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="http://info.mzalendo.com/person/politicians/">Politicians</a></li>
-                    <li><a href="http://info.mzalendo.com/info/parliament-overview">Parliament</a>
-                        <ul id="parliament-overview">
-                            <li>
-                                <a href="http://info.mzalendo.com/info/parliament-overview">Parliament Overview</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/ministry-overview">Ministry Overview</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/committee-overview">Committee Overview</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/bills-overview">Bills Overview</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/hansard/">Hansard</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/political-parties">Political Parties</a>
-                            </li>
-                        </ul>                    
-                    </li>
-                    <li><a href="http://info.mzalendo.com/info/places-overview">Places</a>
-                        <ul id="places-overview">
-                            <li>
-                                <a href="http://info.mzalendo.com/info/places-overview">Places Overview</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/place/constituencies/">Constituencies</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/place/counties/">Counties</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class=""><a href="http://info.mzalendo.com/info/2012-overview">2012</a>
-                        <ul id="2012">
-                            <li>
-                                <a href="http://info.mzalendo.com/info/2012-overview">2012 Overview</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/2012-aspirants">Aspirants</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li><a href="http://mzalendo.com/blog/">Blog</a></li>
-                    <li><a href="http://info.mzalendo.com/info/newsletter">Newsletter</a></li>
-                    <li class=""><a href="http://info.mzalendo.com/info/mzalendo-overview">About</a>
-                        <ul id="mzalendo-overview">
-                            <li>
-                                <a href="http://info.mzalendo.com/info/mzalendo-overview">Mzalendo Overview</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/site-faq">Site FAQs</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/policies">Policies</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/partners">Partners</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/scorecard-faq">Scorecard FAQs</a>
-                            </li>
-                            <li>
-                                <a href="http://info.mzalendo.com/info/contact">Contact</a>
-                            </li>
-                        </ul>
-                    </li>
+                  <li><a href="http://info.mzalendo.com/">Home</a></li>
+                  <li><a href="http://info.mzalendo.com/info/democracy-resources">Democracy Resources</a>
+                  
+                      <ul id="democracy-resources">
+                          <li>
+                              <a href="http://info.mzalendo.com/info/citizens-rights">Citizens Rights &amp; Responsibilities</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/how-democracy-works">How Our Democracy Works</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/democracy-faq">Democracy FAQs</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/constitution-2010">Constitution 2010</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/election-legislation">Election Legislation</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/democracy-documents">Democracy Documents</a>
+                          </li>
+                      </ul>
+                  
+                  </li>
+                  <li><a href="http://info.mzalendo.com/person/politicians/">Politicians</a></li>
+                  <li><a href="http://info.mzalendo.com/info/parliament-overview">Parliament</a>
+                  
+                      <ul id="parliament-overview">
+                          <li>
+                              <a href="http://info.mzalendo.com/info/parliament-overview">Parliament Overview</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/ministry-overview">Ministry Overview</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/committee-overview">Committee Overview</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/bills-overview">Bills Overview</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/hansard/">Hansard</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/political-parties">Political Parties</a>
+                          </li>
+                      </ul>                    
+                  
+                  </li>
+                  <li><a href="http://info.mzalendo.com/info/places-overview">Places</a>
+                  
+                      <ul id="places-overview">
+                          <li>
+                              <a href="http://info.mzalendo.com/info/places-overview">Places Overview</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/place/constituencies/">Constituencies</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/place/counties/">Counties</a>
+                          </li>
+                      </ul>
+                  
+                  </li>
+                  <li><a href="http://info.mzalendo.com/info/2012-overview">2012</a>
+                  
+                      <ul id="2012">
+                          <li>
+                              <a href="http://info.mzalendo.com/info/2012-overview">2012 Overview</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/2012-aspirants">Aspirants</a>
+                          </li>
+                      </ul>
+                  
+                  </li>
+                  <li><a href="http://www.mzalendo.com/blog/">Blog</a></li>
+                  <li><a href="http://info.mzalendo.com/info/newsletter">Newsletter</a></li>
+                  <li><a href="http://info.mzalendo.com/info/mzalendo-overview">About</a>
+                  
+                      <ul id="mzalendo-overview">
+                          <li>
+                              <a href="http://info.mzalendo.com/info/mzalendo-overview">Mzalendo Overview</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/site-faq">Site FAQs</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/policies">Policies</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/partners">Partners</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/scorecard-faq">Scorecard FAQs</a>
+                          </li>
+                          <li>
+                              <a href="http://info.mzalendo.com/info/contact">Contact</a>
+                          </li>
+                      </ul>
+                  
+                  </li>
                 </ul>
             </nav>
         </div>
-
-        <div id="page"> <!-- start #page -->
-            <div class="page-wrapper wrapper"> <!-- start .page-wrapper -->
+        <div id="page">
+            <div class="page-wrapper wrapper">
