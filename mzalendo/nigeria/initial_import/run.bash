d16ddb013a87bdd6a934446e2cf1093deb412438
Edmund von der Burg <evdb@mysociety.org>
2012-07-23 12:36:40 +0100

    Separate fetch stage from process stage

--- mzalendo/nigeria/initial_import/run.bash
+++ mzalendo/nigeria/initial_import/run.bash
@@ -10,17 +10,21 @@ mkdir -p var/representatives var/senators
 export ALL_REP_URL="http://www.nassnig.org/nass2/Princ_officers_all.php?title_sur=Hon."
 export ALL_SEN_URL="http://www.nassnig.org/nass/Princ_officers_all.php?title_sur=Sen."
 
+# gather all html content
 cd var/representatives
-curl -o all.html $ALL_REP_URL
-../../extract_urls.py < all.html > urls.txt
-wget -B $ALL_REP_URL -i urls.txt 
 ../../representative_page_to_json.py profile.php*
 cd ../..
 
 cd var/senators
-curl -o all.html $ALL_SEN_URL
-../../extract_urls.py < all.html > urls.txt
-wget -B $ALL_SEN_URL -i urls.txt 
-../../senators_page_to_json.py profile.php*
+../../senator_page_to_json.py profile.php*
 cd ../..
 
+
+# process html into json
+cd var/representatives
+../../representative_page_to_json.py profile.php*
+cd ../..
+
+cd var/senators
+../../senator_page_to_json.py profile.php*
+cd ../..
