95ca3b7b8d4acbaf37b2099d4edc8eba78bef9b9
Edmund von der Burg <evdb@mysociety.org>
2012-10-01 16:14:58 +0100

    Replace accidentally removed fetch commands

--- mzalendo/nigeria/initial_import/run.bash
+++ mzalendo/nigeria/initial_import/run.bash
@@ -10,6 +10,20 @@ mkdir -p var/representatives var/senators
 export ALL_REP_URL="http://www.nassnig.org/nass2/Princ_officers_all.php?title_sur=Hon."
 export ALL_SEN_URL="http://www.nassnig.org/nass/Princ_officers_all.php?title_sur=Sen."
 
+# fetch all html
+
+cd var/representatives
+curl -o all.html $ALL_REP_URL
+../../extract_urls.py < all.html > urls.txt
+wget -B $ALL_REP_URL -i urls.txt 
+cd ../..
+
+cd var/senators
+curl -o all.html $ALL_SEN_URL
+../../extract_urls.py < all.html > urls.txt
+wget -B $ALL_SEN_URL -i urls.txt 
+cd ../..
+
 # gather all html content
 cd var/representatives
 ../../representative_page_to_json.py profile.php*
