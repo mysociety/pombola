5401f07228cb19fb9dd7bee9d0e8fd90d1f19dee
Emmanuel Okyere <chief@hutspace.net>
2012-11-12 14:41:10 +0000

    Added ghana hansard parser

---
+++ mzalendo/odekro/management/hansard_parser.py
@@ -0,0 +1,190 @@
+import sys
+import re
+import datetime
+
+BLANK, SERIES_VOL_NO, TIME, DATE, START_TIME, \
+HEADING, LINE, SCENE, SPEECH, ACTION = range(10)
+
+SERIES_VOL_NO_PATTERN = r'^\s*([A-Z]+)\s+SERIES\s+VOL\.?\s*(\d+)\s*N(O|o|0)\.?\s*(\d+)\s*$'
+DATE_PATTERN = r'^\s*(\w+\s*,\s*)?(\d+)\w{0,2}\s+(\w+),?\s+(\d+)\s*$'
+
+TITLES_TEMPLATE = '(Mr|Mrs|Ms|Miss|Papa|Alhaji|Madam|Dr|Prof|Chairman|Chairperson)'
+TIME_TEMPLATE = '(\d\d?)(:|\.)(\d\d)\s*(am|a.m|AM|A.M|pm|PM|p.m|P.M|noon)\.?[\s\-]*'
+
+
+HEADING_PATTERN = r'^\s*([A-Z\s]+)\s*$'
+SCENE_PATTERN = r'^\s*(\[[\w\s]+\])\s*$'
+SPEECH_PATTERN = r'^\s*%s(.+):\s*(.*)\s*$' % TITLES_TEMPLATE
+# POSSIBLE_SPEECH_PATTERN = r'^\s*%s(.+)\s*$' % TITLES_TEMPLATE
+
+ACTION_PATTERN = r'^\s*%s(.+)\s*-\s+(.+)\s+-\s*$' % TITLES_TEMPLATE
+
+START_TIME_PATTERN = r'The\s+House\s+met\s+at\s+%s' % TIME_TEMPLATE
+TIME_PATTERN = r'^\s*%s$' % TIME_TEMPLATE
+
+
+MONTHS = dict(jan=1, feb=2, mar=3, apr=4, may=5, jun=6, 
+              jul=7, aug=8, sep=9, oct=10, nov=11, dec=12)
+
+ORDINAL_WORDS = dict(first=1, second=2, third=3, fourth=4, fifth=5,
+                     sixth=6, seventh=7)
+
+
+HEADER_PATTERNS = (
+    (DATE, DATE_PATTERN),
+    (SERIES_VOL_NO, SERIES_VOL_NO_PATTERN)
+    )
+
+PATTERNS = (
+    (SPEECH, SPEECH_PATTERN),
+    (HEADING, HEADING_PATTERN),
+    (TIME, TIME_PATTERN),
+    (START_TIME, START_TIME_PATTERN),
+    (SCENE, SCENE_PATTERN),
+    (ACTION, ACTION_PATTERN)
+    )
+
+
+
+def parse(lines):
+    lines = scan(lines)
+
+    head = parse_head(lines) # end_line
+    entries = parse_body(body(lines)) # start_line
+    return head, entries
+
+def scan(lines, header=True):
+    if header:
+        return [scan_header_line(x) or scan_line(x) for x in lines]
+    return [scan_line(x) for x in lines]
+
+def body(lines):
+    """Returns a generator for lines in the body of the hansard"""
+    for i, row in enumerate(lines):
+        _, line, _ = row
+        if line.lower().strip().startswith('the house met at'):
+            return (x for x in lines[i:])
+    return (x for x in lines)
+
+
+def parse_head(lines, nbr=0):
+
+    series, vol, no, date = None, None, None, None
+    
+    for i, row in enumerate(lines):
+        kind, line, match = row
+        if SERIES_VOL_NO == kind:
+            series = ORDINAL_WORDS[match.group(1).lower()]
+            vol = int(match.group(2))
+            no = int(match.group(4))
+        elif DATE == kind:
+            date = datetime.date(int(match.group(4)),
+                                 MONTHS[match.group(3).lower()[:3]], 
+                                 int(match.group(2)))
+        if series and vol and no and date:
+            nbr += i
+            break
+    return series, vol, no, date, nbr
+
+def parse_body(lines):
+    entries = []
+    
+    time = None
+    topic = None
+    page = None
+    ahead = False
+
+    kind, line, match = None, None, None
+
+    # lines = (x for x in lines)
+    while True:
+        try:
+            if not ahead:
+                kind, line, match = lines.next()
+            else:
+                ahead = False
+
+            if kind is SPEECH:
+                speech, kind, line, match, ahead = parse_speech(time, match, lines)
+                entries.append(speech)
+            elif kind is HEADING:
+                entries.append(dict(heading=line.strip(), time=time))
+            elif kind in (TIME, START_TIME):
+                time = _time(match)
+            elif kind is ACTION:
+                person = '%s%s' % (match.group(1), match.group(2))
+                entries.append(dict(action=match.group(3), name=person.strip()))
+            else:
+                pass
+        except StopIteration:
+            break
+
+    return entries
+
+
+def parse_speech(time, match, lines):
+    """A speech has the ff properties:
+    1. Starts with a member's name followed by a colon
+    2. Is followed by a number of LINEs or BLANKs"""
+    name = '%s%s' % (match.group(1), match.group(2))
+    speech = match.group(3)
+    kind, line, match = None, None, None
+    ahead = False
+
+    while True:
+        try:
+            kind, line, match = lines.next()
+            if kind == LINE: 
+                speech += line
+            elif kind == BLANK:
+                speech = speech.strip() + '\n'
+            else:
+                ahead = True
+                break
+        except StopIteration:
+            break
+    return (dict(time=time, name=name, speech=speech.strip()), kind, line, match, ahead)
+
+def parse_time(s):
+    match = re.match(TIME_PATTERN, s)
+    if match:
+        return _time(match)
+    return None
+
+def _time(match):
+    hh, mm = int(match.group(1)), int(match.group(3))
+    t = match.group(4)
+    if t in ('pm', 'PM', 'p.m', 'P.M') and hh != 12:
+        hh += 12
+    return datetime.time(hh, mm)
+
+def scan_header_line(line):
+    for kind, pattern in HEADER_PATTERNS:
+        match = re.match(pattern, line)
+        if match:
+            return (kind, line, match)
+    return None
+
+def scan_line(line):
+    if not line.strip():
+        return (BLANK, '\n', None)
+    for kind, pattern in PATTERNS:
+        match = re.match(pattern, line)
+        if match:
+            return (kind, line, match)
+    return (LINE, line.replace('\n', ' '), None)
+
+
+def main(args):
+    fin = open(args[1], 'r')
+    lines = fin.readlines()
+    fin.close()
+    # print lines
+    head, entries = parse(lines)
+    print head
+    for entry in entries:
+        print entry, '\n'
+    return 0
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ mzalendo/odekro/tests.py
@@ -0,0 +1,87 @@
+import os
+import datetime
+# import time
+# import json
+# import tempfile
+# import subprocess
+
+# from unittest import TestCase
+
+import unittest
+
+from management.hansard_parser import parse
+
+
+class GhanaParserTest(unittest.TestCase):
+    basedir = os.path.abspath(os.path.dirname( __file__ ))
+    sample = os.path.join(basedir, 'data', 'hansard-sample.txt')
+    
+    entries = None
+    head = None
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.head, cls.entries = parse(open(cls.sample, 'r').readlines())
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_parse_time_string(self):
+        
+        data = {
+            '1.00 p.m.':  datetime.time(13),
+            '1.00 a.m.':  datetime.time(1),
+            '12.00 p.m.': datetime.time(12), # am and pm make no sense at noon or midnight - but define what we want to happen
+            '12.30 p.m.': datetime.time(12, 30)
+        }
+        
+        for s, t in data.items():
+            self.assertEqual(parse_time(s), t)
+
+        self.assertNone(parse_time('foo.bar'))
+        
+
+    def test_header(self):
+        self.assertEqual(4, self.head['series'])
+        self.assertEqual(76, self.head['volume'])
+        self.assertEqual(13, self.head['number'])
+        self.assertEqual(datetime.date(2012, 2, 14), self.head['date'])
+        self.assertEqual(datetime.time(10, 40), self.head['time'])
+
+    def test_entries(self):
+        t = datetime.time(10, 40)
+
+        x = self.entries[0]
+        self.assertTrue(x['text'].lower().endswith('speaker in the chair'))
+        self.assertEqual(t, x['time'])
+
+        x = self.entries[1]
+        self.assertEqual('PRAYERS', x['text'])
+        self.assertEqual(t, x['time'])
+
+        topic = 'Votes and Proceedings and the Official Report'
+        
+
+        speeches = [x for x in self.entries if x['kind'] is 'speech']
+
+        self.assertEqual(122, len(speeches))
+        
+        x = self.entries[0]
+
+        self.assertEqual(topic, x['topic'])
+        self.assertEqual('Madam Speaker', x['name'])
+
+
+        x = self.entries[-1]
+        self.assertEqual('ADJOURNMENT', x['text'])
+        self.assertEqual(datetime.time(13, 20), x['time'])
+        self.assertEqual(datetime.datetime(2012, 2, 15, 10), x['next'])
+
+    def test_page_order(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
