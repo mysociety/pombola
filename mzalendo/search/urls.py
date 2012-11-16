09432b88e561afc6d228f45d579911cd10c33aa7
Ian <idesouza@hutspace.net>
2012-11-16 14:04:05 -0500

    added url and view for generating  json for a tag clould

---
+++ mzalendo/search/urls.py
@@ -0,0 +1,47 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from haystack.forms import ModelSearchForm, SearchForm
+from haystack.query import SearchQuerySet
+from haystack.views import SearchView
+
+from mzalendo.core    import models as core_models
+from mzalendo.hansard import models as hansard_models
+
+urlpatterns = patterns('search.views',
+
+    # Haystack and other searches
+    # url( r'^location/',    'location_search',        name="location_search"     ),
+    url( r'^autocomplete/', 'autocomplete',           name="autocomplete"        ),
+
+    # General search - just intended for the core app
+    url(
+        r'^$',
+        SearchView(
+            searchqueryset = SearchQuerySet().models(
+                core_models.Person,
+                core_models.Organisation,
+                core_models.Place,
+                core_models.PositionTitle
+            ),
+            form_class=SearchForm,            
+        ),
+        name='core_search'
+    ),
+
+    # Hansard search
+    url(
+        r'^hansard/$',
+        SearchView(
+            searchqueryset = SearchQuerySet().models(
+                hansard_models.Entry,
+            ),
+            form_class=SearchForm,
+            template="search/hansard.html",            
+        ),
+        name='hansard_search',
+    ),
+
+    # Tag cloud
+    url( r'^tagcloud/$', 'tagcloud', name='tagcloud'),
+
+)
---
+++ mzalendo/search/views.py
@@ -0,0 +1,99 @@
+import re
+
+from django.http import HttpResponse
+from django.shortcuts  import render_to_response, get_object_or_404, redirect
+from django.template   import RequestContext
+from django.utils import simplejson
+
+# from mzalendo.helpers import geocode
+
+from core import models
+
+from haystack.query import SearchQuerySet
+from mzalendo.hansard import models as hansard_models
+
+# def location_search(request):
+#     
+#     loc = request.GET.get('loc', '')
+# 
+#     results = geocode.find(loc) if loc else []
+#     
+#     # If there is one result find that matching areas for it
+#     if len(results) == 1:
+#         mapit_areas = geocode.coord_to_areas( results[0]['lat'], results[0]['lng'] )
+#         areas = [ models.Place.objects.get(mapit_id=area['mapit_id']) for area in mapit_areas.values() ]
+#     else:
+#         areas = None
+#         
+#     return render_to_response(
+#         'search/location.html',
+#         {
+#             'loc': loc,
+#             'results': results,
+#             'areas': areas,
+#         },
+#         context_instance = RequestContext( request ),        
+#     )
+
+
+def autocomplete(request):
+    """Return autocomple JSON results"""
+    
+    term = request.GET.get('term','').strip()
+    response_data = []
+
+    if len(term):
+
+        # Does not work - probably because the FLAG_PARTIAL is not set on Xapian
+        # (trying to set it in settings.py as documented appears to have no effect)
+        # sqs = SearchQuerySet().autocomplete(name_auto=term)
+
+        # Split the search term up into little bits
+        terms = re.split(r'\s+', term)
+
+        # Build up a query based on the bits
+        sqs = SearchQuerySet()        
+        for bit in terms:
+            # print "Adding '%s' to the '%s' query" % (bit,term)
+            sqs = sqs.filter_and(
+                name_auto__startswith = sqs.query.clean( bit )
+            )
+
+        # collate the results into json for the autocomplete js
+        for result in sqs.all()[0:10]:
+            response_data.append({
+            	'url':   result.object.get_absolute_url(),
+            	'label': result.object.name,
+            })
+    
+    # send back the results as JSON
+    return HttpResponse(
+        simplejson.dumps(response_data),
+        mimetype='application/json'
+    )
+  
+
+def tagcloud(request):
+    """ Return tag cloud JSON results"""
+    # Build a query based on duration default is 1 month
+    cutoff = datetime.date.today() - datetime.timedelta(weeks=1)
+    sqs  = SearchQuerySet().models(hansard_models.Entry).filter(sitting__start_date>=cuttoff)
+
+    # Generate tag cloud from content of returned entries
+    ignore_terms =['and', 'of', 'the','is','to']
+    words = {}
+    for entry in sqs.all():
+        text = entry.object.content
+        for x in text.split():
+            if x.strip() in ignore_terms:
+                pass
+            words[x.strip()] = 1 + words.get(x.strip(), 0)
+
+
+    # return results
+    return HttpResponse(
+        simplejson.dumps(words),
+        mimetype='application/json'
+    }
+
+
