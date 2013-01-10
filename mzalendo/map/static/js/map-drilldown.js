f927565d85c142dfc0f18afad97aebff656dcd9f
Edmund von der Burg <evdb@mysociety.org>
2013-01-10 12:41:14 -0300

    Put a simple map on the page

---
+++ mzalendo/map/static/js/map-drilldown.js
@@ -0,0 +1,47 @@
+(function () {
+
+  function initialize_map() {
+
+    var map_element = document.getElementById("map-drilldown-canvas");
+    if (!map_element) return false;
+
+    // start with the default bounds for kenya
+    var map_bounds = {
+      north: 5, 
+      east:  44,
+      south: -5,
+      west:  33.5
+    };
+
+    var map_has_been_located = false;
+
+    var myOptions = {
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true,
+      maxZoom: 10
+    };
+
+    var map = new google.maps.Map(map_element, myOptions);
+
+    map.fitBounds( make_bounds( map_bounds ) );
+
+  }
+  
+  function make_bounds ( bounds ) {
+      var sw = new google.maps.LatLng( bounds.south, bounds.west );
+      var ne = new google.maps.LatLng( bounds.north, bounds.east );
+      return new google.maps.LatLngBounds( sw, ne );
+  }
+  
+  mzalendo_run_when_document_ready(
+      function () {
+          google.load(
+              'maps', '3',
+              {
+                  callback: initialize_map,
+                  other_params:'sensor=false'
+              }
+          );
+      }
+  );
+})();
