9bbc9b14d6eb7cdb40254c8b732702bc58f32db4
Edmund von der Burg <evdb@mysociety.org>
2013-01-10 11:35:35 -0300

    Hook up map urls to basic view and template

--- mzalendo/map/views.py
+++ mzalendo/map/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts  import render_to_response
+from django.template   import RequestContext
+
+def home(request):
+    """Homepage"""
+
+    return render_to_response(
+        'map/home.html',
+        {},
+        context_instance=RequestContext(request)
+    )
+
