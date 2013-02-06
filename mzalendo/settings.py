418d66964dda6db7742f0b8b7932f6bf5762fff1
Edmund von der Burg <evdb@mysociety.org>
2013-02-06 13:42:49 +0000

    Provide autocomplete with a value to use as the text in the input field. Closes #502

--- mzalendo/settings.py
+++ mzalendo/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 
 # integer which when updated causes the caches to fetch new content. See note in
 # 'base.html' for a better alternative in Django 1.4
-STATIC_GENERATION_NUMBER = 28
+STATIC_GENERATION_NUMBER = 29
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
