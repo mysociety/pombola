09640b660c38bcaf286b827c37dffb9f815e38c4
Edmund von der Burg <evdb@mysociety.org>
2013-02-06 16:45:30 +0000

    Load templates from the apps first, so that they can override filesystem ones.

--- mzalendo/settings.py
+++ mzalendo/settings.py
@@ -185,9 +185,9 @@ FILE_UPLOAD_HANDLERS = (
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    'django.template.loaders.filesystem.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 
