7fcf7153aaf05434fba8d70ea2d562ced7e22370
Edmund von der Burg <evdb@mysociety.org>
2013-02-08 16:58:37 +0000

    typo

--- mzalendo/votematch/admin.py
+++ mzalendo/votematch/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 import models 
 
 class QuizAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ["legal_name"]}
+    prepopulated_fields = {"slug": ["name"]}
 
 class StatementAdmin(admin.ModelAdmin):
     pass
