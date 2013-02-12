861a5fcda82fefbe10c844fda4075688dc6baf8e
Edmund von der Burg <evdb@mysociety.org>
2013-02-12 12:44:23 +0000

    Save submissions, and redirect user to the submission detail page after completing form.

--- mzalendo/votematch/views.py
+++ mzalendo/votematch/views.py
@@ -3,12 +3,39 @@ import models
 from django.shortcuts  import render_to_response, get_object_or_404, redirect
 from django.template   import RequestContext
 
+
 def quiz_detail (request, slug):
+
     quiz = get_object_or_404(
         models.Quiz,
         slug=slug
     )
     
+    # If this is a POST then extract all the answers
+    if request.method == 'POST':
+
+        # get the answers. Use the current set of statements to look for
+        # submitted values. Ignore anything that is not expected.
+        answers    = {}
+        statements = {}
+        for statement in quiz.statement_set.all():
+            statements[statement.id] = statement
+            val = request.POST.get( 'statement-' + str(statement.id) )
+            if len( val ): # ignore "" which is used for 'don't know' defaults
+                answers[statement.id] = int(val)
+                
+        if len(answers):
+            submission = models.Submission.objects.create(quiz=quiz)
+            
+            for statement_id, answer in answers.iteritems():
+                submission.answer_set.create(
+                    statement = statements[statement_id],
+                    agreement = answer
+                )
+
+            return redirect(submission)
+
+
     return render_to_response(
         'votematch/quiz_detail.html',
         {
