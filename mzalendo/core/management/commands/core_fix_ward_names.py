e692ea935713b21dbaefb8cf270831413b5f7bd2
Mark Longair <mhl@pobox.com>
2013-02-14 15:59:41 +0000

    Update ward names even if just the slug would have changed

--- mzalendo/core/management/commands/core_fix_ward_names.py
+++ mzalendo/core/management/commands/core_fix_ward_names.py
@@ -8,6 +8,9 @@ from optparse import make_option
 
 from core.models import PlaceKind, Place
 
+def slugify_place_name(place_name):
+    return 'ward-' + slugify(place_name)
+
 class Command(NoArgsCommand):
     help = 'Standardize the form of ward names with regard to / and - separators'
 
@@ -20,11 +23,12 @@ class Command(NoArgsCommand):
         for ward in Place.objects.filter(kind=PlaceKind.objects.get(slug='ward')):
             # print "ward is:", ward
             new_version = re.sub(r'(\w) *([/-]) *(\w)', '\\1 \\2 \\3', ward.name)
-            if new_version != ward.name:
+            new_version_slug = slugify_place_name(new_version)
+            if (new_version != ward.name) or (new_version_slug != ward.slug):
                 if options['commit']:
                     print "changing:", ward.name, "to", new_version
                     ward.name = new_version
-                    ward.slug = 'ward-' + slugify(ward.name)
+                    ward.slug = new_version_slug
                     ward.save()
                 else:
                     print "would change:", ward.name, "to", new_version, "if --commit were specified"
