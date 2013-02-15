f42acfb7458f17afc9945dfd4f9066aa11782607
Edmund von der Burg <evdb@mysociety.org>
2013-02-15 12:52:57 +0000

    Format analytics code to more clearly see what is going on.

--- mzalendo/core/static/js/analytics.js
+++ mzalendo/core/static/js/analytics.js
@@ -1,5 +1,8 @@
-// mathiasbynens.be/notes/async-analytics-snippet
-var _gaq=[["_setAccount",mzalendo_settings.google_analytics_account],["_trackPageview"]];
-(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-s.parentNode.insertBefore(g,s)}(document,"script"));
+ // mathiasbynens.be/notes/async-analytics-snippet
+var _gaq = [["_setAccount", mzalendo_settings.google_analytics_account], ["_trackPageview"]];
+(function(d, t) {
+  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  g.async = 1;
+  g.src = ("https:" == location.protocol ? "//ssl" : "//www") + ".google-analytics.com/ga.js";
+  s.parentNode.insertBefore(g, s)
+}(document, "script"));
