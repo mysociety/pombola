0bd743254c19f70fa1c70a315aa9b62a69c7766d
Edmund von der Burg <evdb@mysociety.org>
2013-07-29 18:02:42 +0200

    Script to create places matching mapit entries

---
+++ pombola/core/management/commands/core_create_places_from_mapit_entries.py
@@ -0,0 +1,43 @@
+# This script will copy areas from mapit to core.places, including creating the
+# place kind if required.
+
+
+# import re
+# import sys
+from django.core.management.base import LabelCommand
+
+from mapit.models import Type
+from core.models import Place, PlaceKind
+from django.template.defaultfilters import slugify
+
+class Command(LabelCommand):
+    help = 'Copy mapit.areas to core.places'
+    args = '<mapit.type.code>'
+
+    def handle_label(self,  mapit_type_code, **options):
+
+        # load the mapit type
+        mapit_type = Type.objects.get(code=mapit_type_code)
+
+        # if needed create the core placetype
+        placekind, created = PlaceKind.objects.get_or_create(
+            name=mapit_type.description,
+            defaults={
+                'slug': slugify(mapit_type.description)
+            }
+        )
+
+        # create all the places as needed for all mapit areas of that type
+        for area in mapit_type.areas.all():
+            print area.name
+            place, created = Place.objects.get_or_create(
+                name=area.name,
+                kind=placekind,
+                defaults={
+                    'slug': slugify(area.name),
+                }
+            )
+            
+            place.mapit_area = area
+            place.save()
+            
