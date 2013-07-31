2b1dbfad2dfbda670b158001a20599f3b0c57878
Ian <idesouza@hutspace.net>
2013-07-31 06:35:35 -0400

    refactor of news widget

--- mzalendo/odekro/management/aggregate_rss.py
+++ mzalendo/odekro/management/aggregate_rss.py
@@ -1,14 +1,14 @@
 # RSS parser to aggregate desired topics from 
 # varios rss feeds
-import csv
+mport csv
 import os
 import feedparser
-
+import dateutil
 
 BASEDIR = os.path.dirname(__file__)
 #read list os rss feeds from file
 FEEDS =[]
-reader= csv.reader(open('rssfeeds.csv','rU'))
+reader= csv.reader(open(os.path.join(BASEDIR,'rssfeeds.csv'),'rU'))
 for row in reader:
     if reader.line_num ==1:
         headers = row
@@ -25,9 +25,11 @@ def aggregate_articles():
         for article in f['entries']:
             if focused or 'parliament' in article.title.lower() or 'parliament' in article.description.lower():
                 feedhtml += '<li>'
-                feedhtml += '<h2>{0}</h3>'.format(article.title.encode('utf-8','ignore'))
-                feedhtml += '<p>Source:{0} Published:{1}</p>'.format(feed['source'],article.published.encode('utf-8','ignore'))
-                feedhtml += '<p>{0}</p>'.format(article.description.encode('utf-8','ignore'))
+                feedhtml += '<h3><a href="{0}">{1}</a></h3>'.format(article.link.encode('utf-8','ignore'), article.title.encode('utf-8','igno
+re'))
+                pubdate = dateutil.parser.parse(article.published.encode('utf-8','ignore')).date().strftime('%a %B %d, %Y')
+                feedhtml += '<span><a href="http://{0}">{0}</a> &nbsp;&nbsp;<b>Published:</b>&nbsp;{1}</span>'.format(feed['source'],pubdate)
+                feedhtml += '<div>{0}</div>'.format(article.description.encode('utf-8','ignore'))
                 feedhtml += '</li>'
     return feedhtml
 
