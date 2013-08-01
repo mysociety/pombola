b84260237d53ee630c5a424364b17c140f88ab7b
Edmund von der Burg <evdb@mysociety.org>
2013-08-01 11:37:04 +0200

    Change how the app removal migrations work to cope with South 0.8 (which catches the DatabaseError exceptions we used to catch)

--- pombola/core/migrations/0049_del_userprofile.py
+++ pombola/core/migrations/0049_del_userprofile.py
@@ -1,46 +1,8 @@
 # encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from south.models import MigrationHistory
-from django.db import models
-from django.db.utils import DatabaseError
-
-from django.contrib.contenttypes.models import ContentType
-
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-
-        # Do the deletes in a separate transaction, as database errors when
-        # deleting a table that does not exist would cause a transaction to be
-        # rolled back
-        db.start_transaction()
-
-        ContentType.objects.filter(app_label='user_profile').delete()
-
-        # Remove the entries from South's tables as we don't want to leave
-        # incorrect entries in there.
-        MigrationHistory.objects.filter(app_name='user_profile').delete()
-
-        # Commit the deletes to the various tables.
-        db.commit_transaction()
-
-        try:
-            db.delete_table('user_profile_userprofile')
-        except DatabaseError:
-            # table does not exist to delete, probably because the database was
-            # not created at a time when the user_profile app was still in use.
-            pass 
+from . import AppRemoveMigration
 
 
-    def backwards(self, orm):
+class Migration(AppRemoveMigration):
 
-        # There is no backwards - to create the user_profile tables again add the app
-        # back in and letting its migrations do the work.
-        pass
-
-
-    models = {}
-
-    complete_apps = ['user_profile']
+    app_name = 'user_profile'
+    tables = ['user_profile_userprofile']
--- pombola/core/migrations/0050_del_social_auth.py
+++ pombola/core/migrations/0050_del_social_auth.py
@@ -1,42 +1,12 @@
 # encoding: utf-8
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
-from django.db.utils import DatabaseError
-
-from django.contrib.contenttypes.models import ContentType
-
-class Migration(SchemaMigration):
-
-    def forwards(self, orm):
-
-        # delete entries in a separate transaction.
-        db.start_transaction()
-        ContentType.objects.filter(app_label='social_auth').delete()
-        db.commit_transaction()
-
-        tables_to_delete = [
-            'social_auth_association',
-            'social_auth_nonce',
-            'social_auth_usersocialauth',
-        ]
-
-        try:
-            for table in tables_to_delete:
-                db.delete_table(table)
-        except DatabaseError:
-            # table does not exist to delete, probably because the database was
-            # not created at a time when the user_profile app was still in use.
-            pass
-
-    def backwards(self, orm):
-
-        # There is no backwards - to create the social_auth tables again add the app
-        # back in and let its migrations do the work.
-        pass
+from . import AppRemoveMigration
 
 
-    models = {}
+class Migration(AppRemoveMigration):
 
-    complete_apps = ['social_auth']
+    app_name = 'social_auth'
+    tables = [
+        'social_auth_association',
+        'social_auth_nonce',
+        'social_auth_usersocialauth',
+    ]
