9fbdddf5e5dbcd9df50f4a10eb38301e213d7c08
Duncan Parkes <duncan@mysociety.org>
2013-08-08 10:06:29 +0100

    WIP commit as I want to change branches

--- pombola/south_africa/urls.py
+++ pombola/south_africa/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',
+urlpatterns = patterns('pombola.south_africa.views',
+    url( r'^place/latlon/(?P<lat>[0-9\.-]+),(?P<lon>[0-9\.-]+)/', 'latlon', name='latlon'),
     # There are no overridden urls for South Africa, yet....
 )
--- pombola/south_africa/views.py
+++ pombola/south_africa/views.py
@@ -1,1 +1,44 @@
-# Create your views here.
+from django.contrib.gis.geos import Point
+
+import mapit
+
+from pombola.core import models
+
+def latlon(request, lat, lon):
+    lat = float(lat)
+    lon = float(lon)
+    
+    location = Point(lon, lat)
+
+    areas = mapit.models.Area.objects.by_location(location)
+
+    # FIXME - Handle not finding a province or getting more than one.
+    province = models.Place.objects.get(mapit_area__in=areas, kind__slug='province')
+    
+    na_members = models.Person.objects.filter(
+        position__place=province,
+        position__organisation__slug='national-assembly',
+        )
+
+    ncop_members = models.Person.objects.filter(
+        position__place=province,
+        position__organisation__slug='ncop',
+        )
+
+    nearest_offices = models.Place.objects.filter(kind__slug='constituency-office').distance(location).order_by('distance')
+
+    # FIXME - Need to limit to active parties.
+    parties = models.Organisation.objects.filter(kind__slug='party')
+
+    office_lists = [
+        nearest_offices.filter(
+            organisation__org_rels_as_b__organisation_a=party,
+            organisation__org_rels_as_b__kind__name='has_office',
+            )
+        for party in parties
+        ]
+
+    offices = [x[0] if x else None for x in office_lists]
+
+    import pdb;pdb.set_trace()
+
