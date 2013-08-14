e8cb8ee9012098f2d6d19b166e7a549fe2b7551d
Ian <idesouza@hutspace.net>
2013-08-13 20:50:36 -0400

    refactor of rss parser plus and js/css placement

--- mzalendo/odekro/management/aggregate_rss.py
+++ mzalendo/odekro/management/aggregate_rss.py
@@ -1,6 +1,6 @@
 # RSS parser to aggregate desired topics from 
 # varios rss feeds
-mport csv
+import csv
 import os
 import feedparser
 import dateutil
@@ -25,8 +25,7 @@ def aggregate_articles():
         for article in f['entries']:
             if focused or 'parliament' in article.title.lower() or 'parliament' in article.description.lower():
                 feedhtml += '<li>'
-                feedhtml += '<h3><a href="{0}">{1}</a></h3>'.format(article.link.encode('utf-8','ignore'), article.title.encode('utf-8','igno
-re'))
+                feedhtml += '<h3><a href="{0}">{1}</a></h3>'.format(article.link.encode('utf-8','ignore'), article.title.encode('utf-8','ignore'))
                 pubdate = dateutil.parser.parse(article.published.encode('utf-8','ignore')).date().strftime('%a %B %d, %Y')
                 feedhtml += '<span><a href="http://{0}">{0}</a> &nbsp;&nbsp;<b>Published:</b>&nbsp;{1}</span>'.format(feed['source'],pubdate)
                 feedhtml += '<div>{0}</div>'.format(article.description.encode('utf-8','ignore'))
