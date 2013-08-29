50ead4fe13eec7ad9760f0f577212beb8e8a51be
Edmund von der Burg <evdb@mysociety.org>
2013-08-29 17:13:39 +0100

    Use a queryset to display only kind=page

--- pombola/info/views.py
+++ pombola/info/views.py
@@ -2,5 +2,6 @@ from django.views.generic import DetailView
 from models import InfoPage
 
 class InfoPageView(DetailView):
-    """Show the page, or 'index' if no slug"""
+    """Show the page for the given slug"""
     model = InfoPage
+    queryset = InfoPage.objects.filter(kind=InfoPage.KIND_PAGE)
