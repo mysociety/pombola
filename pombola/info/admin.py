88301c58dac4fc496f1626d8597cb5cfa12117b3
Edmund von der Burg <evdb@mysociety.org>
2013-08-30 11:28:09 +0100

    Adapt admin to suit the blog posts, round off some rough edges.

--- pombola/info/admin.py
+++ pombola/info/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 import models
 
 class InfoPageAdmin(admin.ModelAdmin):
-    pass
-    list_display  = [ 'slug', 'title' ]
+    search_fields = [ 'title', 'content' ]
+    list_display  = [ 'slug', 'title', 'kind' ]
+    list_filter   = [ 'kind' ]
+
+    fields = ('title', 'slug', 'publication_date', 'kind', 'content')
+    prepopulated_fields = {'slug': ['title']}
 
 
 admin.site.register( models.InfoPage, InfoPageAdmin )
