042beed12536e66b2089a8a461137fe546c8957a
Edmund von der Burg <evdb@mysociety.org>
2013-08-30 11:00:39 +0100

    Add RSS feed for the blog

--- pombola/info/urls/blog.py
+++ pombola/info/urls/blog.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url, handler404
 
-from ..views import InfoBlogView, InfoBlogList
+from ..views import InfoBlogView, InfoBlogList, InfoBlogFeed
 
 urlpatterns = patterns('',
-    url(r'^$',                  InfoBlogList.as_view(), { 'slug': 'index' } ),
+    url(r'^$',                  InfoBlogList.as_view(), name='info_blog_list' ),
+    url(r'^feed\.rss$',         InfoBlogFeed(),         name='info_blog_feed' ),
     url(r'^(?P<slug>[\w\-]+)$', InfoBlogView.as_view(), name='info_blog' ),
     url(r'^.*$',                handler404    ),
 )
--- pombola/info/views.py
+++ pombola/info/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import DetailView, ListView
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse_lazy
+
 from models import InfoPage
+
 
 class InfoBlogList(ListView):
     """Show list of blog posts"""
@@ -8,11 +12,28 @@ class InfoBlogList(ListView):
     paginate_by = 10
     template_name = 'info/blog_list.html'
 
+
 class InfoBlogView(DetailView):
     """Show the blog post for the given slug"""
     model = InfoPage
     queryset = InfoPage.objects.filter(kind=InfoPage.KIND_BLOG)
     template_name = 'info/blog_post.html'
+
+
+class InfoBlogFeed(Feed):
+    """Create a feed with the latest 10 blog entries in"""
+    title = "Recent blog posts"
+    link = reverse_lazy('info_blog_list')
+    description = "Recent blog posts"
+
+    def items(self):
+        return InfoPage.objects.filter(kind=InfoPage.KIND_BLOG).order_by("-publication_date")[:10]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
 
 
 class InfoPageView(DetailView):
