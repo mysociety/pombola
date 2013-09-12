584d1fe6a054acc3bfd593f62d2b3d8df662c9bc
Edmund von der Burg <evdb@mysociety.org>
2013-09-12 17:31:53 +0100

    Special case breadcrumb entries that look like coordinates.

--- pombola/core/tests/test_templatetags.py
+++ pombola/core/tests/test_templatetags.py
@@ -17,6 +17,10 @@ class BreadcrumbTest(TestCase):
             ( '/foo',     home_li + '<li>Foo</li>'),
             ( '/foo/bar', home_li + '<li><a href="foo/" title="Breadcrumb link to Foo">Foo</a>  <span class="sep">&raquo;</span> </li><li>Bar</li>'),
 
+            # Test that coordinates are passed through correctly
+            # (don't drop '-', put space after ',')
+            # See issue #762
+            ( '/-1.23,4.56', home_li + '<li>-1.23, 4.56</li>'),
         )
 
         for url, expected in tests:
