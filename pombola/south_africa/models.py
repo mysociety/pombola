b8ec2827495d24c5b2623ad432c55f53745bb903
Duncan Parkes <duncan@mysociety.org>
2013-09-12 10:10:02 +0100

    Use a proxy model for methods we'd like on a constituency office place.

--- pombola/south_africa/models.py
+++ pombola/south_africa/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from pombola.core.models import Place, Position
 
-# Create your models here.
+class ZAPlace(Place):
+    class Meta:
+        proxy = True
+
+    def postal_addresses(self):
+        return self.organisation.contacts.filter(kind__slug='address')
+
+    def related_positions(self):
+        return Position.objects.filter(organisation=self.organisation)
