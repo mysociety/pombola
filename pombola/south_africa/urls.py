6a2d3600f6022dcdb1d3c0cbdaf1ecb0cba2d881
Chris Mytton <self@hecticjeff.net>
2013-09-26 17:50:48 +0100

    Don't override existing urls
    This can only lead to confusion

--- pombola/south_africa/urls.py
+++ pombola/south_africa/urls.py
@@ -6,5 +6,4 @@ from pombola.south_africa.views import LatLonDetailView,SAPlaceDetailSub
 urlpatterns = patterns('pombola.south_africa.views',
     url(r'^place/latlon/(?P<lat>[0-9\.-]+),(?P<lon>[0-9\.-]+)/', LatLonDetailView.as_view(), name='latlon'),
     url(r'^place/(?P<slug>[-\w]+)/places/', SAPlaceDetailSub.as_view(), {'sub_page': 'places'}, name='place_places'),
-    url(r'^person/(?P<slug>[-\w]+)/$', PersonDetailSub.as_view(), { 'sub_page': 'experience' }, name='person'),
 )
