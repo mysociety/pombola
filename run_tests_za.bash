bf65bb27e29c00c2e5372bc72a84cbda2dbcf267
Edmund von der Burg <evdb@mysociety.org>
2013-10-01 12:50:19 +0100

    Run the zah tests

--- run_tests_za.bash
+++ run_tests_za.bash
@@ -13,7 +13,8 @@ find . -name '*.pyc' -delete
     images         \
     scorecards     \
     search         \
-    tasks
+    tasks          \
+    zah
 
 # This is a very ugly solution to running all the tests just for our own code.
 # running './manage.py test' will cause all the django etc tests to run as well
