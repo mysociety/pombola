c471d35de3ae060706c99b4b2749242a3f0a2966
Edmund von der Burg <evdb@mysociety.org>
2013-10-03 10:05:56 +0100

    Change zah to za_hansard

--- run_tests_za.bash
+++ run_tests_za.bash
@@ -14,7 +14,7 @@ find . -name '*.pyc' -delete
     scorecards     \
     search         \
     tasks          \
-    zah
+    za_hansard
 
 # This is a very ugly solution to running all the tests just for our own code.
 # running './manage.py test' will cause all the django etc tests to run as well
