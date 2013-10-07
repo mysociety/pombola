b2f6b83c3c1c1f2116ae95ba4541db35e678c654
Chris Mytton <self@hecticjeff.net>
2013-10-07 16:44:49 +0100

    Revert "Handle a '+' in organisation slugs"
    This problem goes deeper than just a single slug.
    
    This reverts commit 42d0edb5fcd71634dccf030cf3daa54e606de0f8.

--- pombola/south_africa/urls.py
+++ pombola/south_africa/urls.py
@@ -6,7 +6,7 @@ from pombola.core.urls import organisation_patterns
 # Override the organisation url so we can vary it depending on the organisation type.
 for index, pattern in enumerate(organisation_patterns):
     if pattern.name == 'organisation':
-        organisation_patterns[index] = url(r'^(?P<slug>[-\w+]+)/$', SAOrganisationDetailView.as_view(), name='organisation')
+        organisation_patterns[index] = url(r'^(?P<slug>[-\w]+)/$', SAOrganisationDetailView.as_view(), name='organisation')
 
 urlpatterns = patterns('pombola.south_africa.views',
     url(r'^place/latlon/(?P<lat>[0-9\.-]+),(?P<lon>[0-9\.-]+)/', LatLonDetailView.as_view(), name='latlon'),
