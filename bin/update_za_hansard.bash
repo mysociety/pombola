48c72724cff93f75e519eda99bdb2237cfb80f74
Edmund von der Burg <evdb@mysociety.org>
2013-10-10 07:53:50 +0100

    Add questions importer to za_hansard cron script

--- bin/update_za_hansard.bash
+++ bin/update_za_hansard.bash
@@ -9,3 +9,6 @@ cd `dirname $0`/..
 ./bin/run_management_command_capture_stdout hansard_check_for_new_sources
 ./bin/run_management_command_capture_stdout hansard_run_parsing
 ./bin/run_management_command_capture_stdout hansard_load_into_sayit
+
+# Run the ZA Hansard questions importer (all steps)
+./bin/run_management_command_capture_stdout questions --run-all-steps
