aa1d5cc7ec322df3eab10f4df521cadf416782b5
Chris Mytton <self@hecticjeff.net>
2013-10-17 18:49:19 +0100

    Exclude constituency offices from south africa place index
    This allows excluding an index in general.yml so that you can override
    it in the country app, however this seems sub-optimal, as it requires a
    specific config to be in place for the country app to work properly.
    Hopefully this can be fixed when #829 is tackled.

--- pombola/settings.py
+++ pombola/settings.py
@@ -335,6 +335,7 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': config.get('POMBOLA_DB_NAME'),
+        'EXCLUDED_INDEXES': config.get('HAYSTACK_EXCLUDED_INDEXES', []),
     },
 }
 HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
