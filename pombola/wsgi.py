9a897d6c80c42d47cb8d6eab13e26050970bb6b5
Edmund von der Burg <evdb@mysociety.org>
2013-10-25 11:56:04 +0100

    Use Spoke's WSGI scripts so that we get monitoring on the dev servers

--- pombola/wsgi.py
+++ pombola/wsgi.py
@@ -1,14 +1,34 @@
-#!/usr/bin/env python 
+"""
+WSGI config for Pombola project.
 
-import os, sys
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-file_dir = os.path.abspath(os.path.realpath(os.path.dirname(__file__)))
-sys.path.insert(
-    0, # insert at the very start
-    os.path.normpath(file_dir + "/..")
-)
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+"""
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pombola.settings')
+import os
+import sys
+import yaml
 
+config_path = os.path.abspath( os.path.join( os.path.dirname(__file__), '..', 'conf', 'general.yml' ) )
+config = yaml.load(open(config_path))
+
+if int(config.get('STAGING')) and sys.argv[1:2] != ['runserver']:
+    import pombola.wsgi_monitor
+    pombola.wsgi_monitor.start(interval=1.0)
+    pombola.wsgi_monitor.track(config_path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pombola.settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
