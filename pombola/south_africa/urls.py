f41b4fd5d3149c3863c6f846d554fdd3a538de69
Hakim Cassimally <hakim@mysociety.org>
2013-10-30 11:28:14 +0000

    Add committee views

--- pombola/south_africa/urls.py
+++ pombola/south_africa/urls.py
@@ -38,12 +38,20 @@ sayit_patterns = patterns('',
     url(r'^(?P<pk>\d+)$',        SectionView.as_view(), name='section-view'),
     url(r'^speech/(?P<pk>\d+)$', SpeechView.as_view(),  name='speech-view'),
 
-    # special Hansard index page that provides listing of the hansard sessions that contain speeches.
-    url(r'^$', SAHansardIndex.as_view(), name='section-list'),
-
     # Fake endpoint to redirect
     url(r'^speaker/(?P<pk>\d+)$', SASpeakerRedirectView.as_view(), name='speaker-view'),
+
+)
+
+hansard_patterns = sayit_patterns + patterns('',
+    # special Hansard index page that provides listing of the hansard sessions that contain speeches.
+    url(r'^$', SAHansardIndex.as_view(), name='section-list'),
+)
+committee_patterns = sayit_patterns + patterns('',
+    # TODO, this is wrong
+    url(r'^$', SAHansardIndex.as_view(), name='section-list'),
 )
 urlpatterns += patterns('',
-    url(r'^hansard/', include(sayit_patterns, namespace='hansard', app_name='speeches')),
+    url(r'^hansard/',   include(hansard_patterns,   namespace='hansard',   app_name='speeches')),
+    url(r'^committee/', include(committee_patterns, namespace='committee', app_name='speeches')),
 )
