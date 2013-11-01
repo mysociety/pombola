mysociety/pombola

commit 584d1fe6a054acc3bfd593f62d2b3d8df662c9bc

Special case breadcrumb entries that look like coordinates.

pombola/core/templatetags/breadcrumbs.py
@@ -50,6 +50,10 @@ def breadcrumbs(url):
                 bread.append(link)
                 if link in url_name_mappings:
                     (sub_link, this_url) = url_name_mappings[link]
+                if re.match(r'^[\d\-\.,]+$', link):
+                    # eg '-1.23,4.56'
+                    sub_link = link
+                    sub_link = re.sub(r',\s*', ', ', sub_link)
                 else:
                     sub_link = re.sub('[_\-]', ' ', link).title()
                     sub_link = re.sub('\\bFaq\\b', 'FAQ', sub_link)

pombola/core/tests/test_templatetags.py
@@ -17,6 +17,10 @@ class BreadcrumbTest(TestCase):
             ( '/foo',     home_li + '<li>Foo</li>'),
             ( '/foo/bar', home_li + '<li><a href="foo/" title="Breadcrumb link to Foo">Foo</a>  <span class="sep">&raquo;</span> </li><li>Bar</li>'),
 
+            # Test that coordinates are passed through correctly
+            # (don't drop '-', put space after ',')
+            # See issue #762
+            ( '/-1.23,4.56', home_li + '<li>-1.23, 4.56</li>'),
         )
 
         for url, expected in tests:

commit 7ffcbe48b2d6d2d669afdb1a5f00462c993c87fa

Fix bug with generating breadcrumbs
There was a subtle bug introduced in 584d1fe6 meaning that the test for
co-ordinates was always being run, even if a url name mapping had been
found, which meant if the url didn't match a co-ordinate it was always
falling back to the default in the else statement.

Part of #801

pombola/core/templatetags/breadcrumbs.py
@@ -50,7 +50,7 @@ def breadcrumbs(url):
                 bread.append(link)
                 if link in url_name_mappings:
                     (sub_link, this_url) = url_name_mappings[link]
-                if re.match(r'^[\d\-\.,]+$', link):
+                elif re.match(r'^[\d\-\.,]+$', link):
                     # eg '-1.23,4.56'
                     sub_link = link
                     sub_link = re.sub(r',\s*', ', ', sub_link)

pombola/core/tests/test_templatetags.py
@@ -1,7 +1,7 @@
 
 from django.test import TestCase
 
-from ..templatetags.breadcrumbs import breadcrumbs
+from ..templatetags.breadcrumbs import breadcrumbs, url_name_mappings
 from ..templatetags.active_class import active_class
 
 
@@ -28,6 +28,13 @@ class BreadcrumbTest(TestCase):
         for url, expected in tests:
             actual = breadcrumbs(url)
             self.assertEqual(actual, expected)
+
+    def test_breadcrumb_url_name_mappings(self):
+        for name, title_url in url_name_mappings.iteritems():
+            title, url = title_url
+            actual = breadcrumbs(name + '/foo')
+            self.assertTrue(title in actual, "Expected {0} to be in {1}".format(title, actual))
+            self.assertTrue(url in actual, "Expected {0} to be in {1}".format(url, actual))
 
 
 class ActiveClassTest(TestCase):