cda6aa87f76a553802c8f6c31759bfeb1bd8f7ca
Edmund von der Burg <evdb@mysociety.org>
2013-11-18 12:35:47 +0000

    Update geocoder tests to match changing Google geocoder results.

--- pombola/search/tests/geocoder.py
+++ pombola/search/tests/geocoder.py
@@ -30,7 +30,7 @@ class GeocoderTests(unittest.TestCase):
         # These are well known results that should be in those returned
         expected_results = [
             {'latitude': -26.195, 'longitude': 28.0,   'address': u'High Street, Johannesburg 2092, South Africa'},
-            {'latitude': -25.786, 'longitude': 28.261, 'address': u'High Street, Pretoria, South Africa'},
+            {'latitude': -33.642, 'longitude': 19.449, 'address': u'High Street, Worcester 6849, South Africa'},
         ]
         for expected in expected_results:
             self.assertTrue(expected in results)
