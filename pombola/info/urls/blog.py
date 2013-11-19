d616198a45cde6e299f8df03802e20bf555d9f5c
Edmund von der Burg <evdb@mysociety.org>
2013-11-19 16:25:29 +0000

    whitespace

--- pombola/info/urls/blog.py
+++ pombola/info/urls/blog.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url, handler404
 from ..views import InfoBlogView, InfoBlogList, InfoBlogFeed
 
 urlpatterns = patterns('',
-    url(r'^$',                  InfoBlogList.as_view(), name='info_blog_list' ),
-    url(r'^feed\.rss$',         InfoBlogFeed(),         name='info_blog_feed' ),
-    url(r'^(?P<slug>[\w\-]+)$', InfoBlogView.as_view(), name='info_blog' ),
-    url(r'^.*$',                handler404    ),
+    url(r'^$', InfoBlogList.as_view(), name='info_blog_list'),
+    url(r'^feed\.rss$', InfoBlogFeed(), name='info_blog_feed'),
+    url(r'^(?P<slug>[\w\-]+)$', InfoBlogView.as_view(), name='info_blog'),
+    url(r'^.*$', handler404),
 )
