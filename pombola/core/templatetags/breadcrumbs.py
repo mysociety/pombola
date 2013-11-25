cc1bd01c8176a1c548a792bd7824f3a27e8dd653
Hakim Cassimally <hakim@mysociety.org>
2013-11-25 15:55:23 +0000

    Remove /*/is/* fragment from breadcrumbs
    Requirement is for South-Africa #1046 but as /*/is/ doesn't resolve to anything
    useful, this is hopefully a useful fix for all instances.

--- pombola/core/templatetags/breadcrumbs.py
+++ pombola/core/templatetags/breadcrumbs.py
@@ -28,9 +28,16 @@ def breadcrumbs(url):
     if total == 0 and links[0] == "":
         bcrumb = '<li>Home</li>'
     else:
+
+        if links[1] == 'is': 
+          # (Organisation|Place|etc.)Kind links like /organization/is/house/
+          # (drop it)
+          links[1:2] = []
+          total -= 1     
         if links[total] == 'all': # if links ends with 'all', drop it
           links = links[0:total]
           total -= 1     
+
         home = ['<li><a href="/" title="Breadcrumb link to the homepage.">Home</a> %s </li>' % separator]
 
         seen_links = {}
