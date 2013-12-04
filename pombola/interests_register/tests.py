d4c487f41f08242ac1da238f9843f275d168c161
Edmund von der Burg <evdb@mysociety.org>
2013-12-04 09:09:16 +0000

    Add tests for current slug behaviour

--- pombola/interests_register/tests.py
+++ pombola/interests_register/tests.py
@@ -6,11 +6,13 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from .models import Category, Release
 
+class InterestsRegisterModelTests(TestCase):
+    def test_category_creates_own_slug(self):
+        cat = Category.objects.create(name="Foo Bar", sort_order=1)
+        self.assertEqual(cat.slug, 'foo-bar')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_release_creates_own_slug(self):
+        rel = Release.objects.create(name="Foo Bar", date="2013-12-04")
+        self.assertEqual(rel.slug, 'foo-bar')
