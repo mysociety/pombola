cf88c783a15c10ae381f32fc4e72b2c8bbb75d8e
Edmund von der Burg <evdb@mysociety.org>
2013-12-05 17:45:24 +0000

    Create ‘slide after’ view

--- pombola/spinner/views.py
+++ pombola/spinner/views.py
@@ -1,1 +1,35 @@
-# Create your views here.
+from django.views.generic import DetailView
+from django.http import Http404
+
+from .models import Slide
+
+class SlideAfterView(DetailView):
+
+    model = Slide
+    template_name = 'spinner/display.html'
+    context_object_name = 'slide'
+
+    def get_object(self):
+        last_slide_id = self.request.GET.get('id', None)
+        last_slide = None
+
+        if last_slide_id:
+            try:
+                last_slide = Slide.objects.get(pk=last_slide_id)
+            except Slide.DoesNotExist:
+                print 'not found'
+                pass
+
+        print last_slide_id
+        print last_slide
+        if last_slide:
+            print last_slide.id
+
+        next_slide = Slide.objects.slide_after(last_slide)
+
+        if not next_slide:
+            raise Http404()
+
+        return next_slide
+
+
