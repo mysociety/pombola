72476a060b14a236de1f4892e8597a0016862dec
Edmund von der Burg <evdb@mysociety.org>
2013-12-16 16:01:22 +0000

    Add wards, tidy up reporting

--- pombola/nigeria/data/add_polling_units_to_mapit.py
+++ pombola/nigeria/data/add_polling_units_to_mapit.py
@@ -15,21 +15,24 @@ sys.path.append(
 )
 
 from mapit import models
+from django.core.exceptions import ObjectDoesNotExist
 
 class PollUnitImporter(object):
 
     cached_states = {}
     cached_lgas = {}
+    cached_wards = {}
+
     cached_poll_unit_code_type = None
+    cached_ward_type = None
+    cached_nigeria = None
+    cached_current_generation = None
 
     def process(self, filename):
         print "Looking at '{0}'".format(filename)
 
-        count = 0
         for row in self.get_rows(filename):
             self.process_row(row)
-            count += 1
-            if count > 20: break
 
 
     def get_rows(self, filename):
@@ -39,9 +42,15 @@ class PollUnitImporter(object):
 
 
     def process_row(self, row):
-        print row
-        state = self.process_state_for_row(row)
-        lga   = self.process_lga_for_row(row, state=state)
+
+        # print row
+
+        try:
+            state = self.process_state_for_row(row)
+            lga   = self.process_lga_for_row(row, state=state)
+            ward  = self.process_ward_for_row(row, lga=lga)
+        except ObjectDoesNotExist:
+            sys.stderr.write(str(row) + "\n")
 
 
     def process_state_for_row(self, row):
@@ -50,6 +59,8 @@ class PollUnitImporter(object):
 
         if code in self.cached_states:
             return self.cached_states[code]
+
+        print "Loading %s" % name
 
         # Should find all of these
         state = models.Area.objects.get(name__iexact=name, type__code='STA')
@@ -67,10 +78,11 @@ class PollUnitImporter(object):
     def process_lga_for_row(self, row, state):
         name = row['LGA NAME']
         code = self.tidy_up_code(row['LGA CODE'], state)
-        print code
 
         if code in self.cached_lgas:
             return self.cached_lgas[code]
+
+        print "  Loading %s" % name
 
         # Should find all of these
         lga = models.Area.objects.get(name__iexact=name, type__code='LGA')
@@ -83,6 +95,37 @@ class PollUnitImporter(object):
 
         self.cached_lgas[code] = lga
         return self.cached_lgas[code]
+
+
+    def process_ward_for_row(self, row, lga):
+        name = row['WARD NAME']
+        code = self.tidy_up_code(row['WARD CODE'], lga)
+
+        if code in self.cached_wards:
+            return self.cached_wards[code]
+
+        print "    Loading %s" % name
+
+        # Create if needed, not expected to exist already
+        ward, created = models.Area.objects.get_or_create(
+            name=name,
+            type=self.ward_type,
+            parent_area=lga,
+            country=self.nigeria,
+            defaults={
+                "generation_low": self.current_generation,
+                "generation_high": self.current_generation,
+            }
+        )
+
+        # Check that the code has been added
+        ward.codes.get_or_create(
+            type=self.poll_unit_code_type,
+            code=code,
+        )
+
+        self.cached_wards[code] = ward
+        return self.cached_wards[code]
 
 
     def tidy_up_code(self, code, parent=None):
@@ -108,6 +151,40 @@ class PollUnitImporter(object):
             )
 
         return self.cached_poll_unit_code_type
+
+
+    @property
+    def nigeria(self):
+        if not self.cached_nigeria:
+            self.cached_nigeria = models.Country.objects.get(
+                name="Nigeria",
+            )
+
+        return self.cached_nigeria
+
+
+    @property
+    def ward_type(self):
+        if not self.cached_ward_type:
+            self.cached_ward_type, created = models.Type.objects.get_or_create(
+                code="WRD",
+                defaults={
+                    "description": "Ward"
+                }
+            )
+
+        return self.cached_ward_type
+
+
+
+    @property
+    def current_generation(self):
+        if not self.cached_current_generation:
+            self.cached_current_generation = models.Generation.objects.get(
+                active=True
+            )
+
+        return self.cached_current_generation
 
 
 
