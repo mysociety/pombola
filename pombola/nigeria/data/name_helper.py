8bdff9c3a58bdd27c739ebf294bd324b84ae66ea
Edmund von der Burg <evdb@mysociety.org>
2013-12-18 10:36:58 +0000

    reduce duplication and ensure name entry is created for each lga area matched to a DUN entry

--- pombola/nigeria/data/name_helper.py
+++ pombola/nigeria/data/name_helper.py
@@ -62,7 +62,7 @@ class LgaNameChecker(object):
             if state_name in seen_states: continue
 
             try:
-                models.Area.objects.get(names__name__iexact=state_name, type__code='STA')
+                self.get_area(names__name__iexact=state_name, type__code='STA')
             except ObjectDoesNotExist:
                 raise Exception("Could not find area matching state '%s'" % state_name)
 
@@ -94,21 +94,20 @@ class LgaNameChecker(object):
             # If it is a potential duplicate check the state is correct as well.
             if lga_name in self.duplicate_lga_names:
                 try:
-                    lga_area = models.Area.objects.get(
+                    lga_area = self.get_area(
                         names__name__iexact=lga_name,
                         type__code='LGA',
                         parent_area__type__code='STA',
                         parent_area__names__name__iexact=state_name,
                     )
-                    self.matched.add(entry)
-                    self.matched_area_ids.add(lga_area.id)
+                    self.match_up_area(lga_name=lga_name, state_name=state_name, lga_area=lga_area)
                 except ObjectDoesNotExist:
                     print "Duplicate: LGA {0} in state {1}".format(lga_name, state_name)
                     self.duplicates.add(entry)
                 continue
 
             try:
-                lga_area = models.Area.objects.get(names__name__iexact=lga_name, type__code='LGA')
+                lga_area = self.get_area(names__name__iexact=lga_name, type__code='LGA')
             except ObjectDoesNotExist:
                 lga_area = None
 
@@ -118,11 +117,30 @@ class LgaNameChecker(object):
 
             if lga_area:
                 # print "Hit  %s" % str(entry)
-                self.matched.add(entry)
-                self.matched_area_ids.add(lga_area.id)
+                self.match_up_area(lga_name=lga_name, state_name=state_name, lga_area=lga_area)
             else:
                 # print "Miss %s" % str(entry)
                 self.unmatched.add(entry)
+
+    def get_area(self, **kwargs):
+        all = models.Area.objects.filter(**kwargs).distinct('id').order_by('id')
+        if all.count() == 0:
+            raise ObjectDoesNotExist()
+        elif all.count() > 1:
+            raise Exception("Multiple matches")
+        else:
+            return all[0]
+
+
+    def match_up_area(self, lga_name, state_name, lga_area):
+        self.matched.add((lga_name, state_name))
+        self.matched_area_ids.add(lga_area.id)
+
+        # TODO
+        # * if no state check that geometry are inside state_name
+        # * set the state
+
+        models.Name.objects.get_or_create(area=lga_area, type=self.name_type, name=lga_name)
 
 
     def set_name_for_unmatched(self):
@@ -149,11 +167,11 @@ class LgaNameChecker(object):
                 # get the first bit of the word and match on that, should narrow field enough for a replacement to be created.
                 match_text = lga_name.split(' ')[0][:match_length]
 
-                self.match_lga_to_area(lga_name=lga_name, match_text=match_text)
+                self.match_lga_to_area(lga_name=lga_name, match_text=match_text, state_name=state_name)
 
 
 
-    def match_lga_to_area(self, match_text, lga_name):
+    def match_lga_to_area(self, match_text, lga_name, state_name):
 
         all_possibles = list(models.Area.objects.filter(names__name__icontains=match_text, type__code='LGA').order_by('name'))
         possibles = [x for x in all_possibles if x.id not in self.matched_area_ids]
@@ -171,8 +189,7 @@ class LgaNameChecker(object):
 
         if counter_chosen:
             choice = possibles[int(counter_chosen) - 1]
-            choice.names.create(type=self.name_type, name=lga_name)
-            self.matched_area_ids.add(choice.id)
+            self.match_up_area(lga_name=lga_name, state_name=state_name, lga_area=choice)
 
     def list_unmatched(self):
         self.partition_lgas()
