bb3c44b6c578e32a06dbea80f45f4242883580fb
Edmund von der Burg <evdb@mysociety.org>
2013-12-19 17:10:37 +0000

    Filter out matches that are just tiny boundary overlaps

--- pombola/nigeria/views.py
+++ pombola/nigeria/views.py
@@ -54,14 +54,46 @@ class SearchPollUnitNumberView(TemplateView):
                 area_polygons = area_for_polygons.polygons.collect()
 
                 # get the overlapping senatorial districts
-                senatorial_district_areas = Area.objects.filter(type__code="SEN", polygons__polygon__intersects=area_polygons)
-                context['senatorial_districts'] = self.convert_areas_to_places(senatorial_district_areas)
-
-                federal_constituency_areas = Area.objects.filter(type__code="FED", polygons__polygon__intersects=area_polygons)
-                context['federal_constutencies'] = self.convert_areas_to_places(federal_constituency_areas)
+                context['senatorial_districts']  = self.find_matching_places("SEN", area_polygons)
+                context['federal_constutencies'] = self.find_matching_places("FED", area_polygons)
 
         return context
 
+
+    def find_matching_places(self, code, polygons):
+
+        all_areas = Area.objects.filter(type__code=code, polygons__polygon__intersects=polygons).distinct()
+
+        size_of_overlap = {}
+
+        # calculate the overlap
+        for area in all_areas:
+            area_polygons = area.polygons.collect()
+            intersection = polygons.intersection(area_polygons)
+
+            # choose the smallest area
+            smallest_area = min(polygons.area, area_polygons.area)
+
+            intersection_fraction_of_smallest_area = intersection.area / smallest_area
+            size_of_overlap[area] = intersection_fraction_of_smallest_area
+
+        # Sort the results by the overlap size
+        all_areas = sorted(
+            all_areas,
+            cmp=lambda x,y: cmp(size_of_overlap[x], size_of_overlap[y])
+        )
+
+        # get the most overlapping ones
+        likely_areas = filter(
+            lambda x: size_of_overlap[x] > 0.5,
+            all_areas
+        )
+
+        # If there are none display first five (better than nothing...)
+        if not len(likely_areas):
+            likely_areas = all_areas[:5]
+
+        return self.convert_areas_to_places(likely_areas)
 
     def convert_areas_to_places(self, areas):
         places = []
