058b9990b3f9e9a980b9734815d4ed0f586240e6
Duncan Parkes <duncan@mysociety.org>
2014-01-07 12:26:05 +0000

    Help latest pip and old setuptools to work together.
    The latest version of pip doesn't play nicely with the ancient
    version of setuptools which comes with Debian - it requires
    setuptools 0.8, and we only have 0.6.
    
    http://stackoverflow.com/questions/20905350/latest-pip-fails-with-requires-setuptools-0-8-for-dist-info
    
    In order to fix this, we use a script get_pip.bash from commonlib
    to securely install pip and then update to a nice new setuptools.
    
    It looks like this is going to be fixed in version 1.5.1 of pip:
    https://github.com/pypa/pip/issues/1422

--- bin/prepare_environment.bash
+++ bin/prepare_environment.bash
@@ -20,12 +20,22 @@ if [ "$(echo -e '1.7\n'$virtualenv_version | sort -V | head -1)" = '1.7' ]; then
     virtualenv_args="--system-site-packages"
 fi
 
+virtualenv_dir='../pombola-virtualenv'
+virtualenv_activate="$virtualenv_dir/bin/activate"
+
 # create the virtual environment, install/update required packages
-virtualenv $virtualenv_args ../pombola-virtualenv
-source ../pombola-virtualenv/bin/activate
+if [ ! -f "$virtualenv_activate" ]
+then
+    virtualenv $virtualenv_args $virtualenv_dir
+fi
+
+source $virtualenv_activate
 
 # Upgrade pip to a secure version
-curl -s https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
+# curl -s https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
+# Revert to the line above once we can get a newer setuptools from Debian, or
+# pip ceases to need such a recent one.
+curl -s https://raw.github.com/mysociety/commonlib/master/bin/get_pip.bash | bash
 
 # Install all the packages
 pip install Mercurial
