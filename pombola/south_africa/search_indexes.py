6d81831ed87ab421fd97f158e150fe45b647c090
Mark Longair <mhl@pobox.com>
2014-02-10 16:07:28 +0000

    Don't import Places that are Wards
    These are just municipalities with a number afterwards, so just
    clutter the search results.

--- pombola/south_africa/search_indexes.py
+++ pombola/south_africa/search_indexes.py
@@ -3,4 +3,5 @@ from pombola.search import search_indexes
 class SAPlaceIndex(search_indexes.PlaceIndex):
 
     def index_queryset(self, **kwargs):
-        return self.get_model().objects.exclude(kind__slug='constituency-office')
+        return self.get_model().objects. \
+            exclude(kind__slug__in=('constituency-office', 'wards'))
