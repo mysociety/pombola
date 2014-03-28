123138310f698481941fb0bf0dac1718289d4906
Mark Longair <mhl@pobox.com>
2014-03-28 11:10:46 +0000

    Return early in the case of an empty path
    This reduces the indentation of the rest of the code and gets that
    case out of the way.

--- pombola/core/templatetags/breadcrumbs.py
+++ pombola/core/templatetags/breadcrumbs.py
@@ -25,55 +25,54 @@ def slash_stripped_path_from_url(url):
 @register.filter
 def breadcrumbs(url):
     bare_url = slash_stripped_path_from_url(url)
+    # If that's empty, immediately return just the home link:
+    if not bare_url:
+        return '<li>Home</li>'
     if bare_url.startswith(hansard_part):
         bare_url = hansard_part + bare_url[len(hansard_part):].replace('/',' : ')
     links = bare_url.split('/')
     bread = []
     total = len(links)-1
-    if total == 0 and links[0] == "":
-        bcrumb = '<li>Home</li>'
-    else:
-        if total > 1 and links[1] == 'is':
-          # (Organisation|Place|etc.)Kind links like /organization/is/house/
-          # (drop it)
-          links[1:2] = []
-          total -= 1
-        if links[total] == 'all': # if links ends with 'all', drop it
-          links = links[0:total]
-          total -= 1
+    if total > 1 and links[1] == 'is':
+      # (Organisation|Place|etc.)Kind links like /organization/is/house/
+      # (drop it)
+      links[1:2] = []
+      total -= 1
+    if links[total] == 'all': # if links ends with 'all', drop it
+      links = links[0:total]
+      total -= 1
 
-        home = ['<li><a href="/" title="Breadcrumb link to the homepage.">Home</a> %s </li>' % separator]
+    home = ['<li><a href="/" title="Breadcrumb link to the homepage.">Home</a> %s </li>' % separator]
 
-        seen_links = {}
+    seen_links = {}
 
-        for i, link in enumerate(links):
+    for i, link in enumerate(links):
 
-            if seen_links.get(link):
-                continue
+        if seen_links.get(link):
+            continue
+        else:
+            seen_links[link] = True
+
+        if not link == '':
+            bread.append(link)
+            if link in url_name_mappings:
+                (sub_link, this_url) = url_name_mappings[link]
+            elif re.match(r'^[\d\-\.,]+$', link):
+                # eg '-1.23,4.56'
+                sub_link = link
+                sub_link = re.sub(r',\s*', ', ', sub_link)
             else:
-                seen_links[link] = True
+                sub_link = re.sub('[_\-]', ' ', link).title()
+                sub_link = re.sub('\\bFaq\\b', 'FAQ', sub_link)
+                this_url = "/{0}/".format("/".join(bread))
+            if not i == total:
+                try:
+                    resolve(this_url)
+                    tlink = '<li><a href="%s" title="Breadcrumb link to %s">%s</a> %s</li>' % (this_url, sub_link, sub_link, separator)
+                except Resolver404:
+                    tlink = '<li>%s %s</li>' % (sub_link, separator)
 
-            if not link == '':
-                bread.append(link)
-                if link in url_name_mappings:
-                    (sub_link, this_url) = url_name_mappings[link]
-                elif re.match(r'^[\d\-\.,]+$', link):
-                    # eg '-1.23,4.56'
-                    sub_link = link
-                    sub_link = re.sub(r',\s*', ', ', sub_link)
-                else:
-                    sub_link = re.sub('[_\-]', ' ', link).title()
-                    sub_link = re.sub('\\bFaq\\b', 'FAQ', sub_link)
-                    this_url = "/{0}/".format("/".join(bread))
-                if not i == total:
-                    try:
-                        resolve(this_url)
-                        tlink = '<li><a href="%s" title="Breadcrumb link to %s">%s</a> %s</li>' % (this_url, sub_link, sub_link, separator)
-                    except Resolver404:
-                        tlink = '<li>%s %s</li>' % (sub_link, separator)
-
-                else:
-                    tlink = '<li>%s</li>' % sub_link
-                home.append(tlink)
-        bcrumb = "".join(home)
-    return mark_safe(bcrumb)
+            else:
+                tlink = '<li>%s</li>' % sub_link
+            home.append(tlink)
+    return mark_safe("".join(home))
