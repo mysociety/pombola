7dc5d20797e1e49ac035f1e1c7d8048379a27dd9
Zarino Zappia <mail@zarino.co.uk>
2014-04-01 13:13:41 +0100

    A initial static version of the MIT county performance page
    This is a squash of 5 commits by Zarino with the following commit
    messages:
    
      * first iteration of coding up the illustrator mockup
      * facebook and twitter icons on country-performance buttons
      * red table column
      * fix country/county typo
      * fix mobile h2 style overrides on country-performance page

--- pombola/kenya/tests.py
+++ pombola/kenya/tests.py
@@ -10,3 +10,5 @@ class CountyPerformancePageTests(WebTest):
     def test_page_view(self):
         response = self.app.get('/county-performance')
         self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.html.get('a', {'id': 'share-facebook'}))
+        self.assertTrue(response.html.get('a', {'id': 'share-twitter'}))
