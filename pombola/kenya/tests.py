f6f08e060c1d82174f949065f009739543362538
Mark Longair <mhl@pobox.com>
2014-04-01 10:43:54 +0100

    Add a skeleton for the MIT "county performance" page

---
+++ pombola/kenya/tests.py
@@ -0,0 +1,12 @@
+from django.core.urlresolvers import reverse
+
+from django_webtest import WebTest
+
+from nose.plugins.attrib import attr
+
+@attr(country='kenya')
+class CountyPerformancePageTests(WebTest):
+
+    def test_page_view(self):
+        response = self.app.get('/county-performance')
+        self.assertEqual(response.status_code, 200)
