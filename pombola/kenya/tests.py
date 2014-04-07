6c6324e428537cbda5eba3dabfe2601d9f4c7556
Mark Longair <mhl@pobox.com>
2014-04-07 16:28:49 +0100

    MIT: Present "Opportunity", "Threat" or neither depending on variant

--- pombola/kenya/tests.py
+++ pombola/kenya/tests.py
@@ -14,3 +14,27 @@ class CountyPerformancePageTests(WebTest):
         self.assertTrue(response.html.get('a', {'id': 'share-twitter'}))
         self.assertFalse(
             response.html.findAll('div', {'id': 'surveyPromoBanner'}))
+
+    def test_threat_variant(self):
+        response = self.app.get('/county-performance?variant=t')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            1, len(response.html.findAll('div', {'id': 'threat'})))
+        self.assertFalse(
+            response.html.findAll('div', {'id': 'opportunity'}))
+
+    def test_threat_opportunity(self):
+        response = self.app.get('/county-performance?variant=o')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            1, len(response.html.findAll('div', {'id': 'opportunity'})))
+        self.assertFalse(
+            response.html.findAll('div', {'id': 'threat'}))
+
+    def test_threat_neither(self):
+        response = self.app.get('/county-performance?variant=n')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(
+            response.html.findAll('div', {'id': 'opportunity'}))
+        self.assertFalse(
+            response.html.findAll('div', {'id': 'threat'}))
