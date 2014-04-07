a278f03c2791d95b7b812471e4cec161de680e6b
Mark Longair <mhl@pobox.com>
2014-04-07 16:09:29 +0100

    Add the "comments for the senate" form

--- pombola/kenya/forms.py
+++ pombola/kenya/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.widgets import Textarea
 
 class CountyPerformancePetitionForm(forms.Form):
     name = forms.CharField(
@@ -9,3 +10,12 @@ class CountyPerformancePetitionForm(forms.Form):
         widget=forms.TextInput(
             attrs={
                 'placeholder': 'Your email address'}))
+
+class CountyPerformanceSenateForm(forms.Form):
+    comments = forms.CharField(
+        label='Tell the senate what you think',
+        widget=Textarea(
+            attrs={
+                'rows': 5,
+                'cols': 60,
+                'placeholder': 'Add any comments here'}))
