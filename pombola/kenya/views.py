06685200083bdcb4a0bdf4f987191d13d2532de5
Mark Longair <mhl@pobox.com>
2014-04-07 16:28:49 +0100

    Present "Opportunity", "Threat" or neither depending on variant

--- pombola/kenya/views.py
+++ pombola/kenya/views.py
@@ -3,7 +3,10 @@ from django.views.generic.base import TemplateView
 from .forms import CountyPerformancePetitionForm, CountyPerformanceSenateForm
 
 class CountyPerformanceView(TemplateView):
-    """This view displays a page about county performance with calls to action"""
+    """This view displays a page about county performance with calls to action
+
+    There are three major variants of the page that include different
+    information."""
 
     template_name = 'county-performance.html'
 
@@ -12,4 +15,12 @@ class CountyPerformanceView(TemplateView):
         context['suppress_banner'] = True
         context['petition_form'] = CountyPerformancePetitionForm()
         context['senate_form'] = CountyPerformanceSenateForm()
+
+        context['show_opportunity'], context['show_threat'] = {
+            'o': (True, False),
+            't': (False, True),
+            'n': (False, False),
+            None: (False, False),
+        }[self.request.GET.get('variant')]
+
         return context
