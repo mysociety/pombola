c9d39b0d7d31265a0519ac442f8200d4d4abc753
Mark Longair <mhl@pobox.com>
2014-04-15 19:22:51 +0100

    Deal with submissions to either form on the county performance page
    Paul has said that we only need to create entries using the existing
    feedback mechanism; so this just adds submissions for petition
    signatures or comments for the senate to the feedback for these
    pages respectively:
    
      /county-performance/petition
      /county-performance/senate
    
    ... marked as non-actionable.
    
    There is some juggling of class-based views required to work with
    two forms on a single page, and so that if the form don't pass
    validation, the user is redirected to a reduced version of the
    page with just the form they were trying to fill.
    
    Note that this doesn't confirm that the supplied email address
    actually works (as with the existing feedback mechanism) - for a
    serious petitions system one would want to make sure the email
    address entered is usable.

--- pombola/kenya/urls.py
+++ pombola/kenya/urls.py
@@ -1,11 +1,25 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
 
-from .views import CountyPerformanceView
+from .views import (CountyPerformanceView, CountyPerformanceSenateSubmission,
+    CountyPerformancePetitionSubmission)
 
 urlpatterns = patterns('',
     url(r'^intro$',                TemplateView.as_view(template_name='intro.html') ),
     url(r'^register-to-vote$',     TemplateView.as_view(template_name='register-to-vote.html') ),
     url(r'^find-polling-station$', TemplateView.as_view(template_name='find-polling-station.html') ),
-    url(r'^county-performance', CountyPerformanceView.as_view()),
+    url(r'^county-performance$', CountyPerformanceView.as_view(), name='county-performance'),
 )
+
+for name, view in (
+        ('senate', CountyPerformanceSenateSubmission),
+        ('petition', CountyPerformancePetitionSubmission)):
+
+    urlpatterns += (
+        url(r'^county-performance/{0}$'.format(name),
+            view.as_view(),
+            name='county-performance-{0}-submission'.format(name)),
+        url(r'^county-performance/{0}/thanks$'.format(name),
+            TemplateView.as_view(
+                template_name='county-performance-{0}-submission.html'.format(name))),
+    )
--- pombola/kenya/views.py
+++ pombola/kenya/views.py
@@ -1,8 +1,16 @@
 from random import shuffle
 
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
 
 from .forms import CountyPerformancePetitionForm, CountyPerformanceSenateForm
+
+from django.shortcuts import redirect
+
+from pombola.feedback.models import Feedback
+
 
 class CountyPerformanceView(TemplateView):
     """This view displays a page about county performance with calls to action
@@ -40,3 +48,63 @@ class CountyPerformanceView(TemplateView):
         shuffle(context['major_partials'])
 
         return context
+
+
+class CountyPerformanceSubmissionMixin(object):
+    """A mixin useful for handling senate comment and petition emails"""
+
+    def create_feedback(self, comment='', email=''):
+        """A helper method for adding feedback to the database"""
+        feedback = Feedback()
+        feedback.status = 'non-actionable'
+        feedback.comment = comment
+        feedback.email = email
+        feedback.url = self.request.build_absolute_uri()
+        if self.request.user.is_authenticated():
+            feedback.user = self.request.user
+        feedback.save()
+
+    def form_invalid(self, form):
+        """Redirect back to a reduced version of the page from either form"""
+        extra_context = {
+            '{0}_form'.format(self.form_key): form,
+            'major_partials': ['_county_{0}.html'.format(self.form_key)],
+            'suppress_banner': True,
+            'correct_errors': True}
+        context = self.get_context_data(**extra_context)
+        return self.render_to_response(context)
+
+
+class CountyPerformanceSenateSubmission(CountyPerformanceSubmissionMixin,
+                                        FormView):
+    """A view for handling submissions of comments for the senate"""
+
+    template_name = 'county-performance.html'
+    success_url = '/county-performance/senate/thanks'
+    form_class = CountyPerformanceSenateForm
+    form_key = 'senate'
+
+    def form_valid(self, form):
+        new_comment = u"senate feedback: "
+        new_comment += form.cleaned_data.get('comments', '').strip()
+        self.create_feedback(comment=new_comment)
+        return super(CountyPerformanceSenateSubmission,
+                     self).form_valid(form)
+
+
+class CountyPerformancePetitionSubmission(CountyPerformanceSubmissionMixin,
+                                          FormView):
+    """A view for handling a petition signature"""
+
+    template_name = 'county-performance.html'
+    success_url = '/county-performance/petition/thanks'
+    form_class = CountyPerformancePetitionForm
+    form_key = 'petition'
+
+    def form_valid(self, form):
+        new_comment = u"petition signature from: "
+        new_comment += form.cleaned_data.get('name', '').strip()
+        self.create_feedback(comment=new_comment,
+                             email=form.cleaned_data.get('email'))
+        return super(CountyPerformancePetitionSubmission,
+                     self).form_valid(form)
