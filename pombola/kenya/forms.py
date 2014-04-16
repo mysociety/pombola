e5b59dc84a3dc4cdf3921d44bbdb210cea929267
Mark Longair <mhl@pobox.com>
2014-04-16 14:55:55 +0100

    MIT: Present all validation errors together, not field-by-field

--- pombola/kenya/forms.py
+++ pombola/kenya/forms.py
@@ -3,17 +3,31 @@ from django.forms.widgets import Textarea
 
 class CountyPerformancePetitionForm(forms.Form):
     name = forms.CharField(
+        error_messages={'required': 'You must enter a name'},
         widget=forms.TextInput(
             attrs={
                 'placeholder': 'Your name'}))
     email = forms.EmailField(
+        error_messages={'required': 'You must enter a valid email address'},
         widget=forms.TextInput(
             attrs={
                 'placeholder': 'Your email address'}))
 
+    def clean(self):
+        # We want to provide a single helpful error message if both
+        # name and email address aren't supplied, so override clean to
+        # detect that condition
+        cleaned_data = super(CountyPerformancePetitionForm,
+                             self).clean()
+        if not cleaned_data:
+            raise forms.ValidationError(
+                "You must specify a name and a valid email address")
+        return cleaned_data
+
 class CountyPerformanceSenateForm(forms.Form):
     comments = forms.CharField(
         label='Tell the senate what you think',
+        error_messages={'required': "You didn't enter a comment"},
         widget=Textarea(
             attrs={
                 'rows': 5,
