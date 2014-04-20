ab7a546e4a7fb686f61b904777aa26c7d596ff03
Geoffrey Kilpin <geoffreykilpin@gmail.com>
2014-04-20 19:21:18 +0200

    ZA: Add members interests data to PopIt export
    (Minor refactoring by Mark Longair.)

--- pombola/south_africa/lib.py
+++ pombola/south_africa/lib.py
@@ -8,5 +8,21 @@ def make_pa_url(pombola_object, base_url):
 def add_extra_popolo_data_for_person(person, popolo_object, base_url):
     popolo_object['pa_url'] = make_pa_url(person, base_url)
 
+    personinterests = person.interests_register_entries.all()
+    if personinterests:
+        interests = {}
+        for entry in personinterests:
+            release = entry.release
+            category = entry.category
+            interests.setdefault(release.name, {})
+            interests[release.name].setdefault(category.name, [])
+
+            #assuming no entrylineitems with duplicate keys within an entry
+            entrylineitems = dict((e.key, e.value) for e in entry.line_items.all())
+
+            interests[release.name][category.name].append(entrylineitems)
+
+        popolo_object['interests_register'] = interests
+
 def add_extra_popolo_data_for_organization(organisation, popolo_object, base_url):
     popolo_object['pa_url'] = make_pa_url(organisation, base_url)
