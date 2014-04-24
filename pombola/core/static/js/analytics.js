d74a14e3ae7d2ba3450d515a6484b1967dddc5fe
Mark Longair <mhl@pobox.com>
2014-04-24 11:29:45 +0100

    Switch to using the Javascript for Universal Analytics
    Also bump the STATIC_GENERATION_NUMBER to break caching of the
    old Javascript.

--- pombola/core/static/js/analytics.js
+++ pombola/core/static/js/analytics.js
@@ -1,11 +1,10 @@
- // mathiasbynens.be/notes/async-analytics-snippet
-var _gaq = [["_setAccount", pombola_settings.google_analytics_account], ["_trackPageview"]];
-(function(d, t) {
-  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-  g.async = 1;
-  g.src = ("https:" == location.protocol ? "//ssl" : "//www") + ".google-analytics.com/ga.js";
-  s.parentNode.insertBefore(g, s)
-}(document, "script"));
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', pombola_settings.google_analytics_account);
+ga('send', 'pageview');
 
 // add in some tracking to detect when users print pages. Will be used to judge
 // how often this happens.
