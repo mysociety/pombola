54d87573a7fbb9806d5f4a9e14831bc540b68f98
Mark Longair <mhl@pobox.com>
2014-05-12 18:23:19 +0100

    MIT: Add tests for the share links and the survey link

--- pombola/kenya/tests.py
+++ pombola/kenya/tests.py
@@ -1,5 +1,7 @@
 import json
 import re
+import urllib
+import urlparse
 
 from django.core.urlresolvers import reverse
 
@@ -29,6 +31,10 @@ class CountyPerformancePageTests(WebTest):
         self.assertTrue(response.html.get('a', {'id': 'share-twitter'}))
         self.assertFalse(
             response.html.findAll('div', {'id': 'surveyPromoBanner'}))
+        self.assertFalse(
+            response.html.findAll('div', {'id': 'opportunity'}))
+        self.assertFalse(
+            response.html.findAll('div', {'id': 'threat'}))
 
     def test_threat_variant(self):
         response = self.app.get('/county-performance?variant=t')
@@ -114,3 +120,64 @@ class CountyPerformancePageTests(WebTest):
         self.assertEqual(user_key, event.user_key)
         extra_data = json.loads(event.extra_data)
         self.assertEqual('m', extra_data['g'])
+
+    def test_survey_link(self):
+        Event.objects.all().delete()
+        response = self.app.get('/county-performance?variant=n&g=f&agroup=under&utm_expid=1234')
+        event = Event.objects.get(
+            variant='n',
+            category='page',
+            action='view',
+            label='county-performance')
+        user_key = re.search(r'^\d+$', event.user_key).group()
+        extra_data = json.loads(event.extra_data)
+        self.assertEqual('f', extra_data['g'])
+        self.assertEqual('under', extra_data['agroup'])
+        survey_url = response.html.find('a', {'id': 'take-survey'})['href']
+        parsed_url = urlparse.urlparse(survey_url)
+        self.assertEqual(parsed_url.path,
+                         reverse('county-performance-survey'))
+        # Follow that link, but don't follow the redirect:
+        response = self.app.get(survey_url)
+        self.assertEqual(response.status_code, 302)
+        redirect_url = response.location
+        parsed_redirect_url = urlparse.urlparse(redirect_url)
+        self.assertEqual(parsed_redirect_url.netloc, 'survey.az1.qualtrics.com')
+        self.assertEqual(parsed_redirect_url.path, '/SE/')
+        parsed_redirect_qs = urlparse.parse_qs(parsed_redirect_url.query)
+        self.assertEqual(parsed_redirect_qs['SID'], ['SV_5hhE4mOfYG1eaOh'])
+        self.assertEqual(parsed_redirect_qs['user_key'], [user_key])
+        self.assertEqual(parsed_redirect_qs['variant'], ['n'])
+
+    def test_share_link(self):
+        Event.objects.all().delete()
+        response = self.app.get('/county-performance?variant=n&g=f&agroup=over&utm_expid=56789')
+        event = Event.objects.get(
+            variant='n',
+            category='page',
+            action='view',
+            label='county-performance')
+        user_key = re.search(r'^\d+$', event.user_key).group()
+        extra_data = json.loads(event.extra_data)
+        self.assertEqual('f', extra_data['g'])
+        self.assertEqual('over', extra_data['agroup'])
+        # Try the Facebook link first:
+        share_url = response.html.find('a', {'id': 'share-facebook'})['href']
+        parsed_url = urlparse.urlparse(share_url)
+        self.assertEqual(parsed_url.path,
+                         reverse('county-performance-share'))
+        self.assertEqual(parsed_url.query,
+                         'n=facebook')
+        # Now follow that link to find the redirect:
+        response = self.app.get(share_url)
+        self.assertEqual(response.status_code, 302)
+        redirect_url = response.location
+        parsed_redirect_url = urlparse.urlparse(redirect_url)
+        self.assertEqual(parsed_redirect_url.netloc, 'www.facebook.com')
+        self.assertEqual(parsed_redirect_url.path, '/sharer/sharer.php')
+        parsed_redirect_qs = urlparse.parse_qs(parsed_redirect_url.query)
+
+        facebook_url = urllib.unquote(parsed_redirect_qs['u'][0])
+        parsed_facebook_url = urlparse.urlparse(facebook_url)
+        self.assertEqual(parsed_facebook_url.path, '/county-performance')
+        self.assertTrue(re.search(r'^via=[a-f0-9]+$', parsed_facebook_url.query))
