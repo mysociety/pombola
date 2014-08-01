6a5ea6f67a5f575707e9d1f40066ea6e832524cc
Mark Longair <mhl@pobox.com>
2014-08-01 14:09:33 +0100

    Use django-pipeline compressed and fingerprinted Javascript

--- pombola/core/static/js/loader.js
+++ pombola/core/static/js/loader.js
@@ -18,9 +18,10 @@
     // create links to all the extra js needed
     var extra_js = [];
     for ( i=0; i<pombola_settings.extra_js.length; i++ ) {
-        var extra = pombola_settings.extra_js[i];
-        var url = static_url( extra );
-        extra_js.push( url );
+        var extra = pombola_settings.extra_js[i], url;
+        if (extra) {
+            extra_js.push(extra);
+        }
     }
         
     Modernizr.load(
@@ -28,29 +29,19 @@
             test : Modernizr.mq('only all and (max-width: 640px)'),
             yep : [
                 static_url('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'),
-                static_url('js/mobile-functions.js')
-            ],
+            ].concat(pipeline_mobile_only),
             nope : [
                 static_url('//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'),
-                static_url('js/libs/jquery-ui-1.8.17.custom.min.js'),
-                static_url('js/libs/jquery.ui.autocomplete.html.2010-10-25.js'),
-                static_url('js/libs/jquery.form-v2.94.js'), // TODO - only load when needed for feedback form                    
-                static_url('js/desktop-functions.js'),                
-            ],
-            both: [
-                static_url('js/both-functions.js'),
-                static_url('js/twitter-embed.js'),
-                '//www.google.com/jsapi' // ?key=INSERT-YOUR-KEY
-            ].concat( extra_js ),
+            ].concat(pipeline_desktop_only),
+            both: pipeline_desktop_and_mobile.concat(
+                [static_url('//www.google.com/jsapi')]).concat(extra_js),
             complete: function () {
                 for (i=0; i<pombola_run_when_document_ready_array.length; i++) {
                     $( pombola_run_when_document_ready_array[i] );
                 }
 
                 // Now load all the optional bits that we didn't want slowing down the more important bits
-                Modernizr.load([
-                  static_url('js/analytics.js')
-                ]);
+                Modernizr.load(pipeline_analytics);
             }
         }
     );
