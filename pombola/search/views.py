1286e6164645a20030653727439a740936fe024b
Mark Longair <mhl@pobox.com>
2014-08-12 12:32:05 +0100

    Refactor the view + template to remove repetition

--- pombola/search/views.py
+++ pombola/search/views.py
@@ -5,6 +5,7 @@ import sys
 import simplejson
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.shortcuts  import render_to_response, get_object_or_404, redirect
 from django.template   import RequestContext
@@ -28,6 +29,7 @@ class SearchBaseView(FormView):
 
     def __init__(self, *args, **kwargs):
         super(SearchBaseView, self).__init__(*args, **kwargs)
+        self.section_ordering = ['persons', 'position_titles', 'organisations', 'places']
         self.search_sections = {
             'persons': {
                 'model': models.Person,
@@ -49,12 +51,14 @@ class SearchBaseView(FormView):
         }
         if settings.ENABLED_FEATURES['speeches']:
             from speeches.models import Speech
+            self.section_ordering.append('speeches')
             self.search_sections['speeches'] = {
                 'model': Speech,
                 'title': 'Speeches',
             }
         if 'pombola.info' in settings.INSTALLED_APPS:
             from pombola.info.models import InfoPage
+            self.section_ordering += ['blog_posts', 'info_pages']
             self.search_sections['blog_posts'] = {
                 'model': InfoPage,
                 'title': 'Info Pages',
@@ -71,15 +75,26 @@ class SearchBaseView(FormView):
         # about
         self.section = self.request.GET.get('section')
         self.query_text = self.request.GET.get('q')
-        if self.section and self.section not in self.search_sections:
-            message = 'The section {0} was not known'
-            return HttpResponseBadRequest(message.format(message))
+        # If someone's requested the "global" section, redirect to the
+        # global search results:
+        if self.section:
+            if self.section == 'global':
+                url = reverse('core_search')
+                if self.query_text:
+                    url += "?q={0}".format(self.query_text)
+                return redirect(url)
+            elif self.section not in self.search_sections:
+                message = 'The section {0} was not known'
+                return HttpResponseBadRequest(message.format(self.section))
         return super(SearchBaseView, self).get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(SearchBaseView, self).get_context_data(**kwargs)
         context['query_text'] = self.query_text
         context['section'] = self.section
+        context['results'] = [
+            self.get_section_data(section) for section in self.section_ordering
+        ]
         context['form_options'] = [('global', 'All', (not self.section))]
         for section in self.section_ordering:
             context['form_options'].append(
@@ -89,7 +104,7 @@ class SearchBaseView(FormView):
             )
         return context
 
-    def get_section_results(self, section):
+    def get_section_data(self, section):
         defaults = self.search_sections[section]
         extra_filter = defaults.get('filter', {})
         extra_exclude = defaults.get('exclude', {})
@@ -97,7 +112,12 @@ class SearchBaseView(FormView):
         if extra_exclude:
             query = query.exclude(**extra_exclude)
         query = query.filter(content=AutoQuery(self.query_text), **extra_filter)
-        return query.highlight()
+        result = defaults.copy()
+        result['results'] = query.highlight()
+        result['results_count'] = result['results'].count()
+        result['section'] = section
+        result['section_dashes'] = section.replace('_', '-')
+        return result
 
 
 class SearchGlobalView(SearchBaseView):
@@ -111,14 +131,14 @@ class SearchGlobalView(SearchBaseView):
     def get_context_data(self, **kwargs):
         context = super(SearchGlobalView, self).get_context_data(**kwargs)
         context['top_hits'] = []
-        for section in self.search_sections:
-            context_key = section + '_results'
-            results = self.get_section_results(section)
-            max_for_top_hits = SearchGlobalView.top_hits_under.get(section, None)
-            if max_for_top_hits and results.count() <= max_for_top_hits:
-                context['top_hits'] += results
+        results_without_top_hits = []
+        for data in context['results']:
+            max_for_top_hits = SearchGlobalView.top_hits_under.get(data['section'], None)
+            if max_for_top_hits and data['results_count'] <= max_for_top_hits:
+                context['top_hits'] += data['results']
             else:
-                context[context_key] = results
+                results_without_top_hits.append(data)
+        context['results'] = results_without_top_hits
         return context
 
 
@@ -129,7 +149,7 @@ class SearchSectionView(SearchBaseView):
         context = super(SearchSectionView, self).get_context_data(**kwargs)
 
         context['title'] = self.search_sections[self.section]['title']
-        all_results = self.get_section_results(self.section)
+        all_results = self.get_section_data(self.section)['results']
         paginator = Paginator(all_results, 10)
         page = self.request.GET.get('page')
         try:
