c5baa7c0bd917a417fafec5f18536479a2d88b12
Mark Longair <mhl@pobox.com>
2014-08-13 10:15:26 +0100

    Move hide-reveals.js into core

--- pombola/settings/south_africa_base.py
+++ pombola/settings/south_africa_base.py
@@ -52,13 +52,6 @@ COUNTRY_JS = {
         'output_filename': 'js/tabs.js',
         'template_name': 'pipeline/js-array.html',
     },
-    'hide-reveal': {
-        'source_filenames': (
-            'js/hide-reveal.js',
-        ),
-        'output_filename': 'js/hide-reveal.js',
-        'template_name': 'pipeline/js-array.html',
-    },
     'za-map-drilldown': {
         'source_filenames': (
             'js/za-map-drilldown.js',
