db90c52f7ede86a34e36a51c1c3faec3371ee4db
Chris Mytton <self@hecticjeff.net>
2014-09-17 16:58:41 +0100

    Add allowed bleach tags to base.py settings
    This instructs bleach that it should keep the allowed tags and
    attributes as they are, but strip anything else.

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -553,3 +553,19 @@ PIPELINE_YUI_BINARY = '/usr/bin/env yui-compressor'
 PIPELINE_DISABLE_WRAPPER = True
 
 EXCLUDE_FROM_SEARCH = ()
+
+# Settings for bleach, used by sayit to determine what html is allowed
+BLEACH_ALLOWED_TAGS = [
+    'a', 'abbr', 'b', 'i', 'u', 'span', 'sub', 'sup', 'br',
+    'p',
+    'ol', 'ul', 'li',
+    'table', 'caption', 'tr', 'th', 'td',
+]
+
+BLEACH_ALLOWED_ATTRIBUTES = {
+    '*': [ 'id', 'title' ], # class, style
+    'a': [ 'href' ],
+    'li': [ 'value' ],
+}
+
+BLEACH_STRIP_TAGS = True
