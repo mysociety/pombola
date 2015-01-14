c3ada10657efa7435564a1d6f8ff7afbfb585f54
Mark Longair <mhl@pobox.com>
2015-01-14 08:47:05 +0000

    NG: Add a regression test for displaying escaped HTML in the blog
    A fixed version of the test that Chris Mytton suggested in:
    
      https://github.com/mysociety/pombola/pull/1587

--- pombola/nigeria/tests.py
+++ pombola/nigeria/tests.py
@@ -6,6 +6,8 @@ from django.test import TestCase
 
 from nose.plugins.attrib import attr
 
+from pombola.info.models import InfoPage
+
 # Needed to run the doc tests in views.py
 
 def suite():
@@ -21,3 +23,20 @@ class HomeViewTest(TestCase):
         self.assertIn('featured_person', response.context)
         self.assertIn('featured_persons', response.context)
         self.assertIn('editable_content', response.context)
+
+@attr(country='nigeria')
+class InfoBlogListTest(TestCase):
+
+    def setUp(self):
+        self.info_page = InfoPage.objects.create(
+            slug='escaping-test',
+            kind='blog',
+            title='Escaping Test', markdown_content="\nTesting\n\n**Escaped**\n\nContent"
+        )
+
+    def tearDown(self):
+        self.info_page.delete()
+
+    def test_html_not_escaped(self):
+        response = self.client.get('/blog/')
+        self.assertNotIn('&lt;p&gt;', response.content)
