ca603f3e43bf64c45437bdb815e022d236deb61d
Chris Mytton <chrismytton@gmail.com>
2015-01-14 12:37:12 +0000

    Move shujaaz finalist data into a separate file
    We're going to reuse this data in the person view to display which
    category a person is a finalist for, so rather than adding more
    confusing top level constants in views.py I've moved this data into its
    own file for clarity.

---
+++ pombola/kenya/shujaaz.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+FINALISTS = [
+    {
+        'category': 'Education (National Assembly)',
+        'name': 'Hon. Boniface Gatobu',
+        'person': 1322,
+        'reasons': '''<ul><li>Motion urging the Government, to
+establish a public databank of all bright and poor students to be in the
+custody of the County Director of Education, and such information
+be disseminated and made available to the public institutions
+including the respective Constituencies Development Fund
+Committees which shall take into consideration when disbursing
+bursaries and such other institutions that may be willing to
+support such students.  Proposed on 15th May, 2013 and adopted in
+July 2013</li></ul>''',
+    },
+    {
+        'category': 'Education (Senate)',
+        'name': 'Sen. Boni Khalwale',
+        'person': 170,
+        'reasons': '''<ul><li>Motion on equitable distribution of
+universities across counties. Notice mooted on 30th April 2013 and adopted
+later in May, 2013</li></ul>''',
+    },
+    {
+        'category': 'Water',
+        'name': 'Hon. Amina Abdalla',
+        'person': 382,
+        'reasons': '''<ul><li>Contributions to the Water Bill 2014 on
+the importance of the separation of responsibilities between the
+water authorities at the national and county government levels.
+(<a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2014-10-23-14-30-00#entry-494909">Read
+the debate on the moving of the Water Bill</a>.)</li></ul>''',
+    },
+    {
+        'category': 'Food',
+        'name': 'Sen. Beatrice Elachi',
+        'person': 13115,
+        'reasons': '<ul><li>The Food Security Bill.</li></ul>',
+    },
+    {
+        'category': 'Health (Senate)',
+        'name': 'Sen. Dr. Wilfred Machage',
+        'person': 179,
+        'reasons': '''<ul><li>Motion urging government to establish
+level 5 hospitals in all 47 counties. Motion proposed on May 23rd and adopted
+in June, 2013.</li></ul>''',
+    },
+    {
+        'category': 'Health (National Assembly)',
+        'name': 'Hon. James Nyikal',
+        'person': 434,
+        'reasons': '''<ul><li>Contributions on the health sector and
+devolution: “Hon. Speaker, thank you. It is true that we have a big problem
+of health in the country now, which has been made worse by the
+process of devolution. What I have said here many times is that
+we failed to devolve the way we planned in the Constitution. The
+TA and the appropriate law have never been applied. One day the
+governors, the Council of Governors and the President decided to
+devolve healthcare in a day. It is not possible. The problem we
+are seeing in healthcare is also in other places, only that in
+health area players are noisy.”
+(<a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2014-08-20-14-30-00#entry-486463">See
+the full speech and debate.</a>)</li></ul>''',
+    },
+    {
+        'category': 'Youth',
+        'name': 'Hon. Chris Wamalwa',
+        'person': 1889,
+        'reasons': '''<ul><li>The mode of disbursement of Kshs. 6
+billion initially earmarked for a Presidential runoff, to be disbursed
+to the youths and women groups across the Country. Sought on 17th, July
+2013, Nairobi.</li></ul>''',
+    },
+    {
+        'category': 'Budgetary oversight',
+        'name': 'Hon. John Mbadi',
+        'person': 110,
+        'reasons': '''<ul><li>The salaries, allowances, and vehicles
+allocated to constitutional commissioners. Sought on 4th June,
+2013.</li></ul>''',
+    },
+    {
+        'category': 'Oversight (progressive legislation)',
+        'name': 'Hon. Kabando wa Kabando',
+        'person': 31,
+        'reasons': '''<ul><li>Proposed the reintroduction of Capital
+gains tax that was included in the Finance Bill 2014</li><li>Central Bank
+(amendment) Act, 2014</li><li>Followed up on the Status of the Kroll report
+and capital gains flight</li></ul>''',
+    },
+    {
+        'category': 'Devolution',
+        'name': 'Sen. Kithure Kindiki',
+        'person': 1046,
+        'reasons': '''<ul><li>The County Allocation of Revenue Bill,
+2013; The Community Land Bill; Division of Revenue Bill, 2014 (Sen. Bill No.12)
+2013; County Allocation of Revenue Bill, 2014 (Sen. Bill No.13); Public Finance
+Management (Amendment) Bill, 2014 (Sen. Bill No. 11); County Assembly Powers
+and Privileges Bill, 2014 (Sen. Bill No. 14); Parliamentary Powers and
+Privileges Bill, 2014 (Sen. Bill No. 15); The County Retirement Scheme
+Bill;</li><li>Motion urging the national government to enter into
+agreements with county government to transfer resources,
+functions and powers to county governments.</li></ul>''',
+    },
+]
--- pombola/kenya/views.py
+++ pombola/kenya/views.py
@@ -23,6 +23,7 @@ from pombola.core.views import PersonDetail, PersonDetailSub
 from pombola.experiments.models import Experiment, Event
 from pombola.feedback.models import Feedback
 from pombola.hansard.views import HansardPersonMixin
+from pombola.kenya import shujaaz
 
 EXPERIMENT_DATA = {
     'mit-county': {
@@ -362,111 +363,13 @@ class ShujaazFinalistsView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(ShujaazFinalistsView, self).get_context_data(**kwargs)
-        finalists = [
-            {
-                'category': 'Education (National Assembly)',
-                'name': 'Hon. Boniface Gatobu',
-                'person': Person.objects.get(pk=1322),
-                'reasons': '''<ul><li>Motion urging the Government, to
-establish a public databank of all bright and poor students to be in the
-custody of the County Director of Education, and such information
-be disseminated and made available to the public institutions
-including the respective Constituencies Development Fund
-Committees which shall take into consideration when disbursing
-bursaries and such other institutions that may be willing to
-support such students.  Proposed on 15th May, 2013 and adopted in
-July 2013</li></ul>''',
-            },
-            {
-                'category': 'Education (Senate)',
-                'name': 'Sen. Boni Khalwale',
-                'person': Person.objects.get(pk=170),
-                'reasons': '''<ul><li>Motion on equitable distribution of
-universities across counties. Notice mooted on 30th April 2013 and adopted
-later in May, 2013</li></ul>''',
-            },
-            {
-                'category': 'Water',
-                'name': 'Hon. Amina Abdalla',
-                'person': Person.objects.get(pk=382),
-                'reasons': '''<ul><li>Contributions to the Water Bill 2014 on
-the importance of the separation of responsibilities between the
-water authorities at the national and county government levels.
-(<a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2014-10-23-14-30-00#entry-494909">Read
-the debate on the moving of the Water Bill</a>.)</li></ul>''',
-            },
-            {
-                'category': 'Food',
-                'name': 'Sen. Beatrice Elachi',
-                'person': Person.objects.get(pk=13115),
-                'reasons': '<ul><li>The Food Security Bill.</li></ul>',
-            },
-            {
-                'category': 'Health (Senate)',
-                'name': 'Sen. Dr. Wilfred Machage',
-                'person': Person.objects.get(pk=179),
-                'reasons': '''<ul><li>Motion urging government to establish
-level 5 hospitals in all 47 counties. Motion proposed on May 23rd and adopted
-in June, 2013.</li></ul>''',
-            },
-            {
-                'category': 'Health (National Assembly)',
-                'name': 'Hon. James Nyikal',
-                'person': Person.objects.get(pk=434),
-                'reasons': '''<ul><li>Contributions on the health sector and
-devolution: “Hon. Speaker, thank you. It is true that we have a big problem
-of health in the country now, which has been made worse by the
-process of devolution. What I have said here many times is that
-we failed to devolve the way we planned in the Constitution. The
-TA and the appropriate law have never been applied. One day the
-governors, the Council of Governors and the President decided to
-devolve healthcare in a day. It is not possible. The problem we
-are seeing in healthcare is also in other places, only that in
-health area players are noisy.”
-(<a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2014-08-20-14-30-00#entry-486463">See
-the full speech and debate.</a>)</li></ul>''',
-            },
-            {
-                'category': 'Youth',
-                'name': 'Hon. Chris Wamalwa',
-                'person': Person.objects.get(pk=1889),
-                'reasons': '''<ul><li>The mode of disbursement of Kshs. 6
-billion initially earmarked for a Presidential runoff, to be disbursed
-to the youths and women groups across the Country. Sought on 17th, July
-2013, Nairobi.</li></ul>''',
-            },
-            {
-                'category': 'Budgetary oversight',
-                'name': 'Hon. John Mbadi',
-                'person': Person.objects.get(pk=110),
-                'reasons': '''<ul><li>The salaries, allowances, and vehicles
-allocated to constitutional commissioners. Sought on 4th June,
-2013.</li></ul>''',
-            },
-            {
-                'category': 'Oversight (progressive legislation)',
-                'name': 'Hon. Kabando wa Kabando',
-                'person': Person.objects.get(pk=31),
-                'reasons': '''<ul><li>Proposed the reintroduction of Capital
-gains tax that was included in the Finance Bill 2014</li><li>Central Bank
-(amendment) Act, 2014</li><li>Followed up on the Status of the Kroll report
-and capital gains flight</li></ul>''',
-            },
-            {
-                'category': 'Devolution',
-                'name': 'Sen. Kithure Kindiki',
-                'person': Person.objects.get(pk=1046),
-                'reasons': '''<ul><li>The County Allocation of Revenue Bill,
-2013; The Community Land Bill; Division of Revenue Bill, 2014 (Sen. Bill No.12)
-2013; County Allocation of Revenue Bill, 2014 (Sen. Bill No.13); Public Finance
-Management (Amendment) Bill, 2014 (Sen. Bill No. 11); County Assembly Powers
-and Privileges Bill, 2014 (Sen. Bill No. 14); Parliamentary Powers and
-Privileges Bill, 2014 (Sen. Bill No. 15); The County Retirement Scheme
-Bill;</li><li>Motion urging the national government to enter into
-agreements with county government to transfer resources,
-functions and powers to county governments.</li></ul>''',
-            },
-        ]
+
+        def populate_person(f):
+            finalist = dict(f)
+            finalist['person'] = Person.objects.get(pk=finalist['person'])
+            return finalist
+
+        finalists = [populate_person(f) for f in shujaaz.FINALISTS]
         half = len(finalists) / 2
         context['finalists_column_1'] = finalists[:half]
         context['finalists_column_2'] = finalists[half:]
