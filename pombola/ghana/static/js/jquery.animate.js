0e6c4e122617508f302b16b3a983efbb4fa8838d
Mark Longair <mhl@pobox.com>
2015-01-15 11:56:42 +0000

    Convert a Javascript file from ISO-8859 to UTF-8
    django-pipeline complains if an input file is non-UTF-8 - the copyright
    symbol in this file was ISO-8859

--- pombola/ghana/static/js/jquery.animate.js
+++ pombola/ghana/static/js/jquery.animate.js
@@ -8,7 +8,7 @@
  * 
  * Open source under the BSD License. 
  * 
- * Copyright © 2008 George McGinley Smith
+ * Copyright Â© 2008 George McGinley Smith
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
@@ -177,7 +177,7 @@ jQuery.extend( jQuery.easing,
  * 
  * Open source under the BSD License. 
  * 
- * Copyright © 2001 Robert Penner
+ * Copyright Â© 2001 Robert Penner
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
