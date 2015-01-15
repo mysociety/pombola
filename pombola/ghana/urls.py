d86bdec5d7d57fe74cb463e391798bd1e5be87ff
Mark Longair <mhl@pobox.com>
2015-01-15 14:06:30 +0000

    Update Ghana code to match current Pombola

--- pombola/ghana/urls.py
+++ pombola/ghana/urls.py
@@ -1,7 +1,5 @@
-from django.conf.urls import patterns, include, url, handler404
+from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
-import django.contrib.auth.views
-
 
 from .views import data_upload, info_page_upload
 
@@ -9,11 +7,5 @@ urlpatterns = patterns('',
     url(r'^intro$', TemplateView.as_view(template_name='intro.html')),
     url(r'^data/upload/mps/$', data_upload, name='data_upload'),
     url(r'^data/upload/info-page/$', info_page_upload, name='info_page_upload'),
-    
-    #auth views
-    url(r'^accounts/login$', django.contrib.auth.views.login, name='login'),
-    url(r'^accounts/logut$', django.contrib.auth.views.logout, name='logout'),
-    #url(r'^accounts/register$', registration.backends.simple.urls, name='register'),
-
-
+    url('', include('django.contrib.auth.urls')),
 )
