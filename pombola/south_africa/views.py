mysociety/pombola

commit 7db85cb55b99901d7a882ac034c495d613727c07

ZA: members' interests browser
Creates an interface to browse members interest declarations as well as
aggregated data.

pombola/south_africa/views.py
@@ -36,6 +36,10 @@ from pombola.search.views import GeocoderView, SearchBaseView
 
 from pombola.south_africa.models import ZAPlace
 
+from pombola.interests_register.models import Release, Category, Entry, EntryLineItem
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django_date_extensions.fields import ApproximateDateField, ApproximateDate
+
 # In the short term, until we have a list of constituency offices and
 # addresses from DA, let's bundle these together.
 CONSTITUENCY_OFFICE_PLACE_KIND_SLUGS = (
@@ -1096,5 +1100,409 @@ class SAElectionProvinceCandidatesView(TemplateView):
         context['province'] = models.Place.objects.get(
             kind=province_kind,
             slug=province_name)
+
+        return context
+
+class SAMembersInterestsIndex(TemplateView):
+    template_name = "interests_register/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = {}
+
+        #get the data for the form
+        kind_slugs = (
+            'parliament',
+            'executive',
+            'joint-committees',
+            'ncop-committees',
+            'ad-hoc-committees',
+            'national-assembly-committees'
+        )
+        context['categories'] = Category.objects.all()
+        context['parties'] = models.Organisation.objects.filter(kind__slug='party')
+        context['organisations'] = models.Organisation.objects \
+            .filter(kind__slug__in=kind_slugs) \
+            .order_by('kind__id','name')
+        context['releases'] = Release.objects.all()
+
+        #set filter values
+        for key in ('display', 'category', 'party', 'organisation', 'release'):
+            context[key] = 'all'
+            if key in self.request.GET:
+                context[key] = self.request.GET[key]
+
+        if context['release']!='all':
+            release = Release.objects.get(slug=context['release'])
+            context['release_id'] = release.id
+
+        if context['category']!='all':
+            categorylookup = Category.objects.get(slug=self.request.GET['category'])
+            context['category_id'] = categorylookup.id
+
+        #complete view - declarations for multiple people in multiple categories
+        if context['display']=='all' and context['category']=='all':
+            context = self.get_complete_view(context)
+
+        #section view - data for multiple people in different categories
+        elif context['display']=='all' and context['category']!='all':
+            context = self.get_section_view(context)
+
+        #numberbyrepresentative view - number of declarations per person per category
+        elif context['display']=='numberbyrepresentative':
+            context = self.get_number_by_representative_view(context)
+
+        #numberbysource view - number of declarations by source per category
+        elif context['display']=='numberbysource':
+            context = self.get_numberbysource_view(context)
+
+        return context
+
+    def get_complete_view(self, context):
+        #complete view - declarations for multiple people in multiple categories
+        context['layout'] = 'complete'
+
+        when = datetime.date.today()
+        now_approx = repr(ApproximateDate(year=when.year, month=when.month, day=when.day))
+
+        people = Entry.objects.order_by(
+            'person__legal_name',
+            'release__date'
+        ).distinct(
+            'person__legal_name',
+            'release__date'
+        )
+
+        if context['release']!='all':
+            people = people.filter(release=context['release_id'])
+
+        if context['party']!='all':
+            people = people.filter(
+                Q(person__position__end_date__gte=now_approx) | Q(person__position__end_date=''),
+                person__position__organisation__slug=context['party'],
+                person__position__start_date__lte=now_approx)
+
+        if context['organisation']!='all':
+            people = people.filter(
+                Q(person__position__end_date__gte=now_approx) | Q(person__position__end_date=''),
+                person__position__organisation__slug=context['organisation'],
+                person__position__start_date__lte=now_approx)
+
+        paginator = Paginator(people, 10)
+        page = self.request.GET.get('page')
+
+        try:
+            people_paginated = paginator.page(page)
+        except PageNotAnInteger:
+            people_paginated = paginator.page(1)
+        except EmptyPage:
+            people_paginated = paginator.page(paginator.num_pages)
+
+        context['paginator'] = people_paginated
+
+        #tabulate the data
+        data = []
+        for entry_person in people_paginated:
+            person = entry_person.person
+            year = entry_person.release.date.year
+            person_data = []
+            person_categories = person.interests_register_entries.filter(
+                release=entry_person.release
+            ).order_by(
+                'category__id'
+            ).distinct(
+                'category__id'
+            )
+
+            for cat in person_categories:
+                entries = person.interests_register_entries.filter(
+                    category=cat.category,
+                    release=entry_person.release)
+
+                headers = []
+                headers_index = {}
+                cat_data = []
+                for entry in entries:
+                    row = ['']*len(headers)
+
+                    for entrylineitem in entry.line_items.all():
+                        if entrylineitem.key not in headers:
+                            headers_index[entrylineitem.key] = len(headers)
+                            headers.append(entrylineitem.key)
+                            row.append('')
+
+                        row[headers_index[entrylineitem.key]] = entrylineitem.value
+
+                    cat_data.append(row)
+
+                person_data.append({
+                    'category': cat.category,
+                    'headers': headers,
+                    'data': cat_data})
+            data.append({
+                'person': person,
+                'data': person_data,
+                'year': year})
+
+        context['data'] = data
+
+        return context
+
+    def get_section_view(self, context):
+        #section view - data for multiple people in different categories
+        context['layout'] = 'section'
+
+        when = datetime.date.today()
+        now_approx = repr(ApproximateDate(year=when.year, month=when.month, day=when.day))
+
+        entries = Entry.objects.select_related(
+            'person__name',
+            'category'
+        ).all().filter(
+            category__id=context['category_id']
+        ).order_by(
+            'person',
+            'category'
+        )
+
+        if context['release']!='all':
+            entries = entries.filter(release=context['release_id'])
+
+        if context['party']!='all':
+            entries = entries.filter(
+                Q(person__position__end_date__gte=now_approx) | Q(person__position__end_date=''),
+                person__position__organisation__slug=context['party'],
+                person__position__start_date__lte=now_approx)
+
+        if context['organisation']!='all':
+            entries = entries.filter(
+                Q(person__position__end_date__gte=now_approx) | Q(person__position__end_date=''),
+                person__position__organisation__slug=context['organisation'],
+                person__position__start_date__lte=now_approx)
+
+        paginator = Paginator(entries, 25)
+        page = self.request.GET.get('page')
+
+        try:
+            entries_paginated = paginator.page(page)
+        except PageNotAnInteger:
+            entries_paginated = paginator.page(1)
+        except EmptyPage:
+            entries_paginated = paginator.page(paginator.num_pages)
+
+        headers = ['Year', 'Person', 'Type']
+        headers_index = {'Year': 0, 'Person': 1, 'Type': 2}
+        data = []
+        for entry in entries_paginated:
+            row = ['']*len(headers)
+            row[0] = entry.release.date.year
+            row[1] = entry.person
+            row[2] = entry.category.name
+
+            for entrylineitem in entry.line_items.all():
+                if entrylineitem.key not in headers:
+                    headers_index[entrylineitem.key] = len(headers)
+                    headers.append(entrylineitem.key)
+                    row.append('')
+
+                row[headers_index[entrylineitem.key]] = entrylineitem.value
+
+            data.append(row)
+
+        context['data'] = data
+        context['headers'] = headers
+        context['paginator'] = entries_paginated
+
+        return context
+
+    def get_number_by_representative_view(self, context):
+        #numberbyrepresentative view - number of declarations per person per category
+        context['layout'] = 'numberbyrepresentative'
+
+        #custom sql used as
+        #Entry.objects.values('category', 'release', 'person').annotate(c=Count('id')).order_by('-c')
+        #returns a ValueQuerySet with foreign keys, not model instances
+        if context['category']=='all' and context['release']=='all':
+            data = Entry.objects.raw(
+                '''SELECT max(id) as id, category_id, release_id, person_id,
+                count(*) as c FROM "interests_register_entry"
+                GROUP BY category_id, release_id, person_id ORDER BY c DESC''',
+                [])
+        elif context['category'] == 'all':
+            data = Entry.objects.raw(
+                '''SELECT max(id) as id, category_id, release_id, person_id,
+                count(*) as c FROM "interests_register_entry"
+                WHERE "interests_register_entry"."release_id" = %s
+                GROUP BY category_id, release_id, person_id ORDER BY c DESC''',
+                [context['release_id']])
+        elif context['category'] != 'all' and context['release']=='all':
+            data = Entry.objects.raw(
+                '''SELECT max(id) as id, category_id, release_id, person_id,
+                count(*) as c FROM "interests_register_entry"
+                WHERE "interests_register_entry"."category_id" = %s
+                GROUP BY category_id, release_id, person_id ORDER BY c DESC''',
+                [context['category_id']])
+        else:
+            data = Entry.objects.raw(
+                '''SELECT max(id) as id, category_id, release_id, person_id,
+                count(*) as c FROM "interests_register_entry"
+                WHERE "interests_register_entry"."category_id" = %s
+                AND "interests_register_entry"."release_id" = %s
+                GROUP BY category_id, release_id, person_id ORDER BY c DESC''',
+                [context['category_id'], context['release_id']])
+
+        paginator = Paginator(data, 20)
+        paginator._count = len(list(data))
+        page = self.request.GET.get('page')
+
+        try:
+            data_paginated = paginator.page(page)
+        except PageNotAnInteger:
+            data_paginated = paginator.page(1)
+        except EmptyPage:
+            data_paginated = paginator.page(paginator.num_pages)
+
+        context['data'] = data_paginated
+
+        return context
+
+    def get_numberbysource_view(self, context):
+        #numberbysource view - number of declarations by source per category
+        context['layout'] = 'numberbysource'
+        if context['category']=='all' and context['release']=='all':
+            data = Entry.objects.raw(
+                '''SELECT max("interests_register_entrylineitem"."id") as id,
+                value, count(*) as c, release_id, category_id
+                FROM "interests_register_entrylineitem"
+                INNER JOIN "interests_register_entry"
+                ON ("interests_register_entrylineitem"."entry_id"
+                = "interests_register_entry"."id")
+                WHERE ("interests_register_entrylineitem"."key" = 'Source')
+                GROUP BY value, release_id, category_id ORDER BY c DESC''',
+                [])
+        elif context['category'] == 'all':
+            data = Entry.objects.raw(
+                '''SELECT max("interests_register_entrylineitem"."id") as id,
+                value, count(*) as c, release_id, category_id
+                FROM "interests_register_entrylineitem"
+                INNER JOIN "interests_register_entry"
+                ON ("interests_register_entrylineitem"."entry_id"
+                = "interests_register_entry"."id")
+                WHERE ("interests_register_entrylineitem"."key" = 'Source'
+                AND "interests_register_entry"."release_id" = %s)
+                GROUP BY value, release_id, category_id ORDER BY c DESC''',
+                [context['release_id']])
+        elif context['release']=='all':
+            data = Entry.objects.raw(
+                '''SELECT max("interests_register_entrylineitem"."id") as id,
+                value, count(*) as c, release_id, category_id
+                FROM "interests_register_entrylineitem"
+                INNER JOIN "interests_register_entry"
+                ON ("interests_register_entrylineitem"."entry_id"
+                = "interests_register_entry"."id")
+                WHERE ("interests_register_entry"."category_id" = %s
+                AND "interests_register_entrylineitem"."key" = 'Source')
+                GROUP BY value, release_id, category_id ORDER BY c DESC''',
+                [context['category_id']])
+        else:
+            data = Entry.objects.raw(
+                '''SELECT max("interests_register_entrylineitem"."id") as id,
+                value, count(*) as c, release_id, category_id
+                FROM "interests_register_entrylineitem"
+                INNER JOIN "interests_register_entry"
+                ON ("interests_register_entrylineitem"."entry_id"
+                = "interests_register_entry"."id")
+                WHERE ("interests_register_entry"."category_id" = %s
+                AND "interests_register_entrylineitem"."key" = 'Source'
+                AND "interests_register_entry"."release_id" = %s)
+                GROUP BY value, release_id, category_id ORDER BY c DESC''',
+                [context['category_id'], context['release_id']])
+
+        paginator = Paginator(data, 20)
+        paginator._count = len(list(data))
+        page = self.request.GET.get('page')
+
+        try:
+            data_paginated = paginator.page(page)
+        except PageNotAnInteger:
+            data_paginated = paginator.page(1)
+        except EmptyPage:
+            data_paginated = paginator.page(paginator.num_pages)
+
+        context['data'] = data_paginated
+
+        return context
+
+class SAMembersInterestsSource(TemplateView):
+    template_name = "interests_register/source.html"
+
+    def get_context_data(self, **kwargs):
+        context = {}
+
+        context['categories'] = Category.objects.filter(
+            slug__in = ['sponsorships',
+                        'gifts-and-hospitality',
+                        'benefits',
+                        'pensions'])
+        context['releases'] = Release.objects.all()
+
+        context['source'] = ''
+        context['match'] = 'absolute'
+        context['category'] = context['categories'][0].slug
+        context['release'] = 'all'
+
+        for key in ('source', 'match', 'category', 'release'):
+            if key in self.request.GET:
+                context[key] = self.request.GET[key]
+
+        #if a source is specified perform the search
+        if context['source']!='':
+            if context['match']=='absolute':
+                entries = Entry.objects.filter(line_items__key='Source',
+                    line_items__value=context['source'],
+                    category__slug=context['category'])
+            else:
+                entries = Entry.objects.filter(line_items__key='Source',
+                    line_items__value__contains=context['source'],
+                    category__slug=context['category'])
+
+            if context['release']!='all':
+                entries = entries.filter(release__slug=context['release'])
+
+            paginator = Paginator(entries, 25)
+            page = self.request.GET.get('page')
+
+            try:
+                entries_paginated = paginator.page(page)
+            except PageNotAnInteger:
+                entries_paginated = paginator.page(1)
+            except EmptyPage:
+                entries_paginated = paginator.page(paginator.num_pages)
+
+            #tabulate the data
+            headers = ['Year', 'Person', 'Type']
+            headers_index = {'Year': 0, 'Person': 1, 'Type': 2}
+            data = []
+            for entry in entries_paginated:
+                row = ['']*len(headers)
+                row[0] = entry.release.date.year
+                row[1] = entry.person
+                row[2] = entry.category.name
+
+                for entrylineitem in entry.line_items.all():
+                    if entrylineitem.key not in headers:
+                        headers_index[entrylineitem.key] = len(headers)
+                        headers.append(entrylineitem.key)
+                        row.append('')
+
+                    row[headers_index[entrylineitem.key]] = entrylineitem.value
+
+                data.append(row)
+
+            context['data'] = data
+            context['headers'] = headers
+            context['paginator'] = entries_paginated
+
+        else:
+            context['data'] = None
 
         return context

pombola/settings/south_africa_base.py
@@ -72,5 +72,12 @@ COUNTRY_JS = {
         ),
         'output_filename': 'js/advanced-search.js',
         'template_name': 'pipeline/js-array.html',
+    },
+    'interests-filter': {
+        'source_filenames' : (
+            'js/interests-filter.js',
+        ),
+        'output_filename': 'js/interests-filter.js',
+        'template_name': 'pipeline/js-array.html',
     }
 }

pombola/south_africa/static/js/interests-filter.js
@@ -1,0 +1,13 @@
+jQuery (function($) {
+    $('#display').change(function() {
+        if ($('#display').val()!='all') {
+            $('#organisation').prop( "disabled", true );
+            $('#party').prop( "disabled", true );
+        }
+        else {
+            $('#organisation').prop( "disabled", false );
+            $('#party').prop( "disabled", false );
+        }
+    });
+    $('#display').trigger("change");
+});

pombola/south_africa/static/sass/_south-africa_overrides.scss
@@ -2547,3 +2547,28 @@ ul.pager {
         }
     }
 }
+
+.members-interests {
+    .members-interests-filter {
+        padding: 1em;
+        background-color: $colour_lighter_grey;
+        vertical-align: top;
+
+        label {
+            width: 90px;
+            display: inline-block;
+        }
+
+        .filter-option {
+            height: 30px;
+        }
+
+        .button {
+            margin-top: 1em;
+        }
+
+        select, input[type="text"] {
+            vertical-align: top;
+        }
+    }
+}

pombola/south_africa/templates/interests_register/index.html
@@ -1,0 +1,107 @@
+{% extends 'base.html' %}
+
+{% load compressed %}
+
+{% block title %}Members' Interests{% endblock %}
+
+{% block extra_js_to_load %}
+    addExtraJS([ {% compressed_js 'interests-filter' %} null ]);
+{% endblock %}
+
+{% block body_attributes %} class="members-interests" {% endblock %}
+
+{% block content %}
+<div class="interests-header">
+    <div class="wrapper">
+        <h1>Members' Interests</h1>
+    </div>
+</div>
+
+<h3>Filter</h3>
+
+<div class="members-interests-filter">
+<form action="{% url 'sa-interests-index' %}" method="get">
+
+<div class="filter-option">
+    <label for="display">Display:</label>
+    <select name="display" id="display">
+        <option value="all"{% if display == "all" %} selected="selected"{% endif %}>
+            All Declarations
+        </option>
+        <option value="numberbyrepresentative"{% if display == "numberbyrepresentative" %} selected="selected"{% endif %}>
+            Number of Declarations per MP
+        </option>
+        <option value="numberbysource"{% if display == "numberbysource" %} selected="selected"{% endif %}>
+            Number of Declarations per Source (gifts, benefits, pensions, and sponsorships only)
+        </option>
+    </select>
+</div>
+
+<div class="filter-option">
+    <label for="category">Category:</label>
+    <select name="category">
+        <option value="all"{% if category == "all" %} selected="selected"{% endif %}>All</option>
+        {% for c in categories %}
+          <option value="{{ c.slug }}"{% if category == c.slug %} selected="selected"{% endif %}>
+              {{ c.name }}
+          </option>
+        {% endfor %}
+    </select>
+</div>
+
+<div class="filter-option">
+    <label for="party">Party:</label>
+    <select name="party" id="party">
+        <option value="all"{% if party == "all" %} selected="selected"{% endif %}>All</option>
+        {% for o in parties %}
+          <option value="{{ o.slug }}"{% if party == o.slug %} selected="selected"{% endif %}>
+              {{ o.name }}
+          </option>
+        {% endfor %}
+    </select>
+</div>
+
+<div class="filter-option">
+    <label for="organisation">Member of:</label>
+    <select name="organisation" id="organisation">
+        <option value="all"{% if organisation == "all" %} selected="selected"{% endif %}>All</option>
+        {% for o in organisations %}
+          {% ifchanged o.kind %}
+            {% if not forloop.first %}</optgroup>{% endif %}
+            <optgroup label="{{ o.kind.name }}">
+          {% endifchanged %}
+            <option value="{{ o.slug }}"{% if organisation == o.slug %} selected="selected"{% endif %}>{{ o.name }}</option>
+        {% endfor %}
+        </optgroup>
+    </select>
+</div>
+
+<div class="filter-option">
+    <label for="release">Year:</label>
+    <select name="release">
+        <option value="all"{% if release == "all" %} selected="selected"{% endif %}>All</option>
+        {% for r in releases %}
+          <option value="{{ r.slug }}"{% if release == r.slug %} selected="selected"{% endif %}>
+              {{ r.name }}
+          </option>
+        {% endfor %}
+    </select>
+</div>
+
+<input type="submit" value="Filter" class="button">
+
+</form>
+</div>
+
+{% if layout == "numberbyrepresentative" %}
+  {% include "interests_register/index_numberbyrepresentative.html" %}
+{% elif layout == "numberbysource" %}
+  {% include "interests_register/index_numberbysource.html" %}
+{% elif layout == "complete" %}
+  {% include "interests_register/index_complete.html" %}
+{% elif layout == "section" %}
+  {% include "interests_register/index_section.html" %}
+{% endif %}
+
+
+{% endblock %}

pombola/south_africa/templates/interests_register/index_complete.html
@@ -1,0 +1,49 @@
+{% load i18n %}
+
+{% for person in data %}
+    <h3>
+        <a href="{{ person.person.get_absolute_url }}">{{ person.person.name }}</a>
+         - {{ person.year }}
+    </h3>
+
+    {% for category in person.data %}
+      <h4>{{ category.category.name }}</h4>
+      <table>
+          <tr>
+              {% for header in category.headers %}
+                  <th>{{ header }}</th>
+              {% endfor %}
+          </tr>
+          {% for row in category.data %}
+            <tr>
+                {% for cell in row %}
+                  <td>{{ cell }}</td>
+                {% endfor %}
+            </tr>
+          {% endfor %}
+      </table>
+    {% endfor %}
+{% endfor %}
+
+
+<div class="step-pagination">
+    <span class="step-links">
+        {% if paginator.has_previous %}
+          <a href="?display={{ display }}&category={{ category }}&party={{ party }}&organisation={{ organisation }}&release={{ release }}&page={{ paginator.previous_page_number }}">
+              {% trans "Previous" %}
+          </a>
+        {% endif %}
+
+        <span class="current">
+          {% blocktrans with num=paginator.number pages=paginator.paginator.num_pages %}
+            Page <strong>{{ num }}</strong> of <strong>{{ pages }}</strong>
+          {% endblocktrans %}
+        </span>
+
+        {% if paginator.has_next %}
+          <a href="?display={{ display }}&category={{ category }}&party={{ party }}&organisation={{ organisation }}&release={{ release }}&page={{ paginator.next_page_number }}">
+              {% trans "Next" %}
+          </a>
+        {% endif %}
+    </span>
+</div>

pombola/south_africa/templates/interests_register/index_numberbyrepresentative.html
@@ -1,0 +1,41 @@
+{% load i18n %}
+
+<table>
+<tr>
+    <th>Year</th>
+    <th>Person</th>
+    <th>Category</th>
+    <th>Number of Declarations</th>
+</tr>
+
+{% for row in data %}
+  <tr>
+      <td>{{ row.release.date.year }}</td>
+      <td><a href="{{ row.person.get_absolute_url }}">{{ row.person.name }} ({{ row.person.parties.0.slug|upper }})</a></td>
+      <td>{{ row.category }}</td>
+      <td>{{ row.c }}</td>
+  </tr>
+{% endfor %}
+</table>
+
+<div class="step-pagination">
+    <span class="step-links">
+        {% if data.has_previous %}
+          <a href="?display={{ display }}&category={{ category }}&party={{ party }}&organisation={{ organisation }}&release={{ release }}&page={{ data.previous_page_number }}">
+              {% trans "Previous" %}
+          </a>
+        {% endif %}
+
+        <span class="current">
+          {% blocktrans with num=data.number pages=data.paginator.num_pages %}
+            Page <strong>{{ num }}</strong> of <strong>{{ pages }}</strong>
+          {% endblocktrans %}
+        </span>
+
+        {% if data.has_next %}
+          <a href="?display={{ display }}&category={{ category }}&party={{ party }}&organisation={{ organisation }}&release={{ release }}&page={{ data.next_page_number }}">
+              {% trans "Next" %}
+          </a>
+        {% endif %}
+    </span>
+</div>

pombola/south_africa/templates/interests_register/index_numberbysource.html
@@ -1,0 +1,44 @@
+{% load i18n %}
+<table>
+<tr>
+    <th>Year</th>
+    <th>Source</th>
+    <th>Category</th>
+    <th>Number Declared</th>
+    <th></th>
+</tr>
+
+{% for row in data %}
+  <tr>
+      <td>{{ row.release.date.year }}</td>
+      <td>{{ row.value }}</td>
+      <td>{{ row.category }}</td>
+      <td>{{ row.c }}</td>
+      <td>
+          <a href="{% url 'sa-interests-source' %}?release={{ row.release.slug }}&category={{ row.category.slug }}&match=absolute&source={{ row.value }}">View Declarations</a>
+      </td>
+  </tr>
+{% endfor %}
+</table>
+
+<div class="step-pagination">
+    <span class="step-links">
+        {% if data.has_previous %}
+          <a href="?display={{ display }}&category={{ category }}&party={{ party }}&organisation={{ organisation }}&release={{ release }}&page={{ data.previous_page_number }}">
+            {% trans "Previous" %}
+          </a>
+        {% endif %}
+
+        <span class="current">
+          {% blocktrans with num=data.number pages=data.paginator.num_pages %}
+            Page <strong>{{ num }}</strong> of <strong>{{ pages }}</strong>
+          {% endblocktrans %}
+        </span>
+
+        {% if data.has_next %}
+          <a href="?display={{ display }}&category={{ category }}&party={{ party }}&organisation={{ organisation }}&release={{ release }}&page={{ data.next_page_number }}">
+              {% trans "Next" %}
+          </a>
+        {% endif %}
+    </span>
+</div>

commit e0740ce7caf441e35ed2aaf6deb55d1bd81e9e03

Fix model name clash in home view
Addresses a bug inadvertently introduced in 7db85cb where a second
Category model is imported, causing the code at the bottom of the home
page which renders the top post in the Comment, MP Corner and Advocacy
blog categories to silently fail

pombola/south_africa/views.py
@@ -30,7 +30,7 @@ from pombola.core import models
 from pombola.core.views import (HomeView, BasePlaceDetailView, PlaceDetailView,
     PlaceDetailSub, OrganisationDetailView, PersonDetail, PlaceDetailView,
     OrganisationDetailSub, PersonSpeakerMappingsMixin)
-from pombola.info.models import InfoPage, Category
+from pombola.info.models import InfoPage, Category as BlogCategory
 from pombola.info.views import InfoPageView
 from pombola.search.views import GeocoderView, SearchBaseView
 from pombola.slug_helpers.views import SlugRedirect
@@ -52,6 +52,7 @@ class SAHomeView(HomeView):
 
     def get_context_data(self, **kwargs):
         context = super(SAHomeView, self).get_context_data(**kwargs)
+
         articles = InfoPage.objects.filter(
             kind=InfoPage.KIND_BLOG).order_by("-publication_date")
 
@@ -71,11 +72,11 @@ class SAHomeView(HomeView):
         context['other_news_categories'] = []
         for slug in ('advocacy-campaigns', 'commentary', 'mp-corner'):
             try:
-                c = Category.objects.get(slug=slug)
+                c = BlogCategory.objects.get(slug=slug)
                 context['other_news_categories'].append(
                     (c, articles.filter(categories=c)[:1]))
-            except Category.DoesNotExist:
-                pass
+            except BlogCategory.DoesNotExist:
+               pass
 
         # If there is editable homepage content make it available to the templates.
         try: