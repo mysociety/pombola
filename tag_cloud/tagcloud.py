8485fed3e5f1e0acbf1e13a9f31f67b16067eb3a
Duncan Parkes <duncan@mysociety.org>
2015-06-15 15:28:30 +0100

    Add basic word cloud implementation to main pombola directory.
    The cloud is based on the last 20 Entry objects. There were a couple
    of attempts at this in the code already, but
    it hadn't been working and was effectively overridden by code
    which just used the search index of all documents.
    
    To do this I've added sitting_start_date to the search index for
    hansard entries. This index will need rebuilding with
    
    python manage.py rebuild_index

---
+++ tag_cloud/tagcloud.py
@@ -0,0 +1,36 @@
+import os
+from operator import itemgetter
+
+from haystack.query import SearchQuerySet
+from pombola.hansard import models as hansard_models
+
+
+BASEDIR = os.path.dirname(__file__)
+# normal english stop words and hansard-centric words to ignore
+STOP_WORDS = open(os.path.join(BASEDIR, 'stopwords.txt'), 'rU').read().splitlines()
+
+
+def popular_words(max_entries=20):
+    sqs = SearchQuerySet().models(hansard_models.Entry).order_by('-sitting_start_date')
+
+    cloudlist = []
+
+    try:
+        # Generate tag cloud from content of returned entries
+        words = {}
+        for entry in sqs[:max_entries]:
+            text = entry.object.content
+
+            for x in text.lower().split():
+                cleanx = x.replace(',', '').replace('.', '').replace('"', '').strip()
+                if cleanx not in STOP_WORDS:  # and not cleanx in hansard_words:
+                    words[cleanx] = 1 + words.get(cleanx, 0)
+
+        for word in words:
+            cloudlist.append({"text": word, "weight": words.get(word), "link": "/search/hansard/?q=%s" % word})
+
+        sortedlist = sorted(cloudlist, key=itemgetter('weight'), reverse=True)[:25]
+    except:
+        sortedlist = []
+
+    return sortedlist
