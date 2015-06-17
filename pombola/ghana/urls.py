76abd68ebc14c3cee3b830b1f85bb4e2971dad5f
Mark Longair <mhl@pobox.com>
2015-06-17 16:29:35 +0100

    Ghana: the intro page was Kenya-specific and broken, so remove it

--- pombola/ghana/urls.py
+++ pombola/ghana/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, url, include
-from django.views.generic import TemplateView
 
 from .views import data_upload, info_page_upload
 
 urlpatterns = patterns('',
-    url(r'^intro$', TemplateView.as_view(template_name='intro.html')),
     url(r'^data/upload/mps/$', data_upload, name='data_upload'),
     url(r'^data/upload/info-page/$', info_page_upload, name='info_page_upload'),
     url('', include('django.contrib.auth.urls')),
