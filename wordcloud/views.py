5cbe2e219197917b5a90ddcce5bbe5bf4269b94b
Duncan Parkes <duncan@mysociety.org>
2015-06-22 16:12:32 +0100

    Specify max_entries as a parameter.

--- wordcloud/views.py
+++ wordcloud/views.py
@@ -4,10 +4,10 @@ from django.utils import simplejson
 from .wordcloud import popular_words
 
 
-def wordcloud(request, n=30):
+def wordcloud(request, max_entries=30):
     """ Return tag cloud JSON results"""
     return HttpResponse(
-        simplejson.dumps(popular_words()),
+        simplejson.dumps(popular_words(max_entries=max_entries)),
         mimetype='application/json'
     )
 
