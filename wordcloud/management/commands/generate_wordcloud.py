e95b64a631a67853c5f425aebc256b5d8680ba84
Duncan Parkes <duncan@mysociety.org>
2015-06-23 09:37:26 +0100

    Allow caching of the wordcloud via a management command.
    Add a couple of settings to store the location of a wordcloud
    cache file in a subdirectory of the root_dir.
    
    The wordcloud now uses the file at wordcloud_cache/wordcloud.json
    if it exists, and dynamically creates it otherwise.
    
    The management command creates the directory if it is missing and
    then populates it with a wordcloud file.

--- wordcloud/management/commands/generate_wordcloud.py
+++ wordcloud/management/commands/generate_wordcloud.py
@@ -1,20 +1,42 @@
+import errno
+import os
+
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import simplejson
+from django.conf import settings
 
 from wordcloud.wordcloud import popular_words
 
 
 class Command(BaseCommand):
-    """Generate json file for wordcloud and store in file"""
+    """Generate json file for wordcloud.
+
+    A single argument, a file path, can be passed in. This will
+    be appended to settings.WORDCLOUD_CACHE_DIR to produce the location
+    to store the json. If no argument is passed, it defaults to
+    'wordcloud.json'. If the file path is absolute it will just be used
+    as is.
+    """
 
     help = 'Generate wordcloud data'
     args = '<file>'
 
     def handle(self, *args, **options):
-        # file = open(args[])
-        if len(args) != 1:
-            raise CommandError
+        if len(args) > 1:
+            raise CommandError(
+                "Usage: python manage.py generate_wordcloud [filepath]")
 
-        path = args[0]
-        with open(path, 'w') as outfile:
-            simplejson.dump(popular_words(max_entries=30), outfile)
+        cache_dir = settings.WORDCLOUD_CACHE_DIR
+        try:
+            os.mkdir(cache_dir)
+        except OSError as exc:
+            # Probably the directory exists already
+            if exc.errno == errno.EEXIST and os.path.isdir(cache_dir):
+                pass
+            else:
+                raise
+
+        cache_path = os.path.join(cache_dir, args[0]) if args else settings.WORDCLOUD_CACHE_PATH
+
+        with open(cache_path, 'w') as cache_file:
+            simplejson.dump(popular_words(max_entries=30), cache_file)
