69cfbce444f520e7d45dc402aebe6e89f7163166
Mark Longair <mhl@pobox.com>
2015-06-26 19:53:41 +0100

    Improve the Javascript enhancement of the Youth Employment experiment page

--- pombola/kenya/static/js/click-tracking.js
+++ pombola/kenya/static/js/click-tracking.js
@@ -24,22 +24,52 @@
 // process form submits without leaving the page
 (function () {
   $.each(['.inplace-form'], function(_, cssSelector) {
+    var form = $(this);
+    /* There doesn't seem to be any particularly nice way of finding
+       which submit button was clicked from jQuery's submit event.
+       One way is to create a hidden input element with the submit
+       button's name and value on clicking of that button, suggested
+       here: http://stackoverflow.com/a/11271850/223092 */
+    form.find(':submit').click(function (e) {
+      if ($(this).attr('name')) {
+        $(form).append(
+          $('<input type="hidden">').attr({
+            name: $(this).attr('name'),
+            value: $(this).attr('value')
+          })
+        );
+      }
+    });
     $(cssSelector).on('submit', function(e) {
-      data = $(this).serialize();
+      var data = $(this).serialize(),
+        url = this.action,
+        panel = $(this).parent()[0],
+        thanks = $('#' + panel.id + 'thanks');
       e.preventDefault();
-      url = this.action;
-      // post the form in the background
+      /* If this is the "support" form, make sure that the constituency
+         field is set, or give an error: */
+      if ($(this).attr('id') == 'support') {
+        if (!$(this).find('select[name="constituencies"]').val()) {
+          alert('You must select a constituency first');
+          return;
+        }
+      }
+      /* We'll post the form as an AJAX request in the background, so
+         set the 'thanks' field to indicate that it's being
+         submitted */
+      $(panel).hide();
+      $(thanks).text('Submitting your response...')
+      $(thanks).show();
       $.ajax({
         url: url,
         type: 'POST',
         data: data
+      }).done(function () {
+        $(thanks).text('Thank-you for your input');
+      }).fail(function () {
+        $(thanks).text('Submission failed; you can try again');
+        $(panel).show();
       });
-      // hide the panel with the form
-      // unhide the pre-prepared thankyou panel
-      panel = $(this).parent()[0];
-      $(panel).hide();
-      thanks = $('#' + panel.id + 'thanks');
-      $(thanks).show();
       if (navigator.userAgent.indexOf('Opera Mini') > -1) {
         $(document).scrollTop(thanks.offsetTop);
       }
