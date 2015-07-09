ad691409202a2b555598e700fa9c1fc3e8a1fc6a
Duncan Parkes <duncan@mysociety.org>
2015-07-09 09:05:10 +0100

    Temporarily keep the old tagcloud URLs.

--- wordcloud/urls.py
+++ wordcloud/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.generic.base import RedirectView
 
 from .views import wordcloud
 
@@ -6,4 +7,10 @@ urlpatterns = patterns(
     '',
     url(r'^wordcloud/$', wordcloud, name='wordcloud'),
     url(r'^wordcloud/(?P<max_entries>\d+)/$', 'wordcloud', name='wordcloud'),
+
+    # Temporary redirects of old urls
+    url(r'^tagcloud/$',
+        RedirectView.as_view(pattern_name='wordcloud', permanent=True)),
+    url(r'^tagcloud/(?P<max_entries>\d+)/$$',
+        RedirectView.as_view(pattern_name='wordcloud', permanent=True)),
     )
