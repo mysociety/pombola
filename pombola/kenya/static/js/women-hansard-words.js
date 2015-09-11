54dd61b93f46a0a6db1652686c40d59fe2c683ad
Mark Longair <mhl@pobox.com>
2015-09-11 12:51:42 +0100

    Add a wordcloud showing the most used words by women parliamentarians
    We ignored very common English words (e.g. 'and', 'of', 'it', etc.)
    but this will probably want some further refinement after feedback
    from Jessica.

---
+++ pombola/kenya/static/js/women-hansard-words.js
@@ -0,0 +1,252 @@
+var womenHansardWords = [
+    {
+        "link": "/search/hansard/?q=report", 
+        "text": "report", 
+        "weight": 7.249215057114389
+    }, 
+    {
+        "link": "/search/hansard/?q=hon", 
+        "text": "hon", 
+        "weight": 7.226936018493289
+    }, 
+    {
+        "link": "/search/hansard/?q=speaker", 
+        "text": "speaker", 
+        "weight": 7.062191632286556
+    }, 
+    {
+        "link": "/search/hansard/?q=version", 
+        "text": "version", 
+        "weight": 6.926577033222725
+    }, 
+    {
+        "link": "/search/hansard/?q=hansard", 
+        "text": "hansard", 
+        "weight": 6.923628628138427
+    }, 
+    {
+        "link": "/search/hansard/?q=committee", 
+        "text": "committee", 
+        "weight": 6.893656354602635
+    }, 
+    {
+        "link": "/search/hansard/?q=bill", 
+        "text": "bill", 
+        "weight": 6.7650389767805414
+    }, 
+    {
+        "link": "/search/hansard/?q=thank", 
+        "text": "thank", 
+        "weight": 6.697034247666484
+    }, 
+    {
+        "link": "/search/hansard/?q=people", 
+        "text": "people", 
+        "weight": 6.612041034833092
+    }, 
+    {
+        "link": "/search/hansard/?q=deputy", 
+        "text": "deputy", 
+        "weight": 6.6052979209482015
+    }, 
+    {
+        "link": "/search/hansard/?q=temporary", 
+        "text": "temporary", 
+        "weight": 6.598509028614515
+    }, 
+    {
+        "link": "/search/hansard/?q=house", 
+        "text": "house", 
+        "weight": 6.539585955617669
+    }, 
+    {
+        "link": "/search/hansard/?q=information", 
+        "text": "information", 
+        "weight": 6.480044561926653
+    }, 
+    {
+        "link": "/search/hansard/?q=country", 
+        "text": "country", 
+        "weight": 6.421622267806518
+    }, 
+    {
+        "link": "/search/hansard/?q=senate", 
+        "text": "senate", 
+        "weight": 6.380122536899765
+    }, 
+    {
+        "link": "/search/hansard/?q=support", 
+        "text": "support", 
+        "weight": 6.376726947898627
+    }, 
+    {
+        "link": "/search/hansard/?q=obtained", 
+        "text": "obtained", 
+        "weight": 6.238324625039508
+    }, 
+    {
+        "link": "/search/hansard/?q=electronic", 
+        "text": "electronic", 
+        "weight": 6.234410725718371
+    }, 
+    {
+        "link": "/search/hansard/?q=time", 
+        "text": "time", 
+        "weight": 6.2324480165505225
+    }, 
+    {
+        "link": "/search/hansard/?q=certified", 
+        "text": "certified", 
+        "weight": 6.230481447578482
+    }, 
+    {
+        "link": "/search/hansard/?q=editor", 
+        "text": "editor", 
+        "weight": 6.2285110035911835
+    }, 
+    {
+        "link": "/search/hansard/?q=government", 
+        "text": "government", 
+        "weight": 6.202535517187923
+    }, 
+    {
+        "link": "/search/hansard/?q=going", 
+        "text": "going", 
+        "weight": 6.184148890937483
+    }, 
+    {
+        "link": "/search/hansard/?q=national", 
+        "text": "national", 
+        "weight": 6.1463292576688975
+    }, 
+    {
+        "link": "/search/hansard/?q=issue", 
+        "text": "issue", 
+        "weight": 6.118097198041348
+    }, 
+    {
+        "link": "/search/hansard/?q=issues", 
+        "text": "issues", 
+        "weight": 6.07993319509559
+    }, 
+    {
+        "link": "/search/hansard/?q=county", 
+        "text": "county", 
+        "weight": 6.068425588244111
+    }, 
+    {
+        "link": "/search/hansard/?q=sir", 
+        "text": "sir", 
+        "weight": 6.052089168924417
+    }, 
+    {
+        "link": "/search/hansard/?q=president", 
+        "text": "president", 
+        "weight": 6.0330862217988015
+    }, 
+    {
+        "link": "/search/hansard/?q=kenya", 
+        "text": "kenya", 
+        "weight": 6.0112671744041615
+    }, 
+    {
+        "link": "/search/hansard/?q=members", 
+        "text": "members", 
+        "weight": 6.0014148779611505
+    }, 
+    {
+        "link": "/search/hansard/?q=women", 
+        "text": "women", 
+        "weight": 5.940171252720432
+    }, 
+    {
+        "link": "/search/hansard/?q=official", 
+        "text": "official", 
+        "weight": 5.886104031450156
+    }, 
+    {
+        "link": "/search/hansard/?q=order", 
+        "text": "order", 
+        "weight": 5.762051382780177
+    }, 
+    {
+        "link": "/search/hansard/?q=children", 
+        "text": "children", 
+        "weight": 5.726847747587197
+    }, 
+    {
+        "link": "/search/hansard/?q=being", 
+        "text": "being", 
+        "weight": 5.717027701406222
+    }, 
+    {
+        "link": "/search/hansard/?q=health", 
+        "text": "health", 
+        "weight": 5.71042701737487
+    }, 
+    {
+        "link": "/search/hansard/?q=motion", 
+        "text": "motion", 
+        "weight": 5.700443573390687
+    }, 
+    {
+        "link": "/search/hansard/?q=kenyans", 
+        "text": "kenyans", 
+        "weight": 5.683579767338681
+    }, 
+    {
+        "link": "/search/hansard/?q=important", 
+        "text": "important", 
+        "weight": 5.66988092298052
+    }, 
+    {
+        "link": "/search/hansard/?q=chairman", 
+        "text": "chairman", 
+        "weight": 5.652489180268651
+    }, 
+    {
+        "link": "/search/hansard/?q=give", 
+        "text": "give", 
+        "weight": 5.648974238161206
+    }, 
+    {
+        "link": "/search/hansard/?q=money", 
+        "text": "money", 
+        "weight": 5.631211781821365
+    }, 
+    {
+        "link": "/search/hansard/?q=public", 
+        "text": "public", 
+        "weight": 5.605802066295998
+    }, 
+    {
+        "link": "/search/hansard/?q=law", 
+        "text": "law", 
+        "weight": 5.598421958998375
+    }, 
+    {
+        "link": "/search/hansard/?q=parliament", 
+        "text": "parliament", 
+        "weight": 5.552959584921617
+    }, 
+    {
+        "link": "/search/hansard/?q=security", 
+        "text": "security", 
+        "weight": 5.5254529391317835
+    }, 
+    {
+        "link": "/search/hansard/?q=good", 
+        "text": "good", 
+        "weight": 5.521460917862246
+    }, 
+    {
+        "link": "/search/hansard/?q=counties", 
+        "text": "counties", 
+        "weight": 5.5093883366279774
+    }, 
+    {
+        "link": "/search/hansard/?q=given", 
+        "text": "given", 
+        "weight": 5.488937726156687
+    }
+]
