abcdb86a20ba24b60aacd83e71c3031f2d5ad5f5
Zarino Zappia <mail@zarino.co.uk>
2015-09-23 11:18:46 +0100

    Remove chevron-{down,up}-alt.png and associated css, img, and js
    chevron-down-alt.png was used in two places: first on
    `h2 a.appearances-trigger` (which doesn’t seem to be used
    in any templates any more) and also in mobile-functions.js
    which adds a button if a `.page-title` element has a
    `data-sub-menu-id` attribute. The attribute is only
    present in the default infopage_detail.html template,
    but that isn’t used by any views, and is only extended by
    south_africa/info_newsletter.html which overrides the `content`
    block anyway to remove the `data-sub-menu-id`.
    
    Seems like the whole lot is legacy cruft that can just be
    removed. So that’s what I’m doing.

--- pombola/core/static/js/mobile-functions.js
+++ pombola/core/static/js/mobile-functions.js
@@ -18,7 +18,7 @@ function hideUrlBar() {
         clearInterval( bodycheck );
         scrollTop = "scrollTop" in doc.body ? doc.body.scrollTop : 1;
         win.scrollTo( 0, scrollTop === 1 ? 0 : 1 );
-      } 
+      }
     }, 15 );
 
     win.addEventListener( "load", function(){
@@ -68,25 +68,6 @@ $(function(){
     hideShow('#mc-embedded-subscribe-form', $(this));
   });
 
-  /*
-   * Get the sub-menu links if on a page with child items
-   */
-  //if .page-title has a data-sub-menu-id attr
-  //clone the relavent ul#data-sub-menu-id from in the menu
-  //stick below .page-title
-  //show button inside .page-title that toggles the ul#data-sub-menu-id
-  var sub_menu_id = '#'+$('.page-title').attr('data-sub-menu-id'),
-      $page_title = $('.page-title');
-  if($(sub_menu_id).length !== 0){
-    $page_title.addClass('has-sub-menu').append('<button class="m-sub-menu-trigger">Show sub menu</button>');
-    $(sub_menu_id).hide().insertAfter($page_title).addClass('m-sub-menu');
-  }
-
-  $('.m-sub-menu-trigger').on('click', function(e){
-    e.preventDefault();
-    hideShow(sub_menu_id, $(this));
-  });
-  
   /* carry search terms across when switching between search pages */
   $("#search-hansard-instead").click(function(e){
     e.preventDefault();
