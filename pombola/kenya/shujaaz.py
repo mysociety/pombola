f1387c94a82442218f4f2cd7ae6f0db5dc173569
Zarino Zappia <mail@zarino.co.uk>
2015-12-11 11:47:36 +0000

    Update Mzalendo to show 2015 People’s Shujaaz finalists
    Fixes #1870.

--- pombola/kenya/shujaaz.py
+++ pombola/kenya/shujaaz.py
@@ -106,13 +106,6 @@ functions and powers to county governments.</li></ul>''',
     },
 ]
 
-FINALISTS2015 = []
-
-FINALISTS_DICT = {
-    '2014': {f['person']: f for f in FINALISTS2014},
-    '2015': {f['person']: f for f in FINALISTS2015},
-}
-
 NOMINEES2015 = [
     {
         'category': 'Financial Oversight',
@@ -332,3 +325,83 @@ NOMINEES2015 = [
         'reasons': '''<h4>Report of the Departmental Committee on Lands on Land issues in Taita Taveta County:</h4><blockquote lang="sw"><p>“Ardhi hii ni mojawapo ya dhuluma ambazo zimetendewa watu wa Taita. Kamati imependekeza yafuatayo: Mipaka ya shamba hili ambayo iliwekwa mwaka wa 1992 iregelewe. Soroveya aende apime shamba hilo. Ni sawa na hamna tatizo kama mwenyewe ana ardhi ya ekari 30,000 lakini ardhi yoyote itakayopatikana juu ya ekari 30,000 irudi kwa wananchi wa Mwasima Mbuwa pale Mwatate. Watu wa Mwasima Mbuwa wamelilia ardhi hii na wamejitoa mhanga kuona kuwa haki yao inarudishwa. Kero ni kuwa hata masoroveya wa pale Wundanyi hawawezi kuingia katika shamba hilo. Tunaomba Serikali itumie nafasi hii kuhakikisha kwamba shamba hili linapimwa. La pili ni kuwa bwawa la maji lililoko pale litumiwe na watu wote. Hayo ndio makubaliano yaliyokuweko. Kituo cha polisi kiwe cha umma na watu wote wakitumie.” <a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2015-06-30-14-30-00#entry-563052">(Read more)</a></p></blockquote>'''
     }
 ]
+
+FINALISTS2015 = [
+    {
+        'category': 'Financial Oversight',
+        'name': 'Col. (Rtd) Ali Rasso Dido',
+        'person': 2749,
+        'reasons': '''<h4>The Excise Duty Bill:</h4><blockquote><p>“Reading it tells me that our tax regime must be people friendly particularly in accessing goods which our people use daily. The citizenry have been taking goods to Uganda or Tanzania and bringing them back to Kenya. In the process, that fights competition and the traders who import their good directly to Kenya get disadvantaged.” <a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2015-08-20-14-30-00#entry-577824">(Read more)</a></p></blockquote>'''
+    },
+    {
+        'category': 'Institutional Oversight',
+        'name': 'Prof. Anyang’ Nyong’o',
+        'person': 193,
+        'reasons': '''<h4>Motion on the state of Kenya Airways</h4><p>H​e proposed the Senate to establish a Select Committee to conduct an inquiry into the affairs of the Kenya Airways Limited and its subsidiaries and report to the Senate within three months, taking into account the following:</p><ol><li>The leasing and buying arrangement of aircrafts since 1996;</li><li>The role of off-shore companies in the investment affairs of the airline;</li><li>The identity of the shareholders of these off-shore companies and their relationship with the management of Kenya Airways Limited;</li><li>The employment policies and practices of personnel, including engineers, pilots, cabin crew and ground personnel;</li><li>The reason for delayed and cancelled flights, their frequency and the magnitude of losses attendant therein;</li><li>Any other matter that may shed light on the financial and management crisis currently facing the airline.</li></ol>'''
+    },
+    {
+        'category': 'Mining (Senate)',
+        'name': 'Francis Nyenze',
+        'person': 1987,
+        'reasons': '''<h4>The Petroleum Bill 2015</h4><blockquote><p>“There are a few places where I feel the committee fell short of. For instance, it has given the Cabinet Secretary (CS) too much power to negotiate contracts for petroleum. That is very wrong. It is good for the Committee to look at the possibility of having a board. A board is wider and it can consult more. Clause 46 obligates the contractor to conserve the environment and promote local industries by buying from them and adopt best practices.” <a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2015-10-27-14-30-00#entry-595306">(Read more)</a></p></blockquote>'''
+    },
+    {
+        'category': 'Access to Justice',
+        'name': 'Amos Wako',
+        'person': 366,
+        'reasons': '''<h4>(Represents a committee)</h4><h4>The Committee Report on the annual Report (2012/2013) of the commission on Administrative Justice:</h4><blockquote><ol><li>T​he Commission on Administrative Justice prepares and presents to the Senate, a report on their proposed amendments to the Commission on Administrative Justice Act and any other statutory amendments, which they may consider necessary for the effective execution of their mandate.</li><li>​T​he Commission on Administrative Justice to leverage on technology to increase accessibility and efficiency in complaints handling through use of toll free numbers; social media and other platforms such as the Integrated Public Complaints Referral Mechanism, the Huduma initiative and improvement of the ICT infrastructure at the Commission.</li><li>​T​he CAJ to open branches in each of the 47 counties through partnerships with the county governments.</li></ol></blockquote>'''
+    },
+    {
+        'category': 'Devolution',
+        'name': 'Mutahi Kagwe',
+        'person': 229,
+        'reasons': '''<h4>The County Allocation of Revenue Allocation Bill</h4><blockquote><p>“Whichever way you look at it, we mediated successfully. This is because the money that we were looking for the counties went to them. It is good for us to repeat this because this is a House of record. Even if it meant that we give up our salaries to travel around the counties, forget about the oversight money, so that devolution works, I personally would be quite happy and willing to do so. If that is the sacrifice that has got to be made so that Kenya’s children can live better tomorrow, so be it.” <a href="http://info.mzalendo.com/hansard/sitting/senate/2015-06-17-14-30-00#entry-559027">(Read more)</a></p></blockquote>'''
+    },
+    {
+        'category': 'Education',
+        'name': 'Agnes Zani',
+        'person': 13119,
+        'reasons': '''<h4>Motion: Classification of Schools:</h4><blockquote><p>“Senate calls upon the Ministry of Education, Science and Technology to take immediate action to review the categorization of public secondary schools and to classify all of them as county schools in order to ensure equity in resource allocation and guarantee quality education for all.”</p></blockquote>'''
+    },
+    {
+        'category': 'Health',
+        'name': 'Cyprian Iringo',
+        'person': 1574,
+        'reasons': '''<h4>Petition: Legal recognition of Kenya Association of Private Hospitals by National Hospital Insurance Fund (NHIF):</h4><blockquote><p>“Departmental Committee on Health to consider passing legislation to give KAPH a legal personality to effectively become a bona fide representative of its members in the NHIF Board in order to play a leading role in driving provision of quality healthcare throughout the country.”</p><p>“Compel NHIF to involve all bona fide healthcare providers in consultative discussions on all aspects of service contracts, and to strictly adhere to contracts concluded with healthcare providers and be ready to compensate the provider, among other key issues.”</p></blockquote>'''
+    },
+    {
+        'category': 'Special interests',
+        'name': 'Isaac Mwaura',
+        'person': 13129,
+        'reasons': '''<h4>Petition: Construction of a footbridge on Waiyaki Way to link ABC Place Bus Stop and the National Council for Persons with Disabilities Office, Westlands, Nairobi:</h4><blockquote><ol><li>That, the National Council for Persons with Disabilities (NCPWD) which promotes the rights of persons with disabilities in Kenya amidst disability issues in all aspects of national development is allocated along the busy Waiyaki Way;</li><li>T​hat, the NCPWD offices are located across the ABC Place main stage along Waiyaki Way making the organisation inaccessible when one is arriving from the city.</li><li>Cognisant of the fact that persons with disabilities and in particular those who are physically challenged, or visually impaired who use assisting devices such as wheel-chairs, crutches and white canes encounter challenges when crossing over to and from the Council as well as other disability affiliated organisations operating in the vicinity.</li></ol></blockquote>'''
+    },
+    {
+        'category': 'Security',
+        'name': 'Fatuma Aden Dullo',
+        'person': 13125,
+        'reasons': '''<h4>Motion: Extra-judicial Killings by KWS:</h4><blockquote><ol><li>​“Noting with concern that human-wildlife conflict between the Kinna Community of Isiolo County and wildlife at Meru National Park has escalated over the years; Further noting with concern that the above conflict has led to tension between the community and the Kenya Wildlife Service (KWS) which the community accuses of kidnapping, shooting, torturing, wounding and even killing members of the community; Alarmed about the latest alleged report that on 18th May, 2015 KWS rangers clashed with the Kinna Community demonstrators, shot and killed one demonstrator and wounded several others;”</li><li>“​The Senate directs the Standing Committee on Land and Natural Resources and the Standing Committee on National Security and Foreign Relations to conduct a fact-finding mission regarding the conflict in the area with immediate effect and report back to the Senate within thirty days.”</li></ol></blockquote>'''
+    },
+    {
+        'category': 'Progressive contributions',
+        'name': 'Joyce Lay',
+        'person': 1022,
+        'reasons': '''<h4>Motion: Translation of the Constitution to Kiswahili:</h4><blockquote><p>“This House urges the National Council for Law Reporting to progressively translate the laws of Kenya into Kiswahili.”</p></blockquote>'''
+    },
+    {
+        'category': 'Business',
+        'name': 'Tom Kajwang',
+        'person': 2712,
+        'reasons': '''<h4>The Companies Bill 2014:</h4><blockquote><p>“We are proposing that any Wanjiku can simply go to the Registrar, get prepared forms, fill them, deposit them with the Registrar and she will have a business vehicle that she can use for trade. This is because the small business people are able to effectively participate in business, that data is captured by the Kenya Revenue Authority (KRA) and everybody who does some business is able to participate in the growth of the nation by paying taxes. So, the Bill creates efficiency, swiftness and is friendly to wananchi because it is simple to understand.” <a href="http://info.mzalendo.com/hansard/sitting/national_assembly/2015-06-30-14-30-00#entry-563209">(Read more)</a></p></blockquote>'''
+    },
+    {
+        'category': 'Land',
+        'name': 'Mutula Kilonzo Jnr',
+        'person': 13156,
+        'reasons': '''<h4>Motion: Historical Land Injustices:</h4><blockquote><p>“T​his Senate urges the National Land Commission to urgently recommend to the Senate a Bill to address historical land injustices to provide a framework to ensure that land is properly utilized for the economic benefit of the affected communities.”</p></blockquote>'''
+    }
+]
+
+FINALISTS_DICT = {
+    '2014': {f['person']: f for f in FINALISTS2014},
+    '2015': {f['person']: f for f in FINALISTS2015},
+}
