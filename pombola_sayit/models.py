02e9276f095d742708af7be5b6158f25389f7efe
Mark Longair <mhl@pobox.com>
2016-01-29 07:54:56 +0000

    Add a model to link SayIt speakers with Pombola people

--- pombola_sayit/models.py
+++ pombola_sayit/models.py
@@ -1,2 +1,15 @@
-# In Django versions earlier than 1.7, Django applications require
-# a models.py file, even if it's empty.
+from django.db import models
+
+from pombola.core.models import Person
+from speeches.models import Speaker
+
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^instances\.fields\.DNSLabelField"])
+
+
+class PombolaSayItJoin(models.Model):
+
+    """This model provides a join table between Pombola and SsayIt people"""
+
+    pombola_person = models.OneToOneField(Person, related_name='sayit_link')
+    sayit_speaker = models.OneToOneField(Speaker, related_name='pombola_link')
