1f247becff973b5378b02e091bca71afb7a60e78
Duncan Parkes <duncan@mysociety.org>
2016-02-23 16:56:22 +0000

    Set the default analyzer to use asciifolding.
    Setting the default analyzer to use asciifolding results in
    'Desiree' and 'Désirée' both producing all the results containing
    either. I'm not yet sure what other options the default analyzer
    should have - it's not something I can see any way to query - so
    this may lose us some functionality somewhere.

--- pombola/south_africa/search.py
+++ pombola/south_africa/search.py
@@ -5,35 +5,13 @@ from haystack.backends.elasticsearch_backend import (
 
 
 class ZAElasticBackend(ElasticsearchSearchBackend):
-    """Subclass in order to add and use a asciifolding analyzer - same as
-    the snowball analyzer but without the stop token filter."""
-
     def __init__(self, connection_alias, **connection_options):
-        print "HELLO FROM THE NEW BACKEND!"
         super(ZAElasticBackend, self).__init__(connection_alias, **connection_options)
 
-        self.DEFAULT_SETTINGS['settings']['analysis']['analyzer']['folding'] = {
+        self.DEFAULT_SETTINGS['settings']['analysis']['analyzer']['default'] = {
             "tokenizer": "standard",
             "filter": ["lowercase", "asciifolding"],
             }
-
-    def build_schema(self, fields):
-        content_field_name, mapping = super(ZAElasticBackend, self).build_schema(fields)
-
-        # Change all the mappings that were 'snowball' to 'folding'
-        for field_name, field_class in fields.items():
-            field_mapping = mapping[field_class.index_fieldname]
-
-            print field_name, field_class
-
-            # if field_mapping['type'] == 'string' and field_class.indexed:
-                # if not hasattr(field_class, 'facet_for') and not field_class.field_type in('ngram', 'edge_ngram'):
-                #     field_mapping["analyzer"] = "folding"
-
-            mapping.update({field_class.index_fieldname: field_mapping})
-
-        print mapping
-        return (content_field_name, mapping)
 
 
 class ZAElasticSearchEngine(ElasticsearchSearchEngine):
