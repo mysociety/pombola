403cb38391418aec22435ab3d59a8dd514f1c066
Mark Longair <mhl@pobox.com>
2016-04-06 20:47:16 +0100

    Create and use a PostGIS database

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -43,6 +43,7 @@ if 'ON_HEROKU' in os.environ:
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES = {'default': db_from_env}
+    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 else:
     DATABASES = {
         'default': {
