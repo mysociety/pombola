48585e704a0a57c17c2e0eb4f8e08da1b376b3e3
Mark Longair <mhl@pobox.com>
2016-04-06 19:50:50 +0100

    Guess types of some environment variable values

--- pombola/config.py
+++ pombola/config.py
@@ -2,11 +2,24 @@ from __future__ import print_function
 
 import os
 from os.path import abspath, realpath, join, dirname
+import re
 import sys
 
 import yaml
 
 base_dir = abspath(join(dirname(realpath(__file__)), '..'))
+
+def env_value_to_python(s):
+    if s.lower() == 'true':
+        return True
+    elif s.lower() == 'false':
+        return False
+    # Look for plain integers (positive or negative):
+    m = re.search(r'^-?\d+$', s)
+    if m:
+        return int(m.group(0), 10)
+    # Otherwise assume this really is a string:
+    return s
 
 config_path = None
 if os.environ.get('CONFIG_FROM_ENV'):
@@ -15,7 +28,7 @@ if os.environ.get('CONFIG_FROM_ENV'):
     with open(general_yml_example_fname) as f:
         example_config = yaml.load(f)
     config = {
-        k: os.environ[k]
+        k: env_value_to_python(os.environ[k])
         for k in example_config.keys()
         if k in os.environ
     }
