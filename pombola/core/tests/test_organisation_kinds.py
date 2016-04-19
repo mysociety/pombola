212a76a05f2fbb5a7c0b4fc429f4ac019a763aef
Duncan Parkes <duncan@mysociety.org>
2016-04-19 17:14:33 +0100

    Tidy up import order

--- pombola/core/tests/test_organisation_kinds.py
+++ pombola/core/tests/test_organisation_kinds.py
@@ -1,10 +1,9 @@
 from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
 from pombola.core import models
 from pombola.slug_helpers.models import SlugRedirect
-
-from django.contrib.contenttypes.models import ContentType
 
 
 class OrganisationKindTest(TestCase):
