5630dbe21a0faa846aeaf4b7d4529e285a317805
Mark Longair <mhl@pobox.com>
2016-04-27 15:16:55 +0100

    Keep the usual STATIC_ROOT except on Heroku
    It was a mistake to change this generally; it was only intended to
    switch to the staticfiles directory for deploys to Heroku.

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -106,7 +106,10 @@ else:
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.normpath( os.path.join( base_dir, "staticfiles") )
+if 'ON_HEROKU' in os.environ:
+    STATIC_ROOT = os.path.normpath( os.path.join( base_dir, "staticfiles") )
+else:
+    STATIC_ROOT = os.path.normpath( os.path.join( root_dir, "collected_static/") )
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
