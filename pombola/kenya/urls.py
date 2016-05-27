01123c8f336c50d621ceeb5e1868656df221c0d7
Mark Longair <mhl@pobox.com>
2016-05-27 09:45:31 +0100

    Fix for Kenya URL overrides breaking /person/all/

--- pombola/kenya/urls.py
+++ pombola/kenya/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView, RedirectView, TemplateView
+from pombola.core import models
 from pombola.kenya.views import (
     KEPersonDetail,
     KEPersonDetailAppearances,
@@ -33,6 +34,9 @@ urlpatterns = [
     url(r'^register-to-vote$',     TemplateView.as_view(template_name='register-to-vote.html') ),
     url(r'^find-polling-station$', TemplateView.as_view(template_name='find-polling-station.html') ),
     url(r'^women/$', TemplateView.as_view(template_name='women.html') ),
+    url(r'^person/all/$',
+        ListView.as_view(model=models.Person),
+        name='person_list'),
     url(r'^person/(?P<slug>[-\w]+)/$',
         KEPersonDetail.as_view(), name='person'),
     url(r'^person/(?P<slug>[-\w]+)/appearances/$',
