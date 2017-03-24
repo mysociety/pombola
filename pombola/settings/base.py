a5db925e28a7f87160dfd705831313dbcd06c186
Mark Longair <mhl@pobox.com>
2017-03-24 09:37:44 +0000

    A small PEP-8 whitespace fix

--- pombola/settings/base.py
+++ pombola/settings/base.py
@@ -30,7 +30,7 @@ TEMPLATES = [
             # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
             # Always use forward slashes, even on Windows.
             # Don't forget to use absolute paths, not relative paths.
-            os.path.join( base_dir, "pombola/templates" ),
+            os.path.join(base_dir, "pombola/templates"),
         ],
         'OPTIONS': {
             'context_processors': [
