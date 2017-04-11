32f7563a560885766e60f3e1c19a298aee3a7436
Zarino Zappia <mail@zarino.co.uk>
2017-04-11 17:34:48 +0100

    [ZA] A/B test for MP Profiles homepage link
    50% of mobile users will see a version of the homepage with the red
    "Hot topics" links at the top of the homepage, rather than the bottom.
    
    We track clicks on the "MP Profiles" link, so we can test whether the
    new position of the links causes an increase in visitors to the MP
    Profiles page.
    
    Part of #2250.

--- pombola/core/static/js/analytics.js
+++ pombola/core/static/js/analytics.js
@@ -3,7 +3,7 @@
 
 
 (function() {
-  // based on code from http://stackoverflow.com/a/11060206/5349  
+  // based on code from http://stackoverflow.com/a/11060206/5349
 
   // track the print request - with debounce for chrome.
   var haveTracked = false;
@@ -26,5 +26,55 @@
     });
   }
   window.onbeforeprint = beforePrint;
+
+  window.analytics = {
+    trackEvents: function(listOfEvents){
+      // Takes a list of arguments suitable for trackEvent.
+      // Returns a jQuery Deferred object.
+      // The deferred object is resolved when
+      // all of the trackEvent calls are resolved.
+      var dfd = $.Deferred();
+      var deferreds = [];
+      var _this = this;
+      $.each(listOfEvents, function(i, params){
+          deferreds.push(_this.trackEvent(params));
+      });
+      $.when.apply($, deferreds).done(function(){
+          dfd.resolve();
+      });
+      return dfd.promise();
+    },
+
+    trackEvent: function(params){
+      // Takes an object of event parameters, eg:
+      // { eventCategory: 'foo', eventAction: 'bar' }
+      // Returns a jQuery Deferred object.
+      // The deferred object is resolved when the GA call
+      // completes or fails to respond within 2 seconds.
+      var dfd = $.Deferred();
+
+      if(typeof ga === 'undefined' || !ga.loaded){
+        // GA has not loaded (blocked by adblock?)
+        return dfd.resolve();
+      }
+
+      var defaults = {
+        hitType: 'event',
+        eventLabel: document.title,
+        hitCallback: function(){
+          dfd.resolve();
+        }
+      }
+
+      ga('send', $.extend(defaults, params));
+
+      // Wait a maximum of 2 seconds for GA response.
+      setTimeout(function(){
+        dfd.resolve();
+      }, 2000);
+
+      return dfd.promise();
+    }
+  };
 
 }());
