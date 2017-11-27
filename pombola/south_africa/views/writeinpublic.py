22cff22eec4afa8eafa8cc3994a8fb5842196f66
Chris Mytton <chrismytton@gmail.com>
2017-11-27 18:32:53 +0000

    Redirect to WiP message after creation
    After a WriteInPublic message has been created redirect to the page for
    the individual message.

--- pombola/south_africa/views/writeinpublic.py
+++ pombola/south_africa/views/writeinpublic.py
@@ -1,9 +1,10 @@
-from pombola.writeinpublic.forms import MessageForm
 from django.views.generic.edit import FormView
-
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponseServerError
+from django.core.urlresolvers import reverse
 
+from pombola.writeinpublic.forms import MessageForm
 from pombola.core.models import Person
 from pombola.writeinpublic.client import WriteInPublic
 
@@ -11,8 +12,6 @@ from pombola.writeinpublic.client import WriteInPublic
 class SAWriteToRepresentative(FormView):
     template_name = "writeinpublic/person-write.html"
     form_class = MessageForm
-    # TODO: Change this to redirect to message page
-    success_url = '/person/write/confirmation/'
 
     def get_context_data(self, **kwargs):
         context = super(SAWriteToRepresentative, self).get_context_data(**kwargs)
@@ -26,17 +25,21 @@ class SAWriteToRepresentative(FormView):
         person_slug = self.kwargs['person_slug']
         person = get_object_or_404(Person, slug=person_slug)
         client = WriteInPublic("http://10.11.12.13.xip.io:8000", "admin", "123abc")
-        r = client.create_message(
+        response = client.create_message(
             author_name=form.cleaned_data['author_name'],
             author_email=form.cleaned_data['author_email'],
             subject=form.cleaned_data['subject'],
             content=form.cleaned_data['content'],
             # FIXME: This shouldn't be hard-coded
             writeitinstance="/api/v1/instance/3/",
-            # FIXME: This shouldn't be hard-coded
             persons=["https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/data/South_Africa/Assembly/ep-popolo-v1.0.json#person-{uuid}".format(uuid=person.everypolitician_uuid)],
         )
-        return super(SAWriteToRepresentative, self).form_valid(form)
+        if response.ok:
+            message_id = response.json()['id']
+            return HttpResponseRedirect(reverse('sa-writeinpublic-message', kwargs={'message_id': message_id}))
+        else:
+            # FIXME: This should do something more intelligent
+            return HttpResponseServerError()
 
 
 class SAWriteInPublicMessage(TemplateView):
@@ -45,5 +48,5 @@ class SAWriteInPublicMessage(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(SAWriteInPublicMessage, self).get_context_data(**kwargs)
         client = WriteInPublic("http://10.11.12.13.xip.io:8000", "admin", "123abc")
-        context['message'] = client.get_message(8)
+        context['message'] = client.get_message(self.kwargs['message_id'])
         return context
