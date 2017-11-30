d309db1ccd296758110a97ac5d15a74b1ae846ed
Zarino Zappia <mail@zarino.co.uk>
2017-11-30 12:12:19 +0000

    [ZA] Styling and markup for person-write form

--- pombola/writeinpublic/forms.py
+++ pombola/writeinpublic/forms.py
@@ -5,6 +5,6 @@ from pombola.writeinpublic.client import WriteInPublic
 
 class MessageForm(forms.Form):
     author_name = forms.CharField()
-    author_email = forms.CharField()
+    author_email = forms.EmailField()
     subject = forms.CharField()
     content = forms.CharField(widget=forms.Textarea)
