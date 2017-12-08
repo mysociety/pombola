6800b6511a7d316b43c3604f094bde5364b10ba8
Chris Mytton <chrismytton@gmail.com>
2017-12-08 16:45:01 +0000

    Use chosen.js to enhance WriteInPublic recipients screen
    This makes the dropdown the on the WriteInPublic recipients screen
    support autocomplete, rather than making the user scroll through a long
    list of names.

--- pombola/settings/south_africa_base.py
+++ pombola/settings/south_africa_base.py
@@ -47,7 +47,13 @@ COUNTRY_CSS = {
             'css/libs/datatables-1.10.10.css',
         ),
         'output_filename': 'css/datatables.css'
-    }
+    },
+    'chosen': {
+        'source_filenames': {
+            'css/libs/chosen/chosen.css',
+        },
+        'output_filename': 'css/chosen.css'
+    },
 }
 
 COUNTRY_JS = {
@@ -107,7 +113,14 @@ COUNTRY_JS = {
             'js/mp-profiles-live-filter.js',
         ),
         'output_filename': 'js/mp-profiles-live-filter.js',
-    }
+    },
+    'writeinpublic': {
+        'source_filenames': (
+            'js/libs/chosen.jquery.js',
+            'js/writeinpublic.js',
+        ),
+        'output_filename': 'js/writeinpublic.js'
+    },
 }
 
 INFO_PAGES_ALLOW_RAW_HTML = True
---
+++ pombola/south_africa/static/js/writeinpublic.js
@@ -0,0 +1,3 @@
+jQuery(function($){
+  $('#id_recipients-persons').chosen();
+});
