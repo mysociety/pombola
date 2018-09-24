ce675de3add4b9b4159c2679ec77266268bad42b
Zarino Zappia <mail@zarino.co.uk>
2018-09-24 14:50:40 +0100

    New $.blogFeed jQuery plugin for loading external RSS feeds

--- pombola/core/static/js/feeds.js
+++ pombola/core/static/js/feeds.js
@@ -1,50 +1,76 @@
-(function() {
+(function($){
+    $.fn.blogFeed = function(){
+        return this.each(function(){
+            var $container = $(this);
+            var feedURL = $container.attr('data-blog-rss-feed');
+            var templateHTML = $( $container.attr('data-blog-template') ).html();
+            var maxItems = parseInt($container.attr('data-blog-items')) || 2;
+            var loadingTimeout;
 
-    var dateFormat = function(dateObject){
-        var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        var day = dateObject.getDay();
-        var date = dateObject.getDate();
-        var month = dateObject.getMonth();
-        var year = dateObject.getFullYear();
-        return '<span class="day">' + days[day] +
-          '</span> <span class="month">' + months[month] +
-          '</span> <span class="date">' + date +
-          '</span> <span class="year">' + year + '</span>';
-    }
-
-    // This Javascript will populate a <ul> with list items from an
-    // RSS feed. The remote site that serves the RSS should include
-    // CORS headers to allow this Javascript to fetch the RSS feed.
-
-    var blogContainer = $("#home-news-list");
-    var maximumListItems = 2;
-    if ( blogContainer ) {
-        var feedURL = blogContainer.attr("data-blog-rss-feed");
-        $.ajax(feedURL, {dataType: 'xml'}).done(
-            function (xml) {
-                blogContainer.html('');
-                $(xml).find('item').each(function (index) {
-                    var item = $(this),
-                        listItem = $('<li>'),
-                        title = item.find('title').text(),
-                        url = item.find('link').text(),
-                        description = item.find('description').text(),
-                        published = new Date(item.find('pubDate').text());
-                    if (index >= maximumListItems) {
-                        return false;
-                    };
-                    listItem.append(
-                        $('<a>').text(title).attr({href: url}).wrap('<h3></h3>').parent()
-                    ).append(
-                        $('<p>').addClass('meta').html(dateFormat(published))
-                    ).append(
-                        $('<p>').html(description)
-                    );
-                    blogContainer.append(listItem);
-                })
+            var dateFormat = function(dateObject) {
+                var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+                var date = dateObject.getDate();
+                var month = dateObject.getMonth();
+                var year = dateObject.getFullYear();
+                return '<span class="date">' + date +
+                '</span> <span class="month">' + months[month] +
+                '</span> <span class="year">' + year + '</span>';
             }
-        );
-    }
 
-})();
+            var showLoading = function() {
+                $container.empty();
+                for (var i=0; i<maxItems; i++) {
+                    var $el = $(templateHTML);
+                    $el.find('[data-blog-title]').html(
+                        greek('90%') + greek('95%') + greek('30%')
+                    );
+                    $el.find('[data-blog-date]').html(greek('6em'));
+                    $el.find('[data-blog-description').html(
+                        greek('90%') + greek('95%') + greek('85%') +
+                        greek('90%') + greek('95%') + greek('30%')
+                    );
+                    $el.appendTo($container);
+                }
+            };
+
+            var greek = function(width) {
+                return '<span class="greek" style="width: ' + (width || '100%') + '"></span>';
+            };
+
+            if (feedURL && templateHTML && maxItems) {
+                // We can get away with half a second of zero feedback
+                // but after that, we really should show a loading state.
+                loadingTimeout = setTimeout(showLoading, 500);
+
+                $.ajax(feedURL, {dataType: 'xml'}).done(function(xml) {
+                    clearTimeout(loadingTimeout);
+                    $container.empty();
+                    $(xml).find('item').each(function(index) {
+                        if (index >= maxItems) {
+                            return false; // break out of .each() loop
+                        }
+
+                        var rssItem = $(this),
+                            title = rssItem.find('title').text(),
+                            url = rssItem.find('link').text(),
+                            description = rssItem.find('description').text(),
+                            date = new Date(rssItem.find('pubDate').text()),
+                            $el = $(templateHTML);
+
+                        $el.find('[data-blog-title]').html(title);
+                        $el.find('[data-blog-link]').attr('href', url);
+                        $el.find('[data-blog-date]').html(dateFormat(date));
+                        $el.find('[data-blog-description]').html(description);
+                        $el.appendTo($container);
+                    });
+                }).fail(function(){
+                    clearTimeout(loadingTimeout);
+                });
+            }
+        });
+    };
+
+    $(function(){
+        $('[data-blog-rss-feed]').blogFeed();
+    });
+})(jQuery);
