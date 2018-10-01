e74ff8f49224566961f4443b401b4125746a7432
Chris Mytton <chrism@mysociety.org>
2018-10-01 17:31:46 +0100

    [KE] Add admin section for managing SMS
    This allows SMS to be accepted or rejected from the admin screen.

---
+++ pombola/sms/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('text', 'msisdn', 'datetime', 'status')
+    list_filter = ('status',)
+    # list_display_links = None
+    actions = ['mark_as_accepted', 'mark_as_rejected']
+    date_hierarchy = 'datetime'
+    readonly_fields = ('text', 'msisdn', 'datetime')
+
+    def mark_as_accepted(self, request, queryset):
+        queryset.update(status=Message.ACCEPTED)
+    mark_as_accepted.short_description = 'Accept selected messages'
+
+    def mark_as_rejected(self, request, queryset):
+        queryset.update(status=Message.REJECTED)
+    mark_as_rejected.short_description = 'Reject selected messages'
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.disable_action('delete_selected')
+admin.site.register(Message, MessageAdmin)
