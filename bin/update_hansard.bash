d3ea3e712f8fc4c52d3bbcac5f4f38c1a3899449
Chris Mytton <chrism@mysociety.org>
2018-11-07 12:26:09 +0000

    [KE] Move unmatched speaker email to separate cronjob
    Remove the `hansard_list_unmatched_speakers` management command from the
    `bin/update_hansard.bash` and replaces it with the
    `hansard_email_unmatched_speakers` command, which is now run directly
    from cron, rather than from the Hansard script.
    
    This means that we can remove the extra `MAILTO` calls in the crontab,
    meaning all cron errors should go to developers.

--- bin/update_hansard.bash
+++ bin/update_hansard.bash
@@ -11,7 +11,3 @@ source ../pombola-virtualenv/bin/activate
 ./manage.py hansard_check_for_new_sources
 ./manage.py hansard_process_sources
 ./manage.py hansard_assign_speakers
-
-# This will print out to STDOUT if it finds any. This should then get emailed to
-# the right people by the cron script.
-./manage.py hansard_list_unmatched_speakers
