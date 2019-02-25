a6678e5aba43bbc285c20e3c714b0dd3cad05a70
Nick Jackson <nick@nickjackson.me>
2019-02-25 17:19:47 +0000

    Include contact details in committee report

--- pombola/south_africa/management/commands/south_africa_export_committee_members.py
+++ pombola/south_africa/management/commands/south_africa_export_committee_members.py
@@ -58,6 +58,15 @@ class Command(BaseCommand):
             'start_date',
             'end_date',
             'parties',
+            'cell',
+            'voice',
+            'email',
+        ]
+
+        contact_kinds = [
+            'cell',
+            'voice',
+            'email',
         ]
 
         with open(os.path.join(destination), 'wb') as output_file:
@@ -79,6 +88,12 @@ class Command(BaseCommand):
                     for party in person.parties():
                         parties.append(party.name)
 
+                    contacts = {}
+                    for contact_kind in contact_kinds:
+                        contacts[contact_kind] = []
+                        for contact in person.contacts.all().filter(kind__slug=contact_kind):
+                            contacts[contact_kind].append(contact.value)
+
                     position_output = {
                         'name': person.name,
                         'title': person.title,
@@ -90,7 +105,10 @@ class Command(BaseCommand):
                         'url': 'https://www.pa.org.za/person/{}/'.format(person.slug),
                         'start_date': formatApproxDate(position.start_date),
                         'end_date': formatApproxDate(position.end_date),
-                        'parties': ', '.join(parties)
+                        'parties': ', '.join(parties),
+                        'cell': ', '.join(contacts['cell']),
+                        'voice': ', '.join(contacts['voice']),
+                        'email': ', '.join(contacts['email']),
                     }
                     writer.writerow(position_output)
 
