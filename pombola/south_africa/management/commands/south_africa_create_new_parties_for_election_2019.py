ae290441ef23c0704d6cfef106476382cecbfd26
Chris Mytton <chrism@mysociety.org>
2019-05-14 15:57:45 +0100

    [ZA] Add parties for 2019 elections
    This adds a CSV file containing the parties from the 2019 elections that
    pombola doesn't know about. There's also a management command to import
    that data into the database.

---
+++ pombola/south_africa/management/commands/south_africa_create_new_parties_for_election_2019.py
@@ -0,0 +1,27 @@
+import unicodecsv
+
+from django.core.management.base import BaseCommand, CommandError
+
+from pombola.core.models import Organisation, OrganisationKind
+
+
+parties_csv = "pombola/south_africa/data/parties-2019-elections.csv"
+
+
+class Command(BaseCommand):
+    help = "Creates new parties for the 2019 elections"
+
+    def handle(self, *args, **options):
+        with open(parties_csv, "rb") as csvfile:
+            csv = unicodecsv.reader(csvfile)
+            for row in csv:
+                party_slug = row[0]
+                party_name = row[1]
+                party_kind = OrganisationKind.objects.get(slug="party")
+                party, created = Organisation.objects.get_or_create(
+                    slug=party_slug, name=party_name, kind=party_kind
+                )
+                if created:
+                    print("Created new party: {}".format(party))
+                else:
+                    print("Party already exists: {}".format(party))
