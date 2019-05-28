a2b900a29073691fc74bacec0b2e1f7009a6761f
Sam Pearson <sam@sgp.me.uk>
2019-05-28 16:04:46 +0100

    Ensure virtualenv location is derived from config
    Now that the location of the virtualenv is configurable, we need to
    dervive this from config. This adds steps to ensure this is done in
    a couple of helper scripts.
    
    Currently this includes a dependency on commonlib and on having
    PyYAML available system-wide (this is already listed as a dependency).

--- bin/update_hansard.bash
+++ bin/update_hansard.bash
@@ -6,7 +6,13 @@ set -e
 # check that we are in the expected directory
 cd `dirname $0`/..
 
-source ../pombola-virtualenv/bin/activate
+# Get application config
+source commonlib/shlib/deployfns
+read_conf conf/general.yml
+
+# Activate virtualenv
+virtualenv_dir="${OPTION_DATA_DIR}/pombola-virtualenv"
+source ${virtualenv_dir}/bin/activate
 
 ./manage.py hansard_check_for_new_sources
 ./manage.py hansard_process_sources
