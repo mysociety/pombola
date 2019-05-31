1cd1f55f7d74cad40ff0e9a9dd207aea20247389
Chris Mytton <chrism@mysociety.org>
2019-05-31 09:14:00 +0100

    Update 2014 election results script for 2019
    This is the simplified version, which just handles adding the new
    memberships for people in the National Assembly and Provincial
    Legislatures.

--- pombola/south_africa/management/commands/south_africa_import_election_results_2019.py
+++ pombola/south_africa/management/commands/south_africa_import_election_results_2019.py
@@ -1,14 +1,10 @@
-import re
+import string
 
 import unicodecsv as csv
-
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.exceptions import MultipleObjectsReturned
-from django.core.management.base import LabelCommand
-
-from django.utils.text import slugify
-
 from django_date_extensions.fields import ApproximateDate
+
+from django.core.management.base import NoArgsCommand
+from django.utils.text import slugify
 
 from pombola.core.models import (
     Person,
@@ -16,361 +12,125 @@ from pombola.core.models import (
     OrganisationKind,
     Place,
     PositionTitle,
-    Contact,
-    ContactKind,
-    ContentType,
 )
 
 # Pretty colours to make it easier to spot things.
 HEADER = "\033[95m"
 ENDC = "\033[0m"
 
-
-def parse_approximate_date(s):
-    """Take a partial ISO 8601 date, and return an ApproximateDate for it
-
-    >>> ad = parse_approximate_date('2014-02-17')
-    >>> type(ad)
-    <class 'django_date_extensions.fields.ApproximateDate'>
-    >>> ad
-    2014-02-17
-    >>> parse_approximate_date('2014-02')
-    2014-02-00
-    >>> parse_approximate_date('2014')
-    2014-00-00
-    """
-
-    for regexp in [r"^(\d{4})-(\d{2})-(\d{2})$", r"^(\d{4})-(\d{2})$", r"^(\d{4})$"]:
-        m = re.search(regexp, s)
-        if m:
-            return ApproximateDate(*(int(g, 10) for g in m.groups()))
-    if s == "future":
-        return ApproximateDate(future=True)
-    raise Exception, "Couldn't parse '{0}' as an ApproximateDate".format(s)
+start_date = ApproximateDate(year=2019, month=5, day=22)
 
 
-class Command(LabelCommand):
+class Command(NoArgsCommand):
 
     help = "Imports South Africa election results from CSV."
 
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
-
-        self.contact_kind_phone = ContactKind.objects.get(slug="phone")
-        self.content_type_person = ContentType.objects.get_for_model(Person)
-
-        self.organisation_executive = Organisation.objects.get(
-            slug="national-executive"
-        )
         self.organisation_assembly = Organisation.objects.get(slug="national-assembly")
-        self.organisation_ncop = Organisation.objects.get(slug="ncop")
+        self.position_title_member = PositionTitle.objects.get(name="Member")
 
-    def match_person(self, name):
-        """ Match up a person with their database entry, or create a new one. """
+    def find_person(self, election_identifier):
+        """ Find a person using their 2019 election identifier """
+        people = Person.objects.filter(
+            identifiers__scheme="elections_2019",
+            identifiers__identifier=election_identifier,
+        )
+        if len(people) == 0:
+            raise Exception(
+                "No people found for identifier {}".format(election_identifier)
+            )
+        elif len(people) > 1:
+            raise Exception(
+                "Multiple people found for identifier {}".format(election_identifier)
+            )
+        else:
+            return people[0]
 
-        slug = slugify(name)
-
-        # Try match on the name first
-        try:
-            person = Person.objects.get(legal_name__iexact=name)
-        except ObjectDoesNotExist:
-            print "Unable to match " + name + " by name, trying by slug"
-            try:
-                person = Person.objects.get(slug=slug)
-            except ObjectDoesNotExist:
-                print "Unable to match " + name + " by slug, creating a new person"
-                person = Person.objects.create(legal_name=name, slug=slug)
-
-        except MultipleObjectsReturned:
-            print "Multiple people returned for " + name + " (" + slug + "). Cannot continue."
-            exit(1)
-
-        return person
-
-    def match_position_title(self, title):
-        """ Find a position title by name, or create one. """
-
-        slug = slugify(title)
-
-        position, created = PositionTitle.objects.get_or_create(slug=slug)
-
-        if created:
-            position.name = title
-            position.save()
-            print "Created new position " + slug + " as " + title
-
-        return position
-
-    def match_organisation(self, name, kind=None):
+    def match_organisation(self, name, kind):
         """ Find an organisation by name, or create one. """
 
         slug = slugify(name)
 
-        if kind:
-            kind = OrganisationKind.objects.get(slug=kind)
-            organisation, created = Organisation.objects.get_or_create(
-                slug=slug, kind=kind
-            )
-        else:
-            organisation, created = Organisation.objects.get_or_create(slug=slug)
-
-        if created:
-            organisation.name = name
-            organisation.save()
-            print "Created new organisation" + slug + " as " + name
-
-        return organisation
+        kind = OrganisationKind.objects.get(slug=kind)
+        return Organisation.objects.get(slug=slug, kind=kind)
 
     def match_place(self, name):
         """ Match up a place with its database entry, or create a new one. """
 
         slug = slugify(name)
 
-        place, created = Place.objects.get_or_create(slug=slug)
-
-        if created:
-            place.name = name
-            place.save()
-            print "Created new place " + slug + " as " + name
-
-        return place
-
-    def save_contact_list(self, contact, person, contact_kind):
-
-        contact = contact.strip()
-
-        # We might already have this, so get_or_create
-        contact_object, created = Contact.objects.get_or_create(
-            value=contact.strip(),
-            object_id=person.id,
-            content_type=self.content_type_person,
-            kind=contact_kind,
-        )
+        return Place.objects.get(slug=slug)
 
     def read_file(self, file):
-        """ Deal with the business of actually iterating over a file and passing the lines to a function. """
+        """ Iterate over a file and pass the lines to a function. """
 
         print HEADER + "Importing from " + file + ENDC
 
-        with open(self.path + file) as f:
-
-            # Use a DictReader so this is a bit more futureproof if the CSV changes.
+        with open(file) as f:
             data = csv.DictReader(f)
 
-            # Iterate over the data, call the function given.
             for person_csv in data:
                 yield person_csv
 
-    def import_executive(self, person_csv):
-        """ Import data for the Executive. """
-
-        # Find the person
-        person = self.match_person(
-            person_csv["First names"].strip() + " " + person_csv["Surname"].strip()
-        )
-
-        # If the person has a preferred name, set it
-        if person_csv["Preferred Name"]:
-            person.add_alternative_name(person_csv["Preferred Name"], True)
-
-        # Find the position title
-        position_title = self.match_position_title(person_csv["Position Title"])
-
-        # Get the position if it already exists, if not create it
-        position, created = person.position_set.get_or_create(
-            title=position_title,
-            organisation=self.organisation_executive,
-            start_date=parse_approximate_date(person_csv["Start Date"]),
-        )
-
-        # Set the things that weren't used to match
-        position.category = "political"
-        position.end_date = parse_approximate_date(person_csv["End Date"])
-
-        # Save the new position
-        position.save()
-
     def import_assembly(self, person_csv):
-        """ Import data for the Assembly. """
-
-        # Find the person
-        person = self.match_person(
-            person_csv["First names"].strip() + " " + person_csv["Surname"].strip()
-        )
-
-        # This person might have a title; if so set it
-        if person_csv["Title"]:
-            person.title = person_csv["Title"]
-            person.save()
-
-        # Find the position title
-        position_title = self.match_position_title(person_csv["Position Title"])
+        """ Import data for the National Assembly """
+        person = self.find_person(person_csv["ID"])
 
         # Find the place, assuming it is set
-        if person_csv["Place"]:
-            place = self.match_place(person_csv["Place"])
+        list_name = person_csv["List"]
+        if list_name != "National":
+            place_name = string.replace(list_name, "Regional: ", "")
+            place = self.match_place(place_name)
         else:
             place = None
 
         # Get the position if it already exists, if not create it
         position, created = person.position_set.get_or_create(
-            title=position_title,
             organisation=self.organisation_assembly,
+            title=self.position_title_member,
             place=place,
-            start_date=parse_approximate_date(person_csv["Start Date"]),
+            start_date=start_date,
+            defaults={"category": "political"},
         )
 
-        # Set the things that weren't used to match
-        position.category = "political"
-        position.end_date = parse_approximate_date(person_csv["End Date"])
-
-        # Save the new position
-        position.save()
+        if created:
+            print "Created new position: {}".format(position)
+        else:
+            print "Existing position found: {}".format(position)
 
     def import_mpls(self, person_csv):
-        """ Import MPLs. """
+        """ Import data for the Provincial Legislatures """
+        person = self.find_person(person_csv["ID"])
 
-        # Find the person
-        person = self.match_person(
-            person_csv["First Name"].strip() + " " + person_csv["Last Name"].strip()
-        )
-
-        # This person might have phone numbers; if so set it
-        if person_csv["Cell phone"]:
-            self.save_contact_list(
-                person_csv["Cell phone"], person, self.contact_kind_phone
-            )
-
-        # If the person has a preferred name, set it
-        if person_csv["Preferred Name"]:
-            person.add_alternative_name(person_csv["Preferred Name"], True)
-
-        # Find the position title.
-        position_title = self.match_position_title(person_csv["Position Title"])
-
-        # Find the organisation
+        # Find the place, assuming it is set
+        place_name = string.replace(person_csv["List"], "Provincial: ", "")
+        place = self.match_place(place_name)
         organisation = self.match_organisation(
-            person_csv["Organisation"], "provincial-legislature"
+            "{} Provincial Legislature".format(place_name), "provincial-legislature"
         )
-
-        # Find the place
-        place = self.match_place(person_csv["Place name"])
 
         # Get the position if it already exists, if not create it
         position, created = person.position_set.get_or_create(
-            title=position_title,
             organisation=organisation,
+            title=self.position_title_member,
             place=place,
-            start_date=parse_approximate_date(person_csv["Start Date"]),
+            start_date=start_date,
+            defaults={"category": "political"},
         )
 
-        # Set the things that weren't used to match
-        position.category = "political"
-        position.end_date = parse_approximate_date(person_csv["End Date"])
-
-        # Save the new position
-        position.save()
-
-    def import_ncop(self, person_csv):
-        """ Import NCOP members. """
-
-        # Find the person
-        person = self.match_person(
-            person_csv["First names"].strip() + " " + person_csv["Surname"].strip()
-        )
-
-        # This person might have a title; if so set it
-        if person_csv["Title"]:
-            person.title = person_csv["Title"]
-            person.save()
-
-        # This person might have phone numbers; if so set it
-        if person_csv["Cell Phone"]:
-            self.save_contact_list(
-                person_csv["Cell Phone"], person, self.contact_kind_phone
-            )
-
-        # Find the position title.
-        position_title = self.match_position_title(person_csv["Position Title"])
-
-        # Find the place, assuming it is set
-        if person_csv["Place Name"]:
-            place = self.match_place(person_csv["Place Name"])
+        if created:
+            print "Created new position: {}".format(position)
         else:
-            place = None
+            print "Existing position found: {}".format(position)
 
-        # Get the position if it already exists, if not create it
-        position, created = person.position_set.get_or_create(
-            title=position_title,
-            organisation=self.organisation_ncop,
-            place=place,
-            start_date=parse_approximate_date(person_csv["Start Date"]),
-        )
-
-        # Set the things that weren't used to match
-        position.category = "political"
-        position.end_date = parse_approximate_date(person_csv["End Date"])
-
-        # Save the new position
-        position.save()
-
-    def import_iec_assignment(self, person_csv):
-        """ Import IEC seat assignments. """
-
-        # Find the person
-        person = self.match_person(
-            person_csv["Name"].strip() + " " + person_csv["Surname"].strip()
-        )
-
-        # Find the position title.
-        position_title = self.match_position_title(person_csv["Position Title"])
-
-        # Find the place, assuming it is set
-        if person_csv["Place Name"]:
-            place = self.match_place(person_csv["Place Name"])
-        else:
-            place = None
-
-        # Find the organisation
-        organisation = self.match_organisation(person_csv["Organisation"])
-
-        # Get the position if it already exists, if not create it
-        position, created = person.position_set.get_or_create(
-            title=position_title,
-            organisation=organisation,
-            place=place,
-            start_date=parse_approximate_date(person_csv["Start Date"]),
-        )
-
-        # Set the things that weren't used to match
-        position.category = "political"
-        position.end_date = parse_approximate_date(person_csv["End Date"])
-
-        # Save the new position
-        position.save()
-
-    def handle_label(self, path, **options):
-
-        self.path = path
-
-        # Read and process the Executive file.
-        for person_row in self.read_file("executive.csv"):
-            self.import_executive(person_row)
+    def handle_noargs(self, **options):
+        path = "pombola/south_africa/data/elections/2019/"
 
         # Read and process the Assembly file.
-        for person_row in self.read_file("assembly.csv"):
-            self.import_assembly(person_row)
+        for person_row in self.read_file(path + "national-seats-assigned.csv"):
+            self.import_assembly(person_csv=person_row)
 
-        # Read and process the MPLs file.
-        for person_row in self.read_file("mpls.csv"):
-            self.import_mpls(person_row)
-
-        # Read and process the NCOP file.
-        for person_row in self.read_file("ncop.csv"):
-            self.import_ncop(person_row)
-
-        # Read and process the IEC assignments file.
-        for person_row in self.read_file("iec_seat_assignment.csv"):
-            self.import_iec_assignment(person_row)
-
-        print "Done!"
+        # Read and process the MPL file.
+        for person_row in self.read_file(path + "provincial-seats-assigned.csv"):
+            self.import_mpls(person_csv=person_row)
