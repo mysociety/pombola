37516c5fb91836256509f3567f532b50340e0a07
Nick Jackson <nick@nickjackson.me>
2019-06-06 12:06:56 +0100

    Remove Nigeria country folder

--- pombola/nigeria/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from .views import NGHomeView, NGSearchView
-
-
-urlpatterns = [
-    url(r'^$', NGHomeView.as_view(), name='home'),
-    url(r'^search/$', NGSearchView.as_view(), name='core_search'),
-]
