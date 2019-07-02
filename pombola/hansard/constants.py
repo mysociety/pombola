4c8ab7ae2fe5a551da2b3ee09f5a543708060b2e
Nick Jackson <nick@nickjackson.me>
2019-07-02 12:50:09 +0100

    Remove Kenya Hansard parser

--- pombola/hansard/constants.py
+++
@@ -1,4 +0,0 @@
-# Symbols for different name matching algorithms
-
-NAME_SUBSTRING_MATCH = 1  # Look for the whole name without the title.
-NAME_SET_INTERSECTION_MATCH = 2  # Intersect sets of words, including the title.
--- pombola/hansard/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-from alias import Alias
-from source import Source, SourceUrlCouldNotBeRetrieved, SourceCouldNotParseTimeString
-from venue import Venue
-from sitting import Sitting
-from entry import Entry, NAME_SUBSTRING_MATCH, NAME_SET_INTERSECTION_MATCH
