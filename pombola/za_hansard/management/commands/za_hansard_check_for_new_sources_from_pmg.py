f820c42bd65e5ca958fe4d1f73093050ed95450a
Chris Mytton <chrismytton@gmail.com>
2019-08-23 17:13:24 +0100

    Add new command to pull hansard sources from PMG's API

---
+++ pombola/za_hansard/management/commands/za_hansard_check_for_new_sources_from_pmg.py
@@ -0,0 +1,46 @@
+import requests
+
+from django.core.management.base import BaseCommand, CommandError
+
+from pombola.za_hansard.models import Source
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        hansard_url = 'https://api.pmg.org.za/hansard/'
+        while True:
+            self.stdout.write(u"Fetching {}\n".format(hansard_url))
+            response = requests.get(hansard_url)
+            response.raise_for_status()
+            response_json = response.json()
+            hansards = response_json['results']
+            urls = [h['url'] for h in hansards]
+            for url in urls:
+                self.stdout.write(u"Fetching {}\n".format(url))
+                response = requests.get(url)
+                response.raise_for_status()
+                hansard = response.json()
+                if not hansard.get('files', []):
+                    continue
+                attached_file = hansard['files'][0]
+                title = attached_file['title'] or ''
+                db_source, created = Source.objects.get_or_create(**{
+                    'document_name': title[:200],
+                    'document_number': str(attached_file['id']),
+                    'defaults':
+                    {
+                        'url': attached_file['url'],
+                        'title': hansard['type'],
+                        'language': 'English',
+                        'house': hansard['house']['name_short'],
+                        'date': hansard['date'].split('T')[0],
+                    }
+                })
+                if created:
+                    self.stdout.write(u"Created one new source: {}\n".format(db_source))
+                else:
+                    self.stdout.write(u"Existing source found: {}\n".format(db_source))
+            if not response_json['next']:
+                break
+            hansard_url = response_json['next']
+
